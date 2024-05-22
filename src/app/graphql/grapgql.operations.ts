73725d4dbf5627e910f16e35f7fc523f3439adfe
CptFranck <francois.caillet71@gmail.com>
2024-05-22 17:16:34 +0200

    Add graphql provider and query

---
+++ src/app/graphql/grapgql.operations.ts
@@ -0,0 +1,53 @@
+import { gql } from 'apollo-angular';
+
+const GET_EXERCISES = gql`
+  query GetExercises {
+    getExercises {
+      id
+      name
+      description
+      goal
+      muscles {
+        id
+        name
+        description
+        function
+      }
+      exerciseTypes {
+        id
+        name
+        goal
+      }
+    }
+  }
+`;
+
+const GET_MUSCLES = gql`
+  query getMus {
+    getMuscles {
+      id
+      name
+      description
+      function
+      exercises {
+        id
+      }
+    }
+  }
+`;
+
+const GET_EXERCISETYPES = gql`
+  query getExType {
+    getExerciseTypes {
+      id
+      name
+      goal
+      exercises {
+        id
+        name
+      }
+    }
+  }
+`;
+
+export { GET_EXERCISES, GET_MUSCLES, GET_EXERCISETYPES };
