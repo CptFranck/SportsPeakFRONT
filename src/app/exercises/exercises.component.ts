0e6714e26e305a930fd2ebe6c27d332fa6ef5a84
CptFranck <francois.caillet71@gmail.com>
2024-05-22 17:17:20 +0200

    Add exercises component

---
+++ src/app/exercises/exercises.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { Apollo } from 'apollo-angular';
+import { GET_EXERCISES } from '../graphql/grapgql.operations';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-exercises',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './exercises.component.html',
+})
+export class ExercisesComponent implements OnInit {
+  exercises: any[] = [];
+  error: any;
+
+  constructor(private apollo: Apollo) {}
+
+  ngOnInit(): void {
+    this.apollo
+      .watchQuery({
+        query: GET_EXERCISES,
+      })
+      .valueChanges.subscribe(({ data, error }: any) => {
+        this.exercises = data.getExercises;
+        this.error = error;
+      });
+  }
+}
