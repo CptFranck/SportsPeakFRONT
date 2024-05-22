6c8fd0aabb4e3affa2d2e8c9a2e99427cdd35d3d
CptFranck <francois.caillet71@gmail.com>
2024-05-22 17:18:45 +0200

    Modify app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MusclesComponent } from './muscles/muscles.component';
+import { ExercisesComponent } from './exercises/exercises.component';
+import { ExerciseTypesComponent } from './exercise-types/exercise-types.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    MusclesComponent,
+    ExercisesComponent,
+    ExerciseTypesComponent,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'SportsPeak';
