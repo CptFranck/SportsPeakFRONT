325db20de2b2664063c5071fa26dc0b5571db51c
CptFranck <francois.caillet71@gmail.com>
2024-05-23 18:40:17 +0200

    Modify muscle component

--- src/app/page/muscles/muscles.component.ts
+++ src/app/page/muscles/muscles.component.ts
@@ -1,28 +1,36 @@
-import { Component, OnInit } from '@angular/core';
-import { Apollo } from 'apollo-angular';
-import { GET_MUSCLES } from '../../graphql/grapgql.operations';
-import { CommonModule } from '@angular/common';
+import {Component, OnInit} from '@angular/core';
+import {Apollo} from 'apollo-angular';
+import {GET_MUSCLES} from '../../graphql/grapgql.operations';
+import {CommonModule} from '@angular/common';
+import {ModalComponent} from "../../components/modal/modal.component";
 
 @Component({
   selector: 'app-muscles',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ModalComponent],
   templateUrl: './muscles.component.html',
 })
 export class MusclesComponent implements OnInit {
+  showDetails: { [key: string]: boolean } = {};
   muscles: any[] = [];
   error: any;
 
-  constructor(private apollo: Apollo) {}
+  constructor(private apollo: Apollo) {
+  }
 
   ngOnInit(): void {
     this.apollo
       .watchQuery({
         query: GET_MUSCLES,
       })
-      .valueChanges.subscribe(({ data, error }: any) => {
-        this.muscles = data.getMuscles;
-        this.error = error;
-      });
+      .valueChanges.subscribe(({data, error}: any) => {
+      this.muscles = data.getMuscles;
+      this.error = error;
+      this.muscles.map(value => this.showDetails[value.id] = false)
+    });
+  }
+
+  expendDetails(id: string): void {
+    this.showDetails[id] = !this.showDetails[id]
   }
 }
