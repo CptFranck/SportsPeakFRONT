cda22561ddc3149ce1657f1d71ab3deb03225107
CptFranck <francois.caillet71@gmail.com>
2024-05-23 18:40:36 +0200

    Add Modal component

---
+++ src/app/components/modal/modal.component.ts
@@ -0,0 +1,11 @@
+import {Component, Input} from '@angular/core';
+
+@Component({
+  selector: 'app-modal',
+  standalone: true,
+  imports: [],
+  templateUrl: './modal.component.html',
+})
+export class ModalComponent {
+  @Input() title!: string;
+}
