15cc35a45fc3ecf6d01e5e3d0c6befd0798c1bb6
CptFranck <francois.caillet71@gmail.com>
2024-06-01 00:28:51 +0200

    add delete muscle graphql operation

--- src/app/graphql/muscle/muscle.operations.ts
+++ src/app/graphql/muscle/muscle.operations.ts
@@ -28,7 +28,33 @@ export const ADD_MUSCLES = gql`
         id
         name
         goal
+        description
       }
     }
   }
 `;
+
+export const MOD_MUSCLES = gql`
+  mutation ($inputMuscle : InputMuscle!){
+    modifyMuscle(inputMuscle: $inputMuscle) {
+      id
+      name
+      function
+      description
+      exercises {
+        id
+        name
+        goal
+        description
+      }
+    }
+  }
+`;
+
+export const DEL_MUSCLES = gql`
+  mutation ($muscleId : ID!){
+    deleteMuscle(muscleId: $muscleId)
+  }
+`;
+
+
