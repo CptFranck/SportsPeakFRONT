6841c1661550158d098141bca25bbc574092bb9d
CptFranck <francois.caillet71@gmail.com>
2024-05-31 16:43:46 +0200

    Modify muscle array by implementing new modal button, and Collapse action

--- src/app/page/muscle/muscles-array/muscles-array.component.ts
+++ src/app/page/muscle/muscles-array/muscles-array.component.ts
@@ -1,25 +1,41 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 import {NgForOf, NgIf} from "@angular/common";
 import {Muscle} from "../../../interface/dto/muscle";
+import {ModalButtonComponent} from "../../../components/modal/modal-button/modal-button.component";
 
 @Component({
   selector: 'app-muscles-array',
   standalone: true,
   imports: [
     NgForOf,
-    NgIf
+    NgIf,
+    ModalButtonComponent
   ],
   templateUrl: './muscles-array.component.html',
 })
 export class MusclesArrayComponent implements OnInit {
   @Input() muscles!: Muscle[];
+  @Input() modalId!: string;
+  @Output() updateMuscle: EventEmitter<Muscle> = new EventEmitter();
   showDetails: { [id: string]: boolean } = {};
 
   ngOnInit(): void {
     this.muscles.map(muscle => this.showDetails[muscle.id] = false)
   }
 
-  expendDetails(id: string): void {
+  expendMuscleDetails(id: string): void {
     this.showDetails[id] = !this.showDetails[id]
+  }
+
+  showMuscleDetails(id: string): void {
+    this.showDetails[id] = !this.showDetails[id]
+  }
+
+  modifyMuscle(id: string): void {
+    this.showDetails[id] = !this.showDetails[id]
+  }
+
+  setMuscle(muscle: Muscle) {
+    this.updateMuscle.emit(muscle);
   }
 }
