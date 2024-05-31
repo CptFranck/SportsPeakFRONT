1cec32c3c866cf34ca9fba14f0145ef022020a5e
CptFranck <francois.caillet71@gmail.com>
2024-05-31 16:51:59 +0200

    Refactor muscle form

--- src/app/page/muscle/muscle-form/muscle-form.component.ts
+++ src/app/page/muscle/muscle-form/muscle-form.component.ts
@@ -2,13 +2,11 @@ import {Component, ElementRef, EventEmitter, Input, OnInit, Output, ViewChild} f
 import {MultiSelectComponent} from "../../../components/select/multi-select/multi-select.component";
 import {SelectExercisesComponent} from "../../../components/select/select-exercises/select-exercises.component";
 import {FormControl, FormGroup, ReactiveFormsModule, Validators} from "@angular/forms";
-import {InputMuscle} from "../../../interface/input/muscle";
 import {NgIf, NgTemplateOutlet} from "@angular/common";
 import {InputControlComponent} from "../../../components/input-control/input-control.component";
 import {Apollo} from "apollo-angular";
 import {Muscle} from "../../../interface/dto/muscle";
 import {GraphQLError} from "graphql/error";
-import {ADD_MUSCLES} from "../../../graphql/muscle/muscle.operations";
 
 @Component({
   selector: 'app-muscle-form',
@@ -25,35 +23,36 @@ import {ADD_MUSCLES} from "../../../graphql/muscle/muscle.operations";
 })
 export class MuscleFormComponent implements OnInit {
 
-  @Input() newMuscle: InputMuscle | undefined
-    = {
-    id: "",
-    name: "NewMuscleTest",
-    description: "NewMuscleTestDescription",
-    function: "NewMuscleTestFunction",
-    exerciseIds: [4]
-  }
+  muscle: Muscle | undefined
+  muscleForm: FormGroup | null = null;
+  submitInvalidForm: boolean = false;
 
   @Input() isAdmin: boolean = false;
   @Output() newMuscleAdded: EventEmitter<Muscle> = new EventEmitter<Muscle>();
   @Output() errorOccurred: EventEmitter<GraphQLError> = new EventEmitter<GraphQLError>();
-  @Input() modalRef!: ElementRef | null;
 
   @ViewChild('btnClose') btnClose!: ElementRef
-
-  muscleForm: FormGroup | null = null;
-  submitInvalidForm: boolean = false;
 
   constructor(private apollo: Apollo, private ref: ElementRef) {
   }
 
+  @Input() set muscleInput(value: Muscle | undefined) {
+    this.muscle = value;
+    this.initializeMuscleForm();
+  }
+
   ngOnInit() {
+    this.initializeMuscleForm()
+  }
+
+  initializeMuscleForm() {
     const exerciseIdsValidator =
       this.isAdmin ? null : Validators.required
-    const muscleName = this.newMuscle ? this.newMuscle.name : "";
-    const muscleDescription = this.newMuscle ? this.newMuscle.description : "";
-    const muscleFunction = this.newMuscle ? this.newMuscle.function : "";
-    const muscleExerciseIds = this.newMuscle ? this.newMuscle.exerciseIds : [];
+    const muscleName = this.muscle ? this.muscle.name : "";
+    const muscleDescription = this.muscle ? this.muscle.description : "";
+    const muscleFunction = this.muscle ? this.muscle.function : "";
+    const muscleExerciseIds = this.muscle ?
+      this.muscle.exercises?.map(ex => ex.id) : [];
 
     this.muscleForm = new FormGroup({
       name: new FormControl(muscleName,
@@ -75,30 +74,32 @@ export class MuscleFormComponent implements OnInit {
       ),
     });
 
-    if (this.newMuscle)
-      this.muscleForm.addControl("id", new FormControl(this.newMuscle.id))
+    if (this.muscle)
+      this.muscleForm.addControl("id", new FormControl(this.muscle.id))
   }
 
   onSubmit() {
     if (!this.muscleForm) return
     if (this.muscleForm.valid) {
-      this.submitInvalidForm = false;
-      this.apollo
-        .mutate({
-          mutation: ADD_MUSCLES,
-          variables: {
-            inputNewMuscle: this.muscleForm.value,
-          },
-        })
-        .subscribe(({data, error}: any) => {
-          if (data) {
-            this.newMuscleAdded.emit(data.addMuscle)
-          }
-          if (error) {
-            this.errorOccurred.emit(error);
-          }
-        });
-      this.btnClose.nativeElement.click()
+      // this.submitInvalidForm = false;
+      // this.apollo
+      //   .mutate({
+      //     mutation: ADD_MUSCLES,
+      //     variables: {
+      //       inputNewMuscle: this.muscleForm.value,
+      //     },
+      //   })
+      //   .subscribe(({data, error}: any) => {
+      //     if (data) {
+      //       this.newMuscleAdded.emit(data.addMuscle)
+      //     }
+      //     if (error) {
+      //       this.errorOccurred.emit(error);
+      //     }
+      //   });
+      // this.btnClose.nativeElement.click()
+    } else {
+      this.submitInvalidForm = true
     }
   }
 }
