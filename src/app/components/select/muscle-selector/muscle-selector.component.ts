3063c9e8e238615e868614f1e614122f97778301
CptFranck <francois.caillet71@gmail.com>
2024-06-05 18:24:43 +0200

    Add muscle selector

---
+++ src/app/components/select/muscle-selector/muscle-selector.component.ts
@@ -0,0 +1,62 @@
+import {Component, forwardRef, inject, Input, OnInit} from '@angular/core';
+import {Option} from "../../../interface/multi-select/option";
+import {MultiSelectComponent} from "../multi-select/multi-select.component";
+import {MuscleService} from "../../../services/muscle/muscle.service";
+import {Muscle} from "../../../interface/dto/muscle";
+import {ControlValueAccessor, NG_VALUE_ACCESSOR} from "@angular/forms";
+
+@Component({
+  selector: 'app-muscle-selector',
+  standalone: true,
+  imports: [
+    MultiSelectComponent,
+  ],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => MuscleSelectorComponent),
+      multi: true,
+    }
+  ],
+  templateUrl: './muscle-selector.component.html',
+})
+export class MuscleSelectorComponent implements OnInit, ControlValueAccessor {
+  error: any;
+  muscles: Option[] = [];
+  @Input() muscleIds: number[] = [];
+  muscleService: MuscleService = inject(MuscleService);
+
+  onChange: (value: number[]) => void = () => {
+  };
+
+  onTouched: ($event: boolean) => void = () => {
+  };
+
+  ngOnInit(): void {
+    this.muscleService.getMuscles().subscribe(({data, error}: any) => {
+      let options: Option[] = []
+      data.getMuscles.forEach((muscle: Muscle) => {
+        options.push({id: muscle.id, title: muscle.name, value: muscle, description: muscle.description});
+      });
+      this.muscles = [...options];
+      this.error = error;
+    });
+  }
+
+  writeValue(muscleIds: number[]): void {
+    this.muscleIds = muscleIds;
+  }
+
+  registerOnChange(fn: (value: number[]) => void): void {
+    this.onChange = fn
+  }
+
+  registerOnTouched(fn: () => void): void {
+    this.onTouched = fn
+  }
+
+  setExerciseIds(muscleIds: number[]) {
+    this.muscleIds = muscleIds;
+    this.onChange(muscleIds);
+  }
+}
