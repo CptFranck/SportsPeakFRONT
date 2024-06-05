1829da97dfc682009a91763d6147823c0e7d960f
CptFranck <francois.caillet71@gmail.com>
2024-06-05 13:22:34 +0200

    Refactoring graphQL provider with error handling

--- src/app/graphql/graphql.provider.ts
+++ src/app/graphql/graphql.provider.ts
@@ -2,13 +2,27 @@ import {Apollo, APOLLO_OPTIONS} from 'apollo-angular';
 import {HttpLink} from 'apollo-angular/http';
 import {ApplicationConfig, inject} from '@angular/core';
 import {ApolloClientOptions, ApolloLink, InMemoryCache} from '@apollo/client/core';
-import {errorLinkHandler} from "./on-error";
+// import {errorLinkHandler} from "./on-error";
+import {AlertService} from "../services/alert/alert.service";
+import {onError} from "@apollo/client/link/error";
 
 // URL of the GraphQL server here
 const uri = 'http://localhost:8080/service/api/graphql';
 
 function apolloOptionsFactory(): ApolloClientOptions<any> {
   const httpLink = inject(HttpLink);
+  const alertService = inject(AlertService)
+
+  const errorLinkHandler = onError(({graphQLErrors, networkError}) => {
+    if (graphQLErrors)
+      graphQLErrors.map((graphQlError) =>
+        alertService.createApolloGraphQLErrorAlert(graphQlError)
+      );
+
+    if (networkError)
+      alertService.createApolloNetWorkErrorAlert(networkError)
+  });
+
   const HttpLinkHandler = httpLink.create({uri})
   return {
     link: ApolloLink.from([
