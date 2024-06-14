41a32ae93245696e329c7375e33e883dab45afec
CptFranck <francois.caillet71@gmail.com>
2024-06-14 10:55:22 +0200

    Add login component to route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,8 +2,10 @@ import {Routes} from '@angular/router';
 import {ExercisesComponent} from './page/exercises/exercises/exercises.component';
 import {ExerciseTypesComponent} from './page/exercise-types/exercise-types/exercise-types.component';
 import {MusclesComponent} from './page/muscle/muscles/muscles.component';
+import {LoginComponent} from "./page/auth/login/login.component";
 
 export const routes: Routes = [
+  {path: 'login', component: LoginComponent},
   {path: 'exercises', component: ExercisesComponent},
   {path: 'exercise-types', component: ExerciseTypesComponent},
   {path: 'muscles', component: MusclesComponent},
