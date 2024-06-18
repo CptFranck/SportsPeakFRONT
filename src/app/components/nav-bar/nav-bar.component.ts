0bb473491b0db2195f9a19dbe1d18d4c3feb3203
CptFranck <francois.caillet71@gmail.com>
2024-06-18 16:53:22 +0200

    Update navbar when JWT received

--- src/app/components/nav-bar/nav-bar.component.ts
+++ src/app/components/nav-bar/nav-bar.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import {Component, inject, OnInit} from '@angular/core';
+import {RouterModule} from '@angular/router';
+import {AuthService} from "../../services/auth/auth.service";
+import {NgIf} from "@angular/common";
 
 @Component({
   selector: 'app-nav-bar',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, NgIf],
   templateUrl: './nav-bar.component.html',
 })
-export class NavBarComponent {}
+export class NavBarComponent implements OnInit {
+  isLogged: boolean = false;
+  authService: AuthService = inject(AuthService);
+
+  ngOnInit() {
+    this.authService.isAuthenticated.subscribe(value => {
+      this.isLogged = value
+      console.log("isLogged", this.isLogged)
+    });
+  }
+
+  logout() {
+    this.authService.logout();
+  }
+}
