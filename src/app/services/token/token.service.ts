5163123e0e9746e01c25fa3275948578ebff2ca5
CptFranck <francois.caillet71@gmail.com>
2024-06-20 17:19:30 +0200

    Add AuthToken service

---
+++ src/app/services/token/token.service.ts
@@ -0,0 +1,28 @@
+import {inject, Injectable} from '@angular/core';
+import {BehaviorSubject} from "rxjs";
+import {LocalStorageService} from "../localStorage/local-storage.service";
+import {AuthToken} from "../../interface/dto/token";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TokenService {
+
+  authToken: BehaviorSubject<AuthToken | null> = new BehaviorSubject<AuthToken | null>(null);
+  localStorageService: LocalStorageService = inject(LocalStorageService);
+
+  getCurrentToken() {
+    return this.authToken.value;
+  }
+
+  setCurrentToken(authToken: AuthToken) {
+    this.authToken.next(authToken);
+    let authTokenJson = JSON.stringify(authToken);
+    this.localStorageService.saveData("authToken", authTokenJson);
+  }
+
+  removeCurrentToken() {
+    this.authToken.next(null);
+    this.localStorageService.removeData("authToken");
+  }
+}
