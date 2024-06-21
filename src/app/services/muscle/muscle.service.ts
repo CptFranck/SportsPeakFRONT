dc06d44c10d2c0fb8aeb9ded742815ab81b9fe93
CptFranck <francois.caillet71@gmail.com>
2024-06-21 12:27:06 +0200

    Modify muscle service

--- src/app/services/muscle/muscle.service.ts
+++ src/app/services/muscle/muscle.service.ts
@@ -2,17 +2,35 @@ import {inject, Injectable} from '@angular/core';
 import {Apollo} from "apollo-angular";
 import {ADD_MUSCLE, DEL_MUSCLE, GET_MUSCLES, MOD_MUSCLE} from "../../graphql/operations/muscle/muscle.operations";
 import {FormGroup} from "@angular/forms";
+import {ApolloQueryResult} from "@apollo/client";
+import {BehaviorSubject} from "rxjs";
+import {Muscle} from "../../interface/dto/muscle";
+import {AlertService} from "../alert/alert.service";
 
 @Injectable({
   providedIn: 'root'
 })
 export class MuscleService {
+
+  muscles: BehaviorSubject<Muscle[]> = new BehaviorSubject<Muscle[]>([]);
+  isLoading: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
   private apollo: Apollo = inject(Apollo);
+  private alertService: AlertService = inject(AlertService);
+
+  constructor() {
+    this.getMuscles();
+  }
 
   getMuscles() {
     return this.apollo.watchQuery({
       query: GET_MUSCLES,
-    }).valueChanges;
+    }).valueChanges.subscribe((result: ApolloQueryResult<any>): void => {
+      if (result.errors) {
+        result.errors.map(err => this.alertService.createGraphQLErrorAlert(err))
+      }
+      this.muscles.next(result.data.getMuscles);
+      this.isLoading.next(result.loading);
+    });
   }
 
   addMuscle(muscleForm: FormGroup) {
