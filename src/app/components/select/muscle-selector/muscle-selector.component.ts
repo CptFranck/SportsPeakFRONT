c77c02f5511f11e0b5ed7d5861b5438a076f3c81
CptFranck <francois.caillet71@gmail.com>
2024-06-21 12:26:23 +0200

    Modify muscle selector to handle loading options

--- src/app/components/select/muscle-selector/muscle-selector.component.ts
+++ src/app/components/select/muscle-selector/muscle-selector.component.ts
@@ -4,8 +4,6 @@ import {MultiSelectComponent} from "../multi-select/multi-select.component";
 import {MuscleService} from "../../../services/muscle/muscle.service";
 import {Muscle} from "../../../interface/dto/muscle";
 import {ControlValueAccessor, NG_VALUE_ACCESSOR} from "@angular/forms";
-import {ApolloQueryResult} from "@apollo/client";
-import {AlertService} from "../../../services/alert/alert.service";
 
 @Component({
   selector: 'app-muscle-selector',
@@ -23,10 +21,9 @@ import {AlertService} from "../../../services/alert/alert.service";
   templateUrl: './muscle-selector.component.html',
 })
 export class MuscleSelectorComponent implements OnInit, ControlValueAccessor {
-  error: any;
-  muscles: Option[] = [];
+  loading: boolean = true;
+  muscleOptions: Option[] = [];
   @Input() muscleIds: number[] = [];
-  alertService: AlertService = inject(AlertService);
   muscleService: MuscleService = inject(MuscleService);
 
   onChange: (value: number[]) => void = () => {
@@ -36,22 +33,19 @@ export class MuscleSelectorComponent implements OnInit, ControlValueAccessor {
   };
 
   ngOnInit(): void {
-    this.muscleService.getMuscles().subscribe((result: ApolloQueryResult<any>): void => {
-      if (result.errors) {
-        result.errors.map(err => this.alertService.createGraphQLErrorAlert(err))
-      } else {
-        let options: Option[] = []
-        result.data.getMuscles.forEach((muscle: Muscle) => {
-          options.push({
-            id: muscle.id,
-            title: muscle.name,
-            value: muscle,
-            description: muscle.description
-          });
+    this.muscleService.muscles.subscribe(muscles => {
+      let options: Option[] = []
+      muscles.forEach((muscle: Muscle) => {
+        options.push({
+          id: muscle.id,
+          title: muscle.name,
+          value: muscle,
+          description: muscle.description
         });
-        this.muscles = [...options];
-      }
+      });
+      this.muscleOptions = [...options];
     });
+    this.muscleService.isLoading.subscribe(value => this.loading = value);
   }
 
   writeValue(muscleIds: number[]): void {
