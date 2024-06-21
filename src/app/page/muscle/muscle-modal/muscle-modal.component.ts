4c3add0ad92488c6d08f2576a244a7ed80ecbb2e
CptFranck <francois.caillet71@gmail.com>
2024-06-21 13:21:17 +0200

    Refactor muscle modal

--- src/app/page/muscle/muscle-modal/muscle-modal.component.ts
+++ src/app/page/muscle/muscle-modal/muscle-modal.component.ts
@@ -1,4 +1,4 @@
-import {Component, inject, Input, TemplateRef, ViewChild} from '@angular/core';
+import {Component, Input, TemplateRef, ViewChild} from '@angular/core';
 import {ActionType} from "../../../enum/action-type";
 import {ModalButtonComponent} from "../../../components/modal/modal-button/modal-button.component";
 import {ModalComponent} from "../../../components/modal/modal/modal.component";
@@ -9,8 +9,6 @@ import {MuscleEntityFormComponent} from "../muscle-modal-components/muscle-entit
 import {NgIf} from "@angular/common";
 import {muscleDeleteFormComponent} from "../muscle-modal-components/muscle-delete-form/muscle-delete-form.component";
 import {Muscle} from "../../../interface/dto/muscle";
-import {GraphQLError} from "graphql/error";
-import {AlertService} from "../../../services/alert/alert.service";
 
 @Component({
   selector: 'app-muscle-modal',
@@ -31,33 +29,14 @@ export class MuscleModalComponent {
   @Input() muscleModalId!: string;
   @Input() muscle: Muscle | undefined;
   @Input() action!: ActionType;
+
   @ViewChild("modalTemplate") modalTemplate!: TemplateRef<any>
 
-  alertService: AlertService = inject(AlertService);
   protected readonly ActionType = ActionType;
 
   onClick(value: undefined) {
     this.muscle = value
     this.action = ActionType.create
     this.modalTitle = "Add new muscle";
-  }
-
-  setAlertError(graphQLError: GraphQLError) {
-    this.alertService.createGraphQLErrorAlert(graphQLError);
-  }
-
-  setAlertSuccessAdded(muscle: Muscle) {
-    let message = "Muscle " + muscle.name + " been successfully created."
-    this.alertService.addSuccessAlert(message);
-  }
-
-  setAlertSuccessUpdated(muscle: Muscle) {
-    let message = "Muscle " + muscle.name + " has been successfully updated."
-    this.alertService.addSuccessAlert(message);
-  }
-
-  setAlertSuccessDeleted(muscle: Muscle) {
-    let message = "Muscle " + muscle.name + " has been successfully deleted."
-    this.alertService.addSuccessAlert(message);
   }
 }
