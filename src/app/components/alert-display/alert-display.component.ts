3ee2e23d9f365b35b28cd0a80cd6ad44510afc8f
CptFranck <francois.caillet71@gmail.com>
2024-06-21 16:40:16 +0200

    Refactor alert display component

--- src/app/components/alert-display/alert-display.component.ts
+++ src/app/components/alert-display/alert-display.component.ts
@@ -14,36 +14,17 @@ import {AlertService} from "../../services/alert/alert.service";
   templateUrl: './alert-display.component.html',
 })
 export class AlertDisplayComponent {
-  alerts!: Alert[]
-  // alerts: Alert[]
-  //   = [{
-  //   "id": 0,
-  //   "title": "Unsuccessful operation :(",
-  //   "message": "org.springframework.dao.DataIntegrityViolationException: could not execute statement [ERROR: update or delete on table \"exercise_type\" violates foreign key constraint \"fk6jy6x1k4t53mwwrlbhqg7f18l\" on table \"classified_exercise\"\n  Détail : Key (id)=(7) is still referenced from table \"classified_exercise\".] [delete from exercise_type where id=?]; SQL [delete from exercise_type where id=?]; constraint [fk6jy6x1k4t53mwwrlbhqg7f18l]",
-  //   "type": AlertType.error,
-  //   "closed": false,
-  //   "errorInformation": {
-  //     "errorType": AlertErrorType.GraphQLError,
-  //     "errorLocation": [
-  //       {
-  //         "line": 2,
-  //         "column": 3
-  //       }
-  //     ],
-  //     "errorPath": [
-  //       "deleteExerciseType"
-  //     ]
-  //   }
-  // }];
-  alertService: AlertService = inject(AlertService);
+  alerts: Alert[] = [];
+
+  private alertService: AlertService = inject(AlertService);
 
   constructor() {
-    this.alertService.getAlertsSubject().subscribe(als => {
-      this.alerts = als;
+    this.alertService.getAlertsSubject().subscribe((alerts: Alert[]) => {
+      this.alerts = alerts;
     })
   }
 
   removeAlert(alert: Alert) {
-    this.alertService.closeAlert(alert)
+    this.alertService.closeAlert(alert);
   }
 }
