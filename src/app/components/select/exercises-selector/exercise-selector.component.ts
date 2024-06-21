3c6d9760e27144eed7771511248beafd466acad8
CptFranck <francois.caillet71@gmail.com>
2024-06-21 15:15:03 +0200

    Refactor exercise selector

--- src/app/components/select/exercises-selector/exercise-selector.component.ts
+++ src/app/components/select/exercises-selector/exercise-selector.component.ts
@@ -4,8 +4,6 @@ import {Exercise} from "../../../interface/dto/exercise";
 import {Option} from "../../../interface/multi-select/option";
 import {ControlValueAccessor, NG_VALUE_ACCESSOR} from "@angular/forms";
 import {ExerciseService} from "../../../services/exercise/exercise.service";
-import {ApolloQueryResult} from "@apollo/client";
-import {AlertService} from "../../../services/alert/alert.service";
 
 @Component({
   selector: 'app-exercise-selector',
@@ -23,10 +21,9 @@ import {AlertService} from "../../../services/alert/alert.service";
   templateUrl: './exercise-selector.component.html',
 })
 export class ExerciseSelectorComponent implements OnInit, ControlValueAccessor {
-  error: any;
-  exercises: Option[] = [];
+  loading: boolean = true;
+  exerciseOptions: Option[] = [];
   @Input() exerciseIds: number[] = [];
-  alertService: AlertService = inject(AlertService);
   exerciseService: ExerciseService = inject(ExerciseService);
 
   onChange: (value: number[]) => void = () => {
@@ -36,22 +33,19 @@ export class ExerciseSelectorComponent implements OnInit, ControlValueAccessor {
   };
 
   ngOnInit(): void {
-    this.exerciseService.getExercises().subscribe((result: ApolloQueryResult<any>): void => {
-      if (result.errors) {
-        result.errors.map(err => this.alertService.createGraphQLErrorAlert(err))
-      } else {
-        let options: Option[] = []
-        result.data.getExercises.forEach((exercise: Exercise) => {
-          options.push({
-            id: exercise.id,
-            title: exercise.name,
-            value: exercise,
-            description: exercise.description
-          });
+    this.exerciseService.exercises.subscribe((exercises: Exercise[]) => {
+      let options: Option[] = []
+      exercises.forEach((exercise: Exercise) => {
+        options.push({
+          id: exercise.id,
+          title: exercise.name,
+          value: exercise,
+          description: exercise.description
         });
-        this.exercises = [...options];
-      }
+      });
+      this.exerciseOptions = [...options];
     });
+    this.exerciseService.isLoading.subscribe(value => this.loading = value);
   }
 
   writeValue(exerciseIds: number[]): void {
