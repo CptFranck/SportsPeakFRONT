24b58471d0e369befd2a34de7e0983020ae63c67
CptFranck <francois.caillet71@gmail.com>
2024-06-21 17:41:37 +0200

    Update nav bar component

--- src/app/components/nav-bar/nav-bar.component.ts
+++ src/app/components/nav-bar/nav-bar.component.ts
@@ -3,13 +3,17 @@ import {RouterModule} from '@angular/router';
 import {NgForOf, NgIf} from "@angular/common";
 import {NavBarLoginComponent} from "../nav-bar-element/nav-bar-login/nav-bar-login.component";
 import {NavBarUserMenuComponent} from "../nav-bar-element/nav-bar-user-menu/nav-bar-user-menu.component";
+import {
+  NavBarLanguageSelectorComponent
+} from "../nav-bar-element/nav-bar-language-selector/nav-bar-language-selector.component";
+import {NavBarAdminMenuComponent} from "../nav-bar-element/nav-bar-admin-menu/nav-bar-admin-menu.component";
+import {NavBarMainMenuComponent} from "../nav-bar-element/nav-bar-main-menu/nav-bar-main-menu.component";
 
 @Component({
   selector: 'app-nav-bar',
   standalone: true,
-  imports: [RouterModule, NgIf, NgForOf, NavBarLoginComponent, NavBarUserMenuComponent],
+  imports: [RouterModule, NgIf, NgForOf, NavBarLoginComponent, NavBarUserMenuComponent, NavBarLanguageSelectorComponent, NavBarAdminMenuComponent, NavBarMainMenuComponent],
   templateUrl: './nav-bar.component.html',
 })
 export class NavBarComponent {
-
 }
