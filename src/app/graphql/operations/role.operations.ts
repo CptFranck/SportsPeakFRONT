fc10cc8b5ca7e26951c349de85ff2a6e1a441b32
CptFranck <francois.caillet71@gmail.com>
2024-06-24 17:52:10 +0200

    Add role query & mutation

---
+++ src/app/graphql/operations/role.operations.ts
@@ -0,0 +1,67 @@
+import {gql} from "apollo-angular";
+
+export const GET_ROLES = gql`
+  query {
+    getRoles {
+      id
+      name
+      users {
+        id
+        email
+        firstName
+        lastName
+        username
+      }
+      privileges {
+        id
+        name
+      }
+    }
+  }
+`;
+
+export const ADD_ROLES = gql`
+  mutation ($inputRole : InputRole!){
+    addRole(inputNewRole: $inputNewRole) {
+      id
+      name
+      users {
+        id
+        email
+        firstName
+        lastName
+        username
+      }
+      privileges {
+        id
+        name
+      }
+    }
+  }
+`;
+
+export const MOD_ROLES = gql`
+  mutation ($inputRole : InputRole!){
+    modifyRole(inputRole: $inputRole) {
+      id
+      name
+      users {
+        id
+        email
+        firstName
+        lastName
+        username
+      }
+      privileges {
+        id
+        name
+      }
+    }
+  }
+`;
+
+export const DEL_ROLES = gql`
+  mutation ($roleId : Int!){
+    deleteRole(roleId: $roleId)
+  }
+`;
