70fbf4fc3a6c01bda58e2ae1e427361b94169a0f
CptFranck <francois.caillet71@gmail.com>
2024-06-24 12:56:52 +0200

    Add addUser query

--- src/app/graphql/operations/user.operations.ts
+++ src/app/graphql/operations/user.operations.ts
@@ -13,6 +13,25 @@ export const GET_USERS = gql`
   }
 `;
 
+export const ADD_USER = gql`
+  mutation ($inputNewUser : InputNewUser!) {
+    addUser(inputNewUser: $inputNewUser){
+      id
+      email
+      firstName
+      lastName
+      username
+      roles {
+        id
+        name
+        privileges {
+          id
+          name
+        }
+      }
+    }
+  }`;
+
 export const MOD_USER = gql`
   mutation ($inputUser : InputUser!){
     modifyUser(inputUser: $inputUser) {
