b36c98d9d11aad3aec4747b37e1be0560d53bcce
CptFranck <francois.caillet71@gmail.com>
2024-06-24 16:02:49 +0200

    Add user array component

---
+++ src/app/page/userManagement/users-array/users-array.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { UsersArrayComponent } from './users-array.component';
+
+describe('UsersArrayComponent', () => {
+  let component: UsersArrayComponent;
+  let fixture: ComponentFixture<UsersArrayComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [UsersArrayComponent]
+    })
+    .compileComponents();
+    
+    fixture = TestBed.createComponent(UsersArrayComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/page/userManagement/users-array/users-array.component.ts
@@ -0,0 +1,44 @@
+import {Component, EventEmitter, Input, Output} from '@angular/core';
+import {FormIndicator} from "../../../interface/utils/form-indicator";
+import {ActionType} from "../../../enum/action-type";
+import {User} from "../../../interface/dto/user";
+import {ModalButtonComponent} from "../../../components/modal/modal-button/modal-button.component";
+import {NgForOf, NgIf} from "@angular/common";
+
+@Component({
+  selector: 'app-users-array',
+  standalone: true,
+  imports: [
+    ModalButtonComponent,
+    NgForOf,
+    NgIf
+  ],
+  templateUrl: './users-array.component.html',
+})
+export class UsersArrayComponent {
+  @Input() users!: User[];
+  @Input() modalId!: string;
+
+  @Output() actionUser: EventEmitter<FormIndicator> = new EventEmitter<FormIndicator>();
+
+  showUserDetails(user: User): void {
+    this.actionUser.emit({
+      actionType: ActionType.read,
+      object: user
+    });
+  }
+
+  modifyUser(user: User) {
+    this.actionUser.emit({
+      actionType: ActionType.update,
+      object: user
+    });
+  }
+
+  delUser(user: User) {
+    this.actionUser.emit({
+      actionType: ActionType.delete,
+      object: user
+    });
+  }
+}
