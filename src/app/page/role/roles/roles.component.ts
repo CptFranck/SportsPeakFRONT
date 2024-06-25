1512eb3f4fd70317a50af5d718f28bcd40fc36d8
CptFranck <francois.caillet71@gmail.com>
2024-06-25 15:38:53 +0200

    add roles component

---
+++ src/app/page/role/roles/roles.component.ts
@@ -0,0 +1,46 @@
+import {Component, inject, OnInit, TemplateRef, ViewChild} from '@angular/core';
+import {ActionType} from "../../../enum/action-type";
+import {FormIndicator} from "../../../interface/utils/form-indicator";
+import {Role} from "../../../interface/dto/role";
+import {RoleService} from "../../../services/role/role.service";
+import {LoadingComponent} from "../../../components/loading/loading.component";
+import {MuscleModalComponent} from "../../muscle/muscle-modal/muscle-modal.component";
+import {MusclesArrayComponent} from "../../muscle/muscles-array/muscles-array.component";
+import {RolesArrayComponent} from "../roles-array/roles-array.component";
+import {RoleModalComponent} from "../role-modal/role-modal.component";
+
+@Component({
+  selector: 'app-roles',
+  standalone: true,
+  imports: [
+    LoadingComponent,
+    MuscleModalComponent,
+    MusclesArrayComponent,
+    RolesArrayComponent,
+    RoleModalComponent
+  ],
+  templateUrl: './roles.component.html',
+})
+export class RolesComponent implements OnInit {
+  loading: boolean = true;
+  roles: Role[] = [];
+  role: Role | undefined;
+  action: ActionType = ActionType.create;
+  modalTitle: string = "";
+  muscleModalId: string = "roleModal";
+  @ViewChild("modalTemplate") modalTemplate!: TemplateRef<any>;
+
+  private roleService: RoleService = inject(RoleService);
+
+  ngOnInit(): void {
+    this.roleService.roles.subscribe((roles: Role[]) => this.roles = roles);
+    this.roleService.isLoading.subscribe((isLoading: boolean) => this.loading = isLoading);
+  }
+
+  setRole(formIndicator: FormIndicator) {
+    this.role = formIndicator.object;
+    this.action = formIndicator.actionType;
+    this.modalTitle = formIndicator.object.name;
+  }
+
+}
