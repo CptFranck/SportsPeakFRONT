aaf2d9cc70ce93985a0b632f3e4f789a782a0a4e
CptFranck <francois.caillet71@gmail.com>
2024-06-25 15:20:03 +0200

    Add roles and privileges routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,8 @@ import {ExerciseTypesComponent} from './page/exercise-types/exercise-types/exerc
 import {MusclesComponent} from './page/muscle/muscles/muscles.component';
 import {LoginComponent} from "./page/auth/login/login.component";
 import {UsersComponent} from "./page/userManagement/users/users.component";
+import {RolesComponent} from "./page/role/roles/roles.component";
+import {PrivilegesComponent} from "./page/privilege/privileges/privileges.component";
 
 export const routes: Routes = [
   {path: 'login', component: LoginComponent},
@@ -11,5 +13,7 @@ export const routes: Routes = [
   {path: 'exercise-types', component: ExerciseTypesComponent},
   {path: 'muscles', component: MusclesComponent},
   {path: 'users', component: UsersComponent},
+  {path: 'roles', component: RolesComponent},
+  {path: 'privileges', component: PrivilegesComponent},
   {path: '', redirectTo: '/muscles', pathMatch: 'full'},
 ];
