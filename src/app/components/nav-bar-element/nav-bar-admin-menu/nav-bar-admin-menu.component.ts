989cb6a3f238cf162381ae491184f32632acc1b3
CptFranck <francois.caillet71@gmail.com>
2024-06-25 13:07:55 +0200

    Fix admin showing when not logged

--- src/app/components/nav-bar-element/nav-bar-admin-menu/nav-bar-admin-menu.component.ts
+++ src/app/components/nav-bar-element/nav-bar-admin-menu/nav-bar-admin-menu.component.ts
@@ -2,13 +2,15 @@ import {Component, inject, OnInit} from '@angular/core';
 import {User} from "../../../interface/dto/user";
 import {UserLoggedService} from "../../../services/userLogged/user-logged.service";
 import {RouterLink, RouterLinkActive} from "@angular/router";
+import {NgIf} from "@angular/common";
 
 @Component({
   selector: 'app-nav-bar-admin-menu',
   standalone: true,
   imports: [
     RouterLink,
-    RouterLinkActive
+    RouterLinkActive,
+    NgIf
   ],
   templateUrl: './nav-bar-admin-menu.component.html',
 })
