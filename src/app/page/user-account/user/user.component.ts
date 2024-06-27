3bb78633c4088f123ec39a26fadc044b3ae5db20
CptFranck <francois.caillet71@gmail.com>
2024-06-27 11:52:54 +0200

    Modify user component to add modification field

--- src/app/page/user-account/user/user.component.ts
+++ src/app/page/user-account/user/user.component.ts
@@ -6,7 +6,6 @@ import {NgIf} from "@angular/common";
 import {
   UserDetailsDisplayComponent
 } from "../../user-management/user-modal-components/user-details-display/user-details-display.component";
-import {UserService} from "../../../services/user/user.service";
 import {ModalComponent} from "../../../components/modal/modal/modal.component";
 import {UserInformationDisplayComponent} from "../user-information-display/user-information-display.component";
 import {
@@ -16,6 +15,8 @@ import {
   UserRolesFormComponent
 } from "../../user-management/user-modal-components/user-entity-form/user-roles-form.component";
 import {UserModalComponent} from "../user-modal/user-modal.component";
+import {FormIndicator} from "../../../interface/utils/form-indicator";
+import {ModificationField} from "../../../enum/modification-field";
 
 @Component({
   selector: 'app-user',
@@ -35,17 +36,34 @@ export class UserComponent {
 
   user: User | undefined = undefined;
   action: ActionType = ActionType.update;
+  modificationField: ModificationField = ModificationField.username;
   modalTemplate: TemplateRef<any> | undefined;
   userModalId: string = "userModal";
   modalTitle: string = "Update User";
 
   protected readonly ActionType = ActionType;
-  private userService: UserService = inject(UserService);
   private userLoggedService: UserLoggedService = inject(UserLoggedService);
 
   constructor() {
     this.userLoggedService.currentUser.subscribe((user: User | undefined) => {
       this.user = user;
     })
+  }
+
+  setUser(formIndicator: FormIndicator) {
+    this.action = formIndicator.actionType;
+    switch (formIndicator.modificationField) {
+      case ModificationField.username:
+        this.modalTitle = formIndicator.object.username;
+        return;
+      case ModificationField.email:
+        this.modalTitle = formIndicator.object.email;
+        return;
+      case ModificationField.name:
+        this.modalTitle = formIndicator.object.firstName + " " + formIndicator.object.lastName;
+        return;
+      default:
+        return;
+    }
   }
 }
