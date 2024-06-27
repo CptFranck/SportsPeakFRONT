9e478f2cac351b7165a3ca7983ed0d278b70f44d
CptFranck <francois.caillet71@gmail.com>
2024-06-27 10:54:42 +0200

    Update form indicator

--- src/app/interface/utils/form-indicator.ts
+++ src/app/interface/utils/form-indicator.ts
@@ -1,6 +1,8 @@
 import {ActionType} from "../../enum/action-type";
+import {ModificationField} from "../../enum/modification-field";
 
 export interface FormIndicator {
   actionType: ActionType,
+  modificationField?: ModificationField,
   object: any
 }
