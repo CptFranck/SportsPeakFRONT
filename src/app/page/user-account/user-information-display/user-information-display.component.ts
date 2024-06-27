c6951a728dc86839ae2958e5c819e4f0e6d02c5c
CptFranck <francois.caillet71@gmail.com>
2024-06-27 11:51:34 +0200

    Modify button on user information display component

--- src/app/page/user-account/user-information-display/user-information-display.component.ts
+++ src/app/page/user-account/user-information-display/user-information-display.component.ts
@@ -1,23 +1,48 @@
-import {Component, Input} from '@angular/core';
+import {Component, EventEmitter, Input, Output} from '@angular/core';
 import {User} from "../../../interface/dto/user";
+import {FormIndicator} from "../../../interface/utils/form-indicator";
+import {ActionType} from "../../../enum/action-type";
+import {ModificationField} from "../../../enum/modification-field";
+import {ModalButtonComponent} from "../../../components/modal/modal-button/modal-button.component";
 
 @Component({
   selector: 'app-user-information-display',
   standalone: true,
-  imports: [],
+  imports: [
+    ModalButtonComponent
+  ],
   templateUrl: './user-information-display.component.html',
 })
 export class UserInformationDisplayComponent {
 
+  btnClass: string = "btn btn-secondary"
+
   @Input() user!: User | undefined;
   @Input() modalId!: string;
 
+  @Output() userAction: EventEmitter<FormIndicator> = new EventEmitter<FormIndicator>();
+
   onChangeEmail() {
+    this.userAction.emit({
+      actionType: ActionType.update,
+      modificationField: ModificationField.email,
+      object: this.user
+    });
   }
 
   onChangeUsername() {
+    this.userAction.emit({
+      actionType: ActionType.update,
+      modificationField: ModificationField.username,
+      object: this.user
+    });
   }
 
   onChangeName() {
+    this.userAction.emit({
+      actionType: ActionType.update,
+      modificationField: ModificationField.name,
+      object: this.user
+    });
   }
 }
