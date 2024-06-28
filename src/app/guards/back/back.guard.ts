82803a110257da2748aea2301eeed1ed98f74900
CptFranck <francois.caillet71@gmail.com>
2024-06-28 16:04:28 +0200

    Refactor back guard

--- src/app/guards/back/back.guard.ts
+++ src/app/guards/back/back.guard.ts
@@ -1,21 +1,14 @@
 import {CanActivateFn, Router} from '@angular/router';
 import {inject} from "@angular/core";
 import {UserLoggedService} from "../../services/userLogged/user-logged.service";
-import {AuthService} from "../../services/auth/auth.service";
 
 export const BackGuard: CanActivateFn = () => {
   const router: Router = inject(Router);
-  const authService: AuthService = inject(AuthService);
   const userLoggedService: UserLoggedService = inject(UserLoggedService)
-
-  if (!authService.isAuthenticated.value) {
-    router.navigate(['auth']);
-    return false;
-  }
 
   if (userLoggedService.isStaff())
     return true;
 
-  router.navigate(['error/access-denied']);
+  router.navigate(['/error/access-denied']);
   return false;
 };
