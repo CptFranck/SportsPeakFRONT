52e942462f7e408242f7985bf09dd1ef15cadaf6
CptFranck <francois.caillet71@gmail.com>
2024-06-28 12:44:57 +0200

    Update project routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,20 +2,33 @@ import {Routes} from '@angular/router';
 import {ExercisesComponent} from './page/exercises/exercises/exercises.component';
 import {ExerciseTypesComponent} from './page/exercise-types/exercise-types/exercise-types.component';
 import {MusclesComponent} from './page/muscle/muscles/muscles.component';
-import {LoginComponent} from "./page/auth/login/login.component";
-import {UsersComponent} from "./page/user-management/users/users.component";
-import {RolesComponent} from "./page/role/roles/roles.component";
-import {PrivilegesComponent} from "./page/privilege/privileges/privileges.component";
+import {AuthComponent} from "./page/auth/auth/auth.component";
 import {UserComponent} from "./page/user-account/user/user.component";
+import {AuthGuard} from "./guards/auth/authGuard";
+import {BackGuard} from "./guards/back/back.guard";
 
 export const routes: Routes = [
-  {path: 'login', component: LoginComponent},
+  {path: '', redirectTo: '/muscles', pathMatch: 'full'},
+  {path: 'login', component: AuthComponent},
   {path: 'exercises', component: ExercisesComponent},
   {path: 'exercise-types', component: ExerciseTypesComponent},
   {path: 'muscles', component: MusclesComponent},
-  {path: 'user', component: UserComponent},
-  {path: 'users', component: UsersComponent},
-  {path: 'roles', component: RolesComponent},
-  {path: 'privileges', component: PrivilegesComponent},
-  {path: '', redirectTo: '/muscles', pathMatch: 'full'},
+  {
+    path: 'back',
+    loadChildren: () => import('./page/error/error.routes').then(value => value.ERROR_ROUTES),
+    canActivate: [AuthGuard, BackGuard]
+  },
+  {
+    path: 'user',
+    component: UserComponent,
+    canActivate: [AuthGuard]
+  },
+  {
+    path: 'error', loadChildren: () => import('./page/error/error.routes').then(value => value.ERROR_ROUTES)
+  },
+  {
+    path: '**',
+    redirectTo: 'error/page-not-found',
+    pathMatch: 'full'
+  },
 ];
