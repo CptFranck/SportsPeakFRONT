31df8eaeb7648b4f0e1cc9d0c82aa5e0394e83da
CptFranck <francois.caillet71@gmail.com>
2024-07-01 16:57:05 +0200

    Add horizontal card component

---
+++ src/app/components/card/horizontal-card/horizontal-card.component.ts
@@ -0,0 +1,17 @@
+import {Component, Input} from '@angular/core';
+import {NgOptimizedImage} from "@angular/common";
+import {RouterLink} from "@angular/router";
+
+@Component({
+  selector: 'app-horizontal-card',
+  standalone: true,
+  imports: [
+    NgOptimizedImage,
+    RouterLink
+  ],
+  templateUrl: './horizontal-card.component.html',
+})
+export class HorizontalCardComponent {
+  @Input() title: string = "Card title";
+  @Input() srcImage: string = "...";
+}
