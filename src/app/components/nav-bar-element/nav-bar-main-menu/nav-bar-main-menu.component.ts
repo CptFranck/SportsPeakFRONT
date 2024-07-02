c9a4e6e20bc88f7669cf1941c1b09a070b9322d1
CptFranck <francois.caillet71@gmail.com>
2024-07-02 12:52:50 +0200

    Refactor nav bar menu

--- src/app/components/nav-bar-element/nav-bar-main-menu/nav-bar-main-menu.component.ts
+++ src/app/components/nav-bar-element/nav-bar-main-menu/nav-bar-main-menu.component.ts
@@ -1,12 +1,18 @@
 import {Component} from '@angular/core';
 import {RouterLink, RouterLinkActive} from "@angular/router";
+import {
+  NavBarExploreMenuComponent
+} from "../nav-bar-main-menu-components/nav-bar-explore-menu/nav-bar-explore-menu.component";
+import {NavBarDocMenuComponent} from "../nav-bar-main-menu-components/nav-bar-doc-menu/nav-bar-doc-menu.component";
 
 @Component({
   selector: 'app-nav-bar-main-menu',
   standalone: true,
   imports: [
     RouterLink,
-    RouterLinkActive
+    RouterLinkActive,
+    NavBarExploreMenuComponent,
+    NavBarDocMenuComponent
   ],
   templateUrl: './nav-bar-main-menu.component.html',
 })
