5f0273e487b3de9dc37934eb5055ae2f3fb44452
CptFranck <francois.caillet71@gmail.com>
2024-07-10 15:26:19 +0200

    move exercise modal

--- src/app/page/docs/exercises/exercise-modal/exercise-modal.component.ts
+++ src/app/page/docs/exercises/exercise-modal/exercise-modal.component.ts
@@ -3,14 +3,14 @@ import {ModalButtonComponent} from "../../../../components/modal/modal-button/mo
 import {ModalComponent} from "../../../../components/modal/modal/modal.component";
 import {
   MuscleDetailsDisplayComponent
-} from "../../../docs/muscles/muscle-modal-components/muscle-details-display/muscle-details-display.component";
+} from "../../muscles/muscle-modal-components/muscle-details-display/muscle-details-display.component";
 import {
   MuscleEntityFormComponent
-} from "../../../docs/muscles/muscle-modal-components/muscle-entity-form/muscle-entity-form.component";
+} from "../../muscles/muscle-modal-components/muscle-entity-form/muscle-entity-form.component";
 import {NgIf} from "@angular/common";
 import {
   muscleDeleteFormComponent
-} from "../../../docs/muscles/muscle-modal-components/muscle-delete-form/muscle-delete-form.component";
+} from "../../muscles/muscle-modal-components/muscle-delete-form/muscle-delete-form.component";
 import {ActionType} from "../../../../enum/action-type";
 import {Exercise} from "../../../../interface/dto/exercise";
 import {
@@ -22,7 +22,7 @@ import {
 import {
   ExerciseDetailsDisplayComponent
 } from "../exercise-modal-components/exercise-details-display/exercise-details-display.component";
-import {UserLoggedService} from "../../../../services/userLogged/user-logged.service";
+import {UserLoggedService} from "../../../../services/user-logged/user-logged.service";
 
 @Component({
   selector: 'app-exercise-modal',
