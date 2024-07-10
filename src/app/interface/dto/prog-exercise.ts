a3f478701feba3e9b0569747689831d7155db9db
CptFranck <francois.caillet71@gmail.com>
2024-07-10 15:57:43 +0200

    Update ProgExercise ts interface

--- src/app/interface/dto/prog-exercise.ts
+++ src/app/interface/dto/prog-exercise.ts
@@ -1,13 +1,15 @@
 import {User} from "./user";
 import {Exercise} from "./exercise";
 import {TargetSet} from "./target-set";
+import {Visibility} from "../enum/visibility";
+import {TrustLabel} from "../enum/trustLabel";
 
 export interface ProgExercise {
   id: string
   name: string
   note: string
-  visibility: string
-  trustLabel: string
+  visibility: Visibility
+  trustLabel: TrustLabel
   creator: User
   exercise: Exercise
   targetSets: TargetSet[]
