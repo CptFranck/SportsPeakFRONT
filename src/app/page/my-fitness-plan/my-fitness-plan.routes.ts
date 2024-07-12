25a1c4e5e30faad59504ff0075c5508ea1ec9ecb
CptFranck <francois.caillet71@gmail.com>
2024-07-12 16:14:49 +0200

    Add MyProgExercisesComponent t o routes

--- src/app/page/my-fitness-plan/my-fitness-plan.routes.ts
+++ src/app/page/my-fitness-plan/my-fitness-plan.routes.ts
@@ -1,11 +1,13 @@
 import {Routes} from "@angular/router";
 import {MyFitnessPlanComponent} from "./my-fitness-plan/my-fitness-plan.component";
+import {MyProgExercisesComponent} from "./my-prog-exercises/my-prog-exercises/my-prog-exercises.component";
 
 export const MY_FITNESS_PLAN_ROUTES: Routes = [
   {
     path: '',
     children: [
       {path: 'my-plan', component: MyFitnessPlanComponent},
+      {path: 'my-programed-exercises', component: MyProgExercisesComponent},
     ],
   }
 ]
