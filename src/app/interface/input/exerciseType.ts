16bd994daa8499a56ad3daeeec9ce543884a1912
CptFranck <francois.caillet71@gmail.com>
2024-07-12 15:34:33 +0200

    Delete unused ts interface (inputs)

--- src/app/interface/input/exerciseType.ts
+++
@@ -1,9 +0,0 @@
-export interface InputNewExerciseType {
-  name: string
-  goal: string
-  exerciseIs: number[]
-}
-
-export interface InputExerciseType extends InputNewExerciseType {
-  id: string
-}
