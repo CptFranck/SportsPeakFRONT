0328d49f0886fceb5726c708372fbe453fd150c0
CptFranck <francois.caillet71@gmail.com>
2024-07-12 15:35:27 +0200

    Fix select component for ControlValueAccessor

--- src/app/components/select/select.component.ts
+++ src/app/components/select/select.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input} from '@angular/core';
+import {Component, EventEmitter, Input, Output} from '@angular/core';
 import {SelectOption} from "../../interface/components/select/selectOption";
 import {NgForOf} from "@angular/common";
 
@@ -13,4 +13,21 @@ import {NgForOf} from "@angular/common";
 export class SelectComponent {
   @Input() value: string | undefined;
   @Input() options!: SelectOption[];
+
+  @Output() onTouched: EventEmitter<boolean> = new EventEmitter<boolean>();
+  @Output() onChange: EventEmitter<string | undefined> = new EventEmitter<string | undefined>();
+
+  onSelect(event: Event) {
+    if (event.target instanceof HTMLSelectElement) {
+      let value: string | undefined = event.target.value;
+      if (value === "undefined")
+        value = undefined;
+      this.value = value;
+      this.onChange.emit(value);
+    }
+  }
+
+  onClick() {
+    this.onTouched.emit(true);
+  }
 }
