0a8064ad9811414fabd264ab0f18b652741196b3
CptFranck <francois.caillet71@gmail.com>
2024-07-15 16:36:38 +0200

    Add new route for MyProgExerciseComponent component

--- src/app/page/my-fitness-plan/my-fitness-plan.routes.ts
+++ src/app/page/my-fitness-plan/my-fitness-plan.routes.ts
@@ -1,6 +1,7 @@
 import {Routes} from "@angular/router";
 import {MyFitnessPlanComponent} from "./my-fitness-plan/my-fitness-plan.component";
 import {MyProgExercisesComponent} from "./my-prog-exercises/my-prog-exercises/my-prog-exercises.component";
+import {MyProgExerciseComponent} from "./my-prog-exercise/my-prog-exercise/my-prog-exercise.component";
 
 export const MY_FITNESS_PLAN_ROUTES: Routes = [
   {
@@ -8,6 +9,7 @@ export const MY_FITNESS_PLAN_ROUTES: Routes = [
     children: [
       {path: 'my-plan', component: MyFitnessPlanComponent},
       {path: 'my-programed-exercises', component: MyProgExercisesComponent},
+      {path: 'my-programed-exercise/:id', component: MyProgExerciseComponent},
     ],
   }
 ]
