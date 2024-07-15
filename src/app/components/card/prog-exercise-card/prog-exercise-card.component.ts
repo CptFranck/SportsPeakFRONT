9e2a6f43be854caa29be6e238652b1143d98ef12
CptFranck <francois.caillet71@gmail.com>
2024-07-15 15:25:29 +0200

    Modify ProgExerciseCardComponent

--- src/app/components/card/prog-exercise-card/prog-exercise-card.component.ts
+++ src/app/components/card/prog-exercise-card/prog-exercise-card.component.ts
@@ -1,16 +1,52 @@
-import {Component, Input} from '@angular/core';
-import {NgForOf} from "@angular/common";
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {NgForOf, NgIf} from "@angular/common";
 import {ProgExercise} from "../../../interface/dto/prog-exercise";
+import {ModalButtonComponent} from "../../modal/modal-button/modal-button.component";
+import {ActionType} from "../../../enum/action-type";
+import {FormIndicator} from "../../../interface/utils/form-indicator";
+import {getProgExerciseTime, getTargetSetsInformation} from "../../../utils/prog-exercise-functions";
 
 @Component({
   selector: 'app-prog-exercise-card',
   standalone: true,
   imports: [
-    NgForOf
+    NgForOf,
+    ModalButtonComponent,
+    NgIf
   ],
   templateUrl: './prog-exercise-card.component.html',
 })
-export class ProgExerciseCardComponent {
-
+export class ProgExerciseCardComponent implements OnInit {
+  exerciseTime: string = "";
+  targetSets: string[] = [];
+  @Input() modalId!: string;
   @Input() progExercise!: ProgExercise;
+
+  @Output() actionProgExercises: EventEmitter<FormIndicator> = new EventEmitter<FormIndicator>();
+
+  ngOnInit() {
+    this.exerciseTime = getProgExerciseTime(this.progExercise);
+    this.targetSets = getTargetSetsInformation(this.progExercise);
+  }
+
+  showMuscleDetails(progExercise: ProgExercise): void {
+    this.actionProgExercises.emit({
+      actionType: ActionType.read,
+      object: progExercise
+    });
+  }
+
+  modifyMuscle(progExercise: ProgExercise) {
+    this.actionProgExercises.emit({
+      actionType: ActionType.update,
+      object: progExercise
+    });
+  }
+
+  delProgExercise(progExercise: ProgExercise) {
+    this.actionProgExercises.emit({
+      actionType: ActionType.delete,
+      object: progExercise
+    });
+  }
 }
