b2914903d35169ee1fb25659deea70b38bbd9d35
CptFranck <francois.caillet71@gmail.com>
2024-07-15 17:34:15 +0200

    Modify My Prog Exercise Component

--- src/app/page/my-fitness-plan/my-prog-exercise/my-prog-exercise/my-prog-exercise.component.ts
+++ src/app/page/my-fitness-plan/my-prog-exercise/my-prog-exercise/my-prog-exercise.component.ts
@@ -2,17 +2,44 @@ import {Component, inject, OnInit} from '@angular/core';
 import {ProgExercise} from "../../../../interface/dto/prog-exercise";
 import {ProgExerciseService} from "../../../../services/prog-exercise/prog-exercise.service";
 import {ActivatedRoute, Params} from "@angular/router";
+import {NgForOf, NgIf} from "@angular/common";
+import {ProgExerciseCardComponent} from "../../../../components/card/prog-exercise-card/prog-exercise-card.component";
+import {FormIndicator} from "../../../../interface/utils/form-indicator";
+import {ActionType} from "../../../../enum/action-type";
+import {LoadingComponent} from "../../../../components/loading/loading.component";
+import {
+  ProgExerciseCardDetailsComponent
+} from "../../../../components/card/prog-exercise-card-details/prog-exercise-card-details.component";
+import {
+  ProgExerciseModalComponent
+} from "../../../explore/prog-exercises/prog-exercise-modal/prog-exercise-modal.component";
+import {
+  MyProgExerciseModalComponent
+} from "../../my-prog-exercises/my-prog-exercise-modal/my-prog-exercise-modal.component";
+import {MyProgExerciseDetailsModalComponent} from "../my-prog-exercise-modal/my-prog-exercise-details-modal.component";
 
 @Component({
   selector: 'app-my-prog-exercise',
   standalone: true,
-  imports: [],
+  imports: [
+    NgForOf,
+    ProgExerciseCardComponent,
+    NgIf,
+    LoadingComponent,
+    ProgExerciseCardDetailsComponent,
+    ProgExerciseModalComponent,
+    MyProgExerciseModalComponent,
+    MyProgExerciseDetailsModalComponent
+  ],
   templateUrl: './my-prog-exercise.component.html',
 })
 export class MyProgExerciseComponent implements OnInit {
   id: string | undefined;
   loading: boolean = true;
   progExercise: ProgExercise | undefined;
+  action: ActionType = ActionType.update;
+  modalTitle: string = "";
+  progExerciseModalId: string = "progExerciseModal";
 
   private activatedRoute: ActivatedRoute = inject(ActivatedRoute);
   private proExerciseService: ProgExerciseService = inject(ProgExerciseService);
@@ -27,5 +54,11 @@ export class MyProgExerciseComponent implements OnInit {
       });
     });
     this.proExerciseService.isLoading.subscribe((isLoading: boolean) => this.loading = isLoading);
+  }
+
+  setProgExercise(formIndicator: FormIndicator) {
+    this.progExercise = formIndicator.object;
+    this.action = formIndicator.actionType;
+    this.modalTitle = formIndicator.object.name;
   }
 }
