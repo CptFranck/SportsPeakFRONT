3750533d23e2e4376da15dc12467f4f3c9185a73
CptFranck <francois.caillet71@gmail.com>
2024-07-16 13:44:45 +0200

    Update target set card display

--- src/app/components/card/target-set-card/target-set-card.component.ts
+++ src/app/components/card/target-set-card/target-set-card.component.ts
@@ -4,6 +4,7 @@ import {ProgExercise} from "../../../interface/dto/prog-exercise";
 import {FormIndicator} from "../../../interface/utils/form-indicator";
 import {ActionType} from "../../../enum/action-type";
 import {TargetSet} from "../../../interface/dto/target-set";
+import {getStringTime, getTargetSetInformation, getTargetSetTime} from "../../../utils/prog-exercise-functions";
 
 @Component({
   selector: 'app-target-set-card',
@@ -14,16 +15,28 @@ import {TargetSet} from "../../../interface/dto/target-set";
   templateUrl: './target-set-card.component.html',
 })
 export class TargetSetCardComponent implements OnInit {
+  targetSets: string = "";
   targetSetTime: string = "";
-  targetSets: string[] = [];
+  targetSetRestTime: string = "";
+  targetSetRepetition: string = "";
   @Input() modalId!: string;
   @Input() targetSet!: TargetSet;
 
   @Output() actionProgExercises: EventEmitter<FormIndicator> = new EventEmitter<FormIndicator>();
 
   ngOnInit() {
-    // this.targetSetTime = getProgExerciseTime(this.targetSet);
-    // this.targetSets = getTargetSetsInformation(this.targetSet);
+    this.targetSetTime = getTargetSetTime(this.targetSet);
+    this.targetSets = getTargetSetInformation(this.targetSet);
+    this.targetSetRestTime = getStringTime(
+      this.targetSet.restTime.seconds,
+      this.targetSet.restTime.minutes,
+      this.targetSet.restTime.hours
+    )
+    this.targetSetRepetition = getStringTime(
+      this.targetSet.physicalExertionUnitTime.seconds,
+      this.targetSet.physicalExertionUnitTime.minutes,
+      this.targetSet.physicalExertionUnitTime.hours
+    )
   }
 
   showProgExercisePerformance(progExercise: ProgExercise): void {
