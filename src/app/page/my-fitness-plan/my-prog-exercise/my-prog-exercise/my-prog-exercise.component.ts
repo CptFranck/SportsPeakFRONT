e9a2c91f3ac79b1d402d6ab5843e78a0e34f35ef
CptFranck <francois.caillet71@gmail.com>
2024-07-17 17:49:26 +0200

    Minor change

--- src/app/page/my-fitness-plan/my-prog-exercise/my-prog-exercise/my-prog-exercise.component.ts
+++ src/app/page/my-fitness-plan/my-prog-exercise/my-prog-exercise/my-prog-exercise.component.ts
@@ -73,6 +73,6 @@ export class MyProgExerciseComponent implements OnInit {
   setTargetSet(formIndicator: FormIndicator) {
     this.targetSet = formIndicator.object;
     this.targetSetAction = formIndicator.actionType;
-    this.targetSetModalTitle = "Set id " + formIndicator.object.id;
+    this.targetSetModalTitle = "Set N° " + formIndicator.object.id;
   }
 }
