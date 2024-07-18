1a2526a9ca333d8c4af6e2d701c8800dc98a62a9
CptFranck <francois.caillet71@gmail.com>
2024-07-18 15:11:22 +0200

    Modify confirmValidator

--- src/app/validators/confirmValidator.ts
+++ src/app/validators/confirmValidator.ts
@@ -2,11 +2,9 @@ import {AbstractControl, ValidationErrors, ValidatorFn} from "@angular/forms";
 
 export function confirmValidator(controlName: string, matchingControlName: string): ValidatorFn {
   return (abstractControl: AbstractControl): ValidationErrors | null => {
+
     const control: AbstractControl | null = abstractControl.get(controlName);
     const matchingControl: AbstractControl | null = abstractControl.get(matchingControlName);
-    if (matchingControl!.errors && !matchingControl!.errors?.['confirmedValidator']) {
-      return null;
-    }
 
     if (control!.value !== matchingControl!.value) {
       const error: ValidationErrors = {confirmedValidator: 'Passwords do not match'};
