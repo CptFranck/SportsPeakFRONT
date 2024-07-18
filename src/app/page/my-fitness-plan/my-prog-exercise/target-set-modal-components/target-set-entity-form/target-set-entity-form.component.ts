65ac9e5b0638a42196edd0debd78eadeed4ae704
CptFranck <francois.caillet71@gmail.com>
2024-07-18 20:13:09 +0200

    Update TargetSetEntityFormComponent

--- src/app/page/my-fitness-plan/my-prog-exercise/target-set-modal-components/target-set-entity-form/target-set-entity-form.component.ts
+++ src/app/page/my-fitness-plan/my-prog-exercise/target-set-modal-components/target-set-entity-form/target-set-entity-form.component.ts
@@ -1,4 +1,4 @@
-import {AfterViewInit, Component, inject, Input, OnInit} from '@angular/core';
+import {AfterViewInit, Component, Input, OnInit} from '@angular/core';
 import {ExerciseSelectComponent} from "../../../../../components/selects/exercise-select/exercise-select.component";
 import {FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators} from "@angular/forms";
 import {InputControlComponent} from "../../../../../components/input-control/input-control.component";
@@ -10,10 +10,12 @@ import {Observable, Subscription} from "rxjs";
 import {TargetSet} from "../../../../../interface/dto/target-set";
 import {WeightUnit} from "../../../../../interface/enum/weightUnit";
 import {Duration} from "../../../../../interface/dto/duration";
-import {TargetSetService} from "../../../../../services/target-set/target-set.service";
 import {ActionType} from "../../../../../enum/action-type";
 import {ProgExercise} from "../../../../../interface/dto/prog-exercise";
 import {WeightSelectComponent} from "../../../../../components/selects/weight-select/weight-select.component";
+import {DurationInputComponent} from "../../../../../components/input/duration-inputs/duration-input.component";
+import {createDurationForm} from "../../../../../utils/duration-functions";
+import {debugFormGroupInvalidField} from "../../../../../utils/debug-functions";
 
 @Component({
   selector: 'app-target-set-entity-form',
@@ -25,12 +27,14 @@ import {WeightSelectComponent} from "../../../../../components/selects/weight-se
     NgIf,
     ReactiveFormsModule,
     VisibilitySelectComponent,
-    WeightSelectComponent
+    WeightSelectComponent,
+    DurationInputComponent,
   ],
   templateUrl: './target-set-entity-form.component.html',
 })
 export class TargetSetEntityFormComponent implements OnInit, AfterViewInit {
   targetSet: TargetSet | undefined;
+  progExercise: ProgExercise | undefined;
   targetSetForm: FormGroup | null = null;
   submitInvalidForm: boolean = false;
   eventsSubscription!: Subscription;
@@ -38,9 +42,13 @@ export class TargetSetEntityFormComponent implements OnInit, AfterViewInit {
   @Input() actionType!: ActionType;
   @Input() btnCloseRef!: HTMLButtonElement;
   @Input() submitEvents!: Observable<void> | undefined;
-  @Input() progExercise: ProgExercise | undefined;
 
-  private targetSetService: TargetSetService = inject(TargetSetService);
+  // private targetSetService: TargetSetService = inject(TargetSetService);
+
+  @Input() set progExerciseInput(value: ProgExercise | undefined) {
+    this.progExercise = value;
+    this.initializeTargetSetForm();
+  }
 
   @Input() set targetSetInput(value: TargetSet | undefined) {
     this.targetSet = value;
@@ -57,12 +65,12 @@ export class TargetSetEntityFormComponent implements OnInit, AfterViewInit {
   }
 
   initializeTargetSetForm() {
-    const defaultDuration: Duration =
-      {seconds: 0, minutes: 0, hours: 0};
+    const defaultDuration: Duration = {seconds: 0, minutes: 0, hours: 0};
+
     const targetSetIndex: number = this.targetSet ? this.targetSet.index : 1;
     const targetSetSetNumber: number = this.targetSet ? this.targetSet.setNumber : 1;
     const targetSetRepetitionNumber: number = this.targetSet ? this.targetSet.repetitionNumber : 1;
-    const targetSetWeight: number = this.targetSet ? this.targetSet.repetitionNumber : 1;
+    const targetSetWeight: number = this.targetSet ? this.targetSet.weight : 1;
     const targetSetWeightUnit: string = this.targetSet ? this.targetSet.weightUnit : WeightUnit.KILOGRAMME;
     const targetSetPhysicalExertionUnitTime: Duration = this.targetSet ? this.targetSet.physicalExertionUnitTime : defaultDuration;
     const targetSetRestTime: Duration = this.targetSet ? this.targetSet.restTime : defaultDuration;
@@ -72,48 +80,43 @@ export class TargetSetEntityFormComponent implements OnInit, AfterViewInit {
         this.targetSet.id : this.targetSet.targetSetUpdate ? this.targetSet.targetSetUpdate.id : null) : null;
     const targetSetProgExerciseId: number | null = this.progExercise ? this.progExercise.id : null;
 
-    this.targetSetForm = new FormGroup({
-      index: new FormControl(
-        targetSetIndex,
-        [Validators.required,
-          Validators.min(1),
-          Validators.max(50),
-          Validators.pattern("^[0-9]*$")]),
-      setNumber: new FormControl(
-        targetSetSetNumber,
-        [Validators.required,
-          Validators.min(0),
-          Validators.max(1000),
-          Validators.pattern("^[0-9]*$")]
-      ),
-      repetitionNumber: new FormControl(
-        targetSetRepetitionNumber,
-        [Validators.required,
-          Validators.min(1),
-          Validators.max(1000),
-          Validators.pattern("^[0-9]*$")]),
-      weight: new FormControl(
-        targetSetWeight,
-        [Validators.required,
-          Validators.min(0),
-          Validators.max(1000),
-          Validators.pattern("^[0-9]+(.[0-9]{1,2})?$")]),
-      weightUnit: new FormControl(
-        targetSetWeightUnit,
-        [Validators.required]
-      ),
-      physicalExertionUnitTime: new FormControl(
-        targetSetPhysicalExertionUnitTime,
-        [Validators.required]
-      ),
-      restTime: new FormControl(
-        targetSetRestTime,
-        [Validators.required]
-      ),
-      creationDate: new FormControl(targetSetDate, [Validators.required]),
-      progExerciseId: new FormControl(targetSetProgExerciseId, [Validators.required]),
-      targetSetUpdateId: new FormControl(targetSetUpdateId),
-    });
+    this.targetSetForm = new FormGroup(
+      {
+        index: new FormControl(
+          targetSetIndex,
+          [Validators.required,
+            Validators.min(1),
+            Validators.max(50),
+            Validators.pattern("^[0-9]*$")]),
+        setNumber: new FormControl(
+          targetSetSetNumber,
+          [Validators.required,
+            Validators.min(0),
+            Validators.max(1000),
+            Validators.pattern("^[0-9]*$")]
+        ),
+        repetitionNumber: new FormControl(
+          targetSetRepetitionNumber,
+          [Validators.required,
+            Validators.min(1),
+            Validators.max(1000),
+            Validators.pattern("^[0-9]*$")]),
+        weight: new FormControl(
+          targetSetWeight,
+          [Validators.required,
+            Validators.min(0),
+            Validators.max(1000),
+            Validators.pattern("^[0-9]+(.[0-9]{1,2})?$")]),
+        weightUnit: new FormControl(
+          targetSetWeightUnit,
+          [Validators.required]
+        ),
+        physicalExertionUnitTime: createDurationForm(targetSetPhysicalExertionUnitTime),
+        restTime: createDurationForm(targetSetRestTime),
+        creationDate: new FormControl(targetSetDate, [Validators.required]),
+        progExerciseId: new FormControl(targetSetProgExerciseId, [Validators.required]),
+        targetSetUpdateId: new FormControl(targetSetUpdateId),
+      });
 
     if (this.targetSet) {
       this.targetSetForm.addControl("id", new FormControl(this.targetSet.id));
@@ -125,16 +128,20 @@ export class TargetSetEntityFormComponent implements OnInit, AfterViewInit {
 
   onSubmit() {
     if (!this.targetSetForm) return;
-    if (this.targetSetForm.valid) {
-      this.submitInvalidForm = false;
-      if (!this.targetSetForm.value.id) {
-        this.targetSetService.addTargetSet(this.targetSetForm);
-      } else {
-        this.targetSetService.modifyTargetSet(this.targetSetForm);
-      }
-      this.btnCloseRef.click();
-    } else {
-      this.submitInvalidForm = true;
-    }
+    debugFormGroupInvalidField(this.targetSetForm)
+    console.log("physicalExertionUnitTime", this.targetSetForm.value.physicalExertionUnitTime)
+    console.log("restTime", this.targetSetForm.controls["restTime"].value)
+
+    // if (this.targetSetForm.valid) {
+    //   this.submitInvalidForm = false;
+    //   if (!this.targetSetForm.value.id) {
+    //     this.targetSetService.addTargetSet(this.targetSetForm);
+    //   } else {
+    //     this.targetSetService.modifyTargetSet(this.targetSetForm);
+    //   }
+    //   this.btnCloseRef.click();
+    // } else {
+    //   this.submitInvalidForm = true;
+    // }
   }
 }
