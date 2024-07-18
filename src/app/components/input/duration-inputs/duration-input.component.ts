85ae245cffc8d84a01fea2fe25708d4ecae44c67
CptFranck <francois.caillet71@gmail.com>
2024-07-18 18:45:14 +0200

    Update DurationInputComponent

--- src/app/components/input/duration-inputs/duration-input.component.ts
+++ src/app/components/input/duration-inputs/duration-input.component.ts
@@ -1,17 +1,5 @@
-import {Component, forwardRef, Input, OnInit} from '@angular/core';
-import {
-  AbstractControl,
-  ControlValueAccessor,
-  FormControl,
-  FormGroup,
-  NG_VALUE_ACCESSOR,
-  ReactiveFormsModule,
-  ValidationErrors,
-  Validator,
-  Validators
-} from "@angular/forms";
-import {Duration} from "../../../interface/dto/duration";
-import {fn} from "@angular/compiler";
+import {Component, Input, OnInit} from '@angular/core';
+import {FormGroup, ReactiveFormsModule} from "@angular/forms";
 import {InputControlComponent} from "../../input-control/input-control.component";
 import {NgIf} from "@angular/common";
 
@@ -23,90 +11,15 @@ import {NgIf} from "@angular/common";
     NgIf,
     ReactiveFormsModule
   ],
-  providers: [
-    {
-      provide: NG_VALUE_ACCESSOR,
-      useExisting: forwardRef(() => DurationInputComponent),
-      multi: true
-    }
-  ],
   templateUrl: './duration-input.component.html',
 })
-export class DurationInputComponent implements OnInit, ControlValueAccessor, Validator {
+export class DurationInputComponent implements OnInit {
+  durationForm!: FormGroup;
 
-  durationForm: FormGroup | null = null;
-
-  @Input() duration!: Duration;
-  @Input() submitInvalidForm!: boolean;
-
-  onChange: (value: Duration) => void = () => {
-  };
-
-  onTouched: ($event: boolean) => void = () => {
-  };
+  @Input() fieldName!: string;
+  @Input() rootFormGroup!: FormGroup;
 
   ngOnInit(): void {
-    this.durationForm = new FormGroup({
-      hours: new FormControl(
-        this.duration.hours,
-        [Validators.required,
-          Validators.min(0),
-          Validators.max(24),
-          Validators.pattern("^[0-9]*$")]),
-      minutes: new FormControl(
-        this.duration.minutes,
-        [Validators.required,
-          Validators.min(0),
-          Validators.max(59),
-          Validators.pattern("^[0-9]*$")]),
-      seconds: new FormControl(
-        this.duration.seconds,
-        [Validators.required,
-          Validators.min(0),
-          Validators.max(59),
-          Validators.pattern("^[0-9]*$")]),
-    })
+    this.durationForm = this.rootFormGroup.get(this.fieldName) as FormGroup;
   }
-
-  registerOnChange(fn: any): void {
-    this.onChange = fn
-  }
-
-  registerOnTouched(fn: any): void {
-    this.onTouched = fn
-  }
-
-  writeValue(duration: Duration): void {
-    this.duration = duration;
-  }
-
-  setHours(event: Event) {
-    if (event.target instanceof HTMLInputElement) {
-      this.duration.hours = event.target.value;
-      this.onChange(this.duration);
-    }
-  }
-
-  setMinutes(event: Event) {
-    if (event.target instanceof HTMLInputElement) {
-      this.duration.minutes = event.target.value;
-      this.onChange(this.duration);
-    }
-  }
-
-  setSeconds(event: Event) {
-    if (event.target instanceof HTMLInputElement) {
-      this.duration.seconds = event.target.value;
-      this.onChange(this.duration);
-    }
-  }
-
-  isValid() {
-    return !!(this.countryCode && this.phoneNumber);
-  }
-
-  validate(control: AbstractControl): ValidationErrors | null {
-    return undefined;
-  }
-
 }
