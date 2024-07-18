d3b82be3e43f9a35284f833aeca91c263839ce87
CptFranck <francois.caillet71@gmail.com>
2024-07-18 12:48:36 +0200

    Modify InputControlComponent

--- src/app/components/input-control/input-control.component.ts
+++ src/app/components/input-control/input-control.component.ts
@@ -14,24 +14,24 @@ export class InputControlComponent implements OnInit {
   @Input() formGroup!: FormGroup;
   @Input() formGroupFieldName!: string;
   @Input() displayedFieldName: string | undefined;
-  @Input() rules: string = "";
-  @Input() errorMessage: string = "";
+  @Input() rules: string | undefined;
+  @Input() errorMessage: string | undefined;
   @Input() submitInvalid!: boolean;
   @Input() ifInvalid: boolean = false;
 
   ngOnInit() {
-    if (this.rules !== undefined)
-      this.rules = " (" + this.rules + ")"
-    else
-      this.rules = "";
-
     let fieldName: string;
     if (this.displayedFieldName !== undefined)
       fieldName = this.displayedFieldName;
     else
       fieldName = this.formGroupFieldName
 
-    if (this.errorMessage === "")
+    if (this.rules !== undefined)
+      this.rules = " (" + this.rules + ")"
+    else
+      this.rules = "";
+
+    if (this.errorMessage === undefined)
       this.errorMessage = "Please write a correct " + fieldName + this.rules;
     else
       this.errorMessage += this.rules;
