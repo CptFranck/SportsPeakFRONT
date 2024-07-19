db7defb6bd8c0c464382365454b80df9dadb28fb
CptFranck <francois.caillet71@gmail.com>
2024-07-19 12:54:49 +0200

    Refactoring utils functions

--- src/app/utils/prog-exercise-functions.ts
+++ src/app/utils/prog-exercise-functions.ts
@@ -3,8 +3,8 @@ import {ProgExercise} from "../interface/dto/prog-exercise";
 import {Dictionary} from "../interface/utils/dictionary";
 
 export function getTargetSetsInformation(progExercise: ProgExercise) {
-  let lastTargetSet: TargetSet[] = getLastTargetSets(progExercise);
-  lastTargetSet = sortLastTargetSetsByWeight(lastTargetSet)
+  let lastTargetSet: TargetSet[] = getUpToDateTargetSets(progExercise);
+  lastTargetSet = lastTargetSet.sort(sortLastTargetSetsByIndex);
 
   return lastTargetSet.map((targetSet: TargetSet) => getTargetSetInformation(targetSet));
 }
@@ -18,14 +18,18 @@ export function getTargetSetInformation(targetSet: TargetSet) {
 }
 
 export function getProgExerciseTime(progExercise: ProgExercise) {
-  let lastTargetSet: TargetSet[] = getLastTargetSets(progExercise);
+  let lastTargetSet: TargetSet[] = getUpToDateTargetSets(progExercise);
+  lastTargetSet = lastTargetSet.sort(sortLastTargetSetsByIndex);
 
   let totalSeconds: number = 0;
   let totalMinutes: number = 0;
   let totalHours: number = 0;
 
-  lastTargetSet.forEach((targetSet: TargetSet) => {
-    const timeAmount: Dictionary<number> = getTimeAmount(targetSet);
+  lastTargetSet.forEach((targetSet: TargetSet, key: number, array: TargetSet[]) => {
+    let isLastTargetSet: boolean = false
+    if (array.length - 1 === key)
+      isLastTargetSet = true;
+    const timeAmount: Dictionary<number> = getTimeAmount(targetSet, isLastTargetSet);
     totalSeconds += timeAmount["seconds"];
     totalMinutes += timeAmount["minutes"];
     totalHours += timeAmount["hours"];
@@ -33,14 +37,16 @@ export function getProgExerciseTime(progExercise: ProgExercise) {
   return getStringTime(totalSeconds, totalMinutes, totalHours);
 }
 
-export function getTargetSetTime(targetSet: TargetSet): string {
-  const timeAmount: Dictionary<number> = getTimeAmount(targetSet);
+export function getTargetSetTime(targetSet: TargetSet, isLastTargetSet: boolean = false): string {
+  const timeAmount: Dictionary<number> = getTimeAmount(targetSet, isLastTargetSet);
   return getStringTime(timeAmount["seconds"], timeAmount["minutes"], timeAmount["hours"]);
 }
 
-export function getTimeAmount(targetSet: TargetSet): Dictionary<number> {
+export function getTimeAmount(targetSet: TargetSet, isLastTargetSet: boolean = false): Dictionary<number> {
   const timeAmount: Dictionary<number> = {}
-  const totalRest: number = targetSet.setNumber;
+  let totalRest: number = targetSet.setNumber;
+  if (isLastTargetSet)
+    totalRest -= 1;
   const totalReps: number = targetSet.repetitionNumber * targetSet.setNumber;
   timeAmount["seconds"] = targetSet.physicalExertionUnitTime.seconds * totalReps + targetSet.restTime.seconds * totalRest;
   timeAmount["minutes"] = targetSet.physicalExertionUnitTime.minutes * totalReps + targetSet.restTime.minutes * totalRest;
@@ -53,18 +59,22 @@ export function getStringTime(seconds: number, minutes: number, hours: number): 
   const additionalMinutes: number = (seconds - secondsLeft) / 60
   const minutesLeft: number = (minutes + additionalMinutes) % 60;
   const additionalHours: number = (minutes + additionalMinutes - minutesLeft) / 60;
-  const h: number = (hours + additionalHours) % 60
-  return +h + "h" + minutesLeft + "m" + secondsLeft + "s";
+  const totalHours: number = (hours + additionalHours) % 60
+  return totalHours + "h" + minutesLeft + "m" + secondsLeft + "s";
 }
 
-function getLastTargetSets(progExercise: ProgExercise): TargetSet[] {
+export function getUpToDateTargetSets(progExercise: ProgExercise): TargetSet[] {
   return progExercise.targetSets.filter((targetSet: TargetSet) => targetSet.targetSetUpdate === null)
 }
 
-function sortLastTargetSetsByWeight(targetSets: TargetSet[]): TargetSet[] {
-  return targetSets.sort((a: TargetSet, b: TargetSet) => {
-    if (a.weight > b.weight) return 1;
-    if (a.weight < b.weight) return -1;
-    return 0;
-  })
+export function sortLastTargetSetsByIndex(a: TargetSet, b: TargetSet) {
+  if (a.index > b.index) return 1;
+  if (a.index < b.index) return -1;
+  return sortLastTargetSetsByWeight(a, b);
+}
+
+function sortLastTargetSetsByWeight(a: TargetSet, b: TargetSet) {
+  if (a.weight > b.weight) return 1;
+  if (a.weight < b.weight) return -1;
+  return 0;
 }
