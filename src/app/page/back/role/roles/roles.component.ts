759e29881546899db0a889211763f4c4534123e0
CptFranck <francois.caillet71@gmail.com>
2024-07-23 11:04:51 +0200

    Fix same errors on RolesComponent

--- src/app/page/back/role/roles/roles.component.ts
+++ src/app/page/back/role/roles/roles.component.ts
@@ -1,4 +1,4 @@
-import {Component, inject, OnInit, TemplateRef, ViewChild} from '@angular/core';
+import {Component, inject, OnDestroy, OnInit, TemplateRef, ViewChild} from '@angular/core';
 import {RolesArrayComponent} from "../roles-array/roles-array.component";
 import {RoleModalComponent} from "../role-modal/role-modal.component";
 import {LoadingComponent} from "../../../../components/loading/loading.component";
@@ -8,6 +8,7 @@ import {Role} from "../../../../interface/dto/role";
 import {ActionType} from "../../../../enum/action-type";
 import {RoleService} from "../../../../services/role/role.service";
 import {FormIndicator} from "../../../../interface/utils/form-indicator";
+import {Subject, takeUntil} from "rxjs";
 
 @Component({
   selector: 'app-roles',
@@ -21,26 +22,41 @@ import {FormIndicator} from "../../../../interface/utils/form-indicator";
   ],
   templateUrl: './roles.component.html',
 })
-export class RolesComponent implements OnInit {
+export class RolesComponent implements OnInit, OnDestroy {
   loading: boolean = true;
   roles: Role[] = [];
   role: Role | undefined;
   action: ActionType = ActionType.create;
   modalTitle: string = "";
   muscleModalId: string = "roleModal";
+
   @ViewChild("modalTemplate") modalTemplate!: TemplateRef<any>;
 
+  private unsubscribe$: Subject<void> = new Subject<void>();
   private roleService: RoleService = inject(RoleService);
 
   ngOnInit(): void {
-    this.roleService.roles.subscribe((roles: Role[]) => this.roles = roles);
-    this.roleService.isLoading.subscribe((isLoading: boolean) => this.loading = isLoading);
+    this.roleService.roles
+      .pipe(takeUntil(this.unsubscribe$))
+      .subscribe((roles: Role[]) =>
+        this.roles = roles);
+    this.roleService.isLoading
+      .pipe(takeUntil(this.unsubscribe$))
+      .subscribe((isLoading: boolean) =>
+        this.loading = isLoading);
+  }
+
+  ngOnDestroy() {
+    this.unsubscribe$.next();
+    this.unsubscribe$.complete();
   }
 
   setRole(formIndicator: FormIndicator) {
     this.role = formIndicator.object;
     this.action = formIndicator.actionType;
-    this.modalTitle = formIndicator.object.name;
+    if (formIndicator.object === undefined)
+      this.modalTitle = "Add new role";
+    else
+      this.modalTitle = formIndicator.object.name;
   }
-
 }
