2f90432e0a35fd74cf6d7bc181d8b2837c598761
CptFranck <francois.caillet71@gmail.com>
2024-07-24 16:49:16 +0200

    ProgExerciseTargetSets ts interface

---
+++ src/app/interface/utils/progExerciseTargetSets.ts
@@ -0,0 +1,8 @@
+import {TargetSet} from "../dto/target-set";
+
+export interface ProgExerciseTargetSets {
+  targetSetUsed: TargetSet[]
+  targetSetUnused: TargetSet[]
+  targetSetHidden: TargetSet[]
+}
+
