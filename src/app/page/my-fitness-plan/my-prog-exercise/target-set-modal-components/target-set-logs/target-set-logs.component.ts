793436684ef5a0aff676e0182668054c3273f705
CptFranck <francois.caillet71@gmail.com>
2024-09-04 17:15:02 +0200

    Add card when no target set logs

--- src/app/page/my-fitness-plan/my-prog-exercise/target-set-modal-components/target-set-logs/target-set-logs.component.ts
+++ src/app/page/my-fitness-plan/my-prog-exercise/target-set-modal-components/target-set-logs/target-set-logs.component.ts
@@ -2,7 +2,7 @@ import {Component, EventEmitter, Input, Output} from '@angular/core';
 import {ProgExercise} from "../../../../../interface/dto/prog-exercise";
 import {TargetSet} from "../../../../../interface/dto/target-set";
 import {getTargetSetLogs} from "../../../../../utils/prog-exercise-functions";
-import {NgForOf} from "@angular/common";
+import {NgForOf, NgIf} from "@angular/common";
 import {
   TargetSetLogsCardComponent
 } from "../../../../../components/card/target-set/target-set-logs-card/target-set-logs-card.component";
@@ -13,7 +13,8 @@ import {FormIndicator} from "../../../../../interface/utils/form-indicator";
   standalone: true,
   imports: [
     NgForOf,
-    TargetSetLogsCardComponent
+    TargetSetLogsCardComponent,
+    NgIf
   ],
   templateUrl: './target-set-logs.component.html',
 })
