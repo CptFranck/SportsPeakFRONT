bc950877a2d8dc977dc15057eb06765dd209eab6
CptFranck <francois.caillet71@gmail.com>
2024-09-09 11:36:19 +0200

    Add trustLabel select to PE card details component

--- src/app/components/card/prog-exercise/prog-exercise-card-details/prog-exercise-card-details.component.ts
+++ src/app/components/card/prog-exercise/prog-exercise-card-details/prog-exercise-card-details.component.ts
@@ -6,6 +6,9 @@ import {ActionType} from "../../../../interface/enum/action-type";
 import {ModalButtonComponent} from "../../../modal/modal-button/modal-button.component";
 import {NgForOf, NgIf} from "@angular/common";
 import {RouterLink} from "@angular/router";
+import {
+  ProgExerciseTrustLabelFormComponent
+} from "../../../../page/my-fitness-plan/my-prog-exercise/my-prog-exercise-modal-components/my-prog-exercise-trust-label-form/prog-exercise-trust-label-form.component";
 
 @Component({
   selector: 'app-prog-exercise-card-details',
@@ -14,7 +17,8 @@ import {RouterLink} from "@angular/router";
     ModalButtonComponent,
     NgForOf,
     NgIf,
-    RouterLink
+    RouterLink,
+    ProgExerciseTrustLabelFormComponent
   ],
   templateUrl: './prog-exercise-card-details.component.html',
 })
