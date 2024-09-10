4d1d86e9a1b957cd54b9cd3244645be97810a6c1
CptFranck <francois.caillet71@gmail.com>
2024-09-10 16:24:10 +0200

    Update NavBarUserMenuComponent with unsubscribe$ subject

--- src/app/components/nav-bar-element/nav-bar-user-menu/nav-bar-user-menu.component.ts
+++ src/app/components/nav-bar-element/nav-bar-user-menu/nav-bar-user-menu.component.ts
@@ -1,8 +1,9 @@
-import {Component, inject, OnInit} from '@angular/core';
+import {Component, inject, OnDestroy, OnInit} from '@angular/core';
 import {User} from "../../../interface/dto/user";
 import {UserLoggedService} from "../../../services/user-logged/user-logged.service";
 import {NgForOf, NgIf} from "@angular/common";
 import {RouterLink, RouterLinkActive} from "@angular/router";
+import {Subject, takeUntil} from "rxjs";
 
 @Component({
   selector: 'app-nav-bar-user-menu',
@@ -15,17 +16,25 @@ import {RouterLink, RouterLinkActive} from "@angular/router";
   ],
   templateUrl: './nav-bar-user-menu.component.html',
 })
-export class NavBarUserMenuComponent implements OnInit {
+export class NavBarUserMenuComponent implements OnInit, OnDestroy {
   user: User | undefined = undefined;
   isAdmin: boolean | undefined;
   navbarDropdownId: string = "NavBarUserMenu"
 
+  private unsubscribe$: Subject<void> = new Subject<void>();
   private userService: UserLoggedService = inject(UserLoggedService);
 
   ngOnInit() {
-    this.userService.currentUser.subscribe((user: User | undefined) => {
-      this.user = user;
-      this.isAdmin = this.userService.isAdmin();
-    })
+    this.userService.currentUser
+      .pipe(takeUntil(this.unsubscribe$))
+      .subscribe((user: User | undefined) => {
+        this.user = user;
+        this.isAdmin = this.userService.isAdmin();
+      })
+  }
+
+  ngOnDestroy(): void {
+    this.unsubscribe$.next();
+    this.unsubscribe$.complete();
   }
 }
