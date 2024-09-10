49ed4abd4a46cb0a477e8e4ebaa6973d3cede63e
CptFranck <francois.caillet71@gmail.com>
2024-09-10 16:44:29 +0200

    Update ProgExerciseTrustLabelFormComponent with unsubscribe$ subject

--- src/app/page/my-fitness-plan/my-prog-exercise/my-prog-exercise-modal-components/my-prog-exercise-trust-label-form/prog-exercise-trust-label-form.component.ts
+++ src/app/page/my-fitness-plan/my-prog-exercise/my-prog-exercise-modal-components/my-prog-exercise-trust-label-form/prog-exercise-trust-label-form.component.ts
@@ -1,4 +1,4 @@
-import {Component, inject, Input, OnInit} from '@angular/core';
+import {Component, inject, Input, OnDestroy, OnInit} from '@angular/core';
 import {NgIf} from "@angular/common";
 import {
   TargetSetSateSelectComponent
@@ -10,6 +10,7 @@ import {
   TrustLabelSelectComponent
 } from "../../../../../components/selects/prog-exercise-trust-label-select/trust-label-select.component";
 import {UserLoggedService} from "../../../../../services/user-logged/user-logged.service";
+import {Subject, takeUntil} from "rxjs";
 
 @Component({
   selector: 'app-prog-exercise-trust-label-form',
@@ -22,21 +23,29 @@ import {UserLoggedService} from "../../../../../services/user-logged/user-logged
   ],
   templateUrl: './prog-exercise-trust-label-form.component.html',
 })
-export class ProgExerciseTrustLabelFormComponent implements OnInit {
+export class ProgExerciseTrustLabelFormComponent implements OnInit, OnDestroy {
   modify: boolean = false;
   isStaff: boolean = false;
   progExerciseTrustLabel: FormGroup | null = null;
 
   @Input() progExercise!: ProgExercise;
 
+  private unsubscribe$: Subject<void> = new Subject<void>();
   private userLoggedService: UserLoggedService = inject(UserLoggedService);
   private progExerciseService: ProgExerciseService = inject(ProgExerciseService);
 
   ngOnInit() {
     this.initializeProgExerciseForm();
-    this.userLoggedService.currentUser.subscribe(() => {
-      this.isStaff = this.userLoggedService.isStaff();
-    })
+    this.userLoggedService.currentUser
+      .pipe(takeUntil(this.unsubscribe$))
+      .subscribe(() => {
+        this.isStaff = this.userLoggedService.isStaff();
+      })
+  }
+
+  ngOnDestroy() {
+    this.unsubscribe$.next();
+    this.unsubscribe$.complete();
   }
 
   initializeProgExerciseForm() {
