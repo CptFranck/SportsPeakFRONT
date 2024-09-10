5f41f78aadc5f683587fc5927178e3c7ebc8ed09
CptFranck <francois.caillet71@gmail.com>
2024-09-10 16:14:11 +0200

    Update AlertDisplayComponent with unsubscribe$ subject

--- src/app/components/alert-display/alert-display.component.ts
+++ src/app/components/alert-display/alert-display.component.ts
@@ -1,8 +1,9 @@
-import {Component, inject} from '@angular/core';
+import {Component, inject, OnDestroy, OnInit} from '@angular/core';
 import {AlertComponent} from "../alert/alert.component";
 import {NgForOf} from "@angular/common";
 import {Alert} from "../../interface/utils/alert";
 import {AlertService} from "../../services/alert/alert.service";
+import {Subject, takeUntil} from "rxjs";
 
 @Component({
   selector: 'app-alert-display',
@@ -13,15 +14,23 @@ import {AlertService} from "../../services/alert/alert.service";
   ],
   templateUrl: './alert-display.component.html',
 })
-export class AlertDisplayComponent {
+export class AlertDisplayComponent implements OnInit, OnDestroy {
   alerts: Alert[] = [];
 
+  private unsubscribe$: Subject<void> = new Subject<void>();
   private alertService: AlertService = inject(AlertService);
 
-  constructor() {
-    this.alertService.getAlertsSubject().subscribe((alerts: Alert[]) => {
-      this.alerts = alerts;
-    })
+  ngOnInit() {
+    this.alertService.getAlertsSubject()
+      .pipe(takeUntil(this.unsubscribe$))
+      .subscribe((alerts: Alert[]) => {
+        this.alerts = alerts;
+      })
+  }
+
+  ngOnDestroy(): void {
+    this.unsubscribe$.next();
+    this.unsubscribe$.complete();
   }
 
   removeAlert(alert: Alert) {
