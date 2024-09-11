6a96952f8cfbdcf36884d308c183159998b48487
CptFranck <francois.caillet71@gmail.com>
2024-09-11 12:01:35 +0200

    Add refetch queries to performance log service

--- src/app/services/performance-log/performance-log.service.ts
+++ src/app/services/performance-log/performance-log.service.ts
@@ -1,9 +1,6 @@
 import {inject, Injectable} from '@angular/core';
 import {FormGroup} from "@angular/forms";
-import {
-  GET_USER_PROG_EXERCISES,
-  MOD_PROG_EXERCISE_TRUST_LABEL
-} from "../../graphql/operations/prog-exercise.operations";
+import {GET_PROG_EXERCISE_BY_ID} from "../../graphql/operations/prog-exercise.operations";
 import {Apollo, MutationResult} from "apollo-angular";
 import {ProgExercise} from "../../interface/dto/prog-exercise";
 import {
@@ -12,33 +9,37 @@ import {
   MOD_PERFORMANCE_LOG
 } from "../../graphql/operations/performance-log.operations";
 import {AlertService} from "../alert/alert.service";
-import {User} from "../../interface/dto/user";
-import {UserLoggedService} from "../user-logged/user-logged.service";
+import {ProgExerciseService} from "../prog-exercise/prog-exercise.service";
+import {PerformanceLog} from "../../interface/dto/performance-log";
 
 @Injectable({
   providedIn: 'root'
 })
 export class PerformanceLogService {
 
+  private progExercise: ProgExercise | undefined;
   private apollo: Apollo = inject(Apollo);
   private alertService: AlertService = inject(AlertService);
-  private userLoggedService: UserLoggedService = inject(UserLoggedService);
+  private progExerciseService: ProgExerciseService = inject(ProgExerciseService);
 
   constructor() {
+    this.progExerciseService.progExercise.subscribe((progExercise: ProgExercise | undefined) => {
+      if (progExercise)
+        this.progExercise = progExercise;
+    })
   }
 
   addPerformanceLog(progExercisesForm: FormGroup) {
-    const user: User | undefined = this.userLoggedService.currentUser.value
-    if (user)
+    if (this.progExercise)
       this.apollo.mutate({
         mutation: ADD_PERFORMANCE_LOG,
         variables: {
-          inputNewProgExercise: progExercisesForm.value,
+          inputNewPerformanceLog: progExercisesForm.value,
         },
         refetchQueries: [{
-          query: GET_USER_PROG_EXERCISES,
+          query: GET_PROG_EXERCISE_BY_ID,
           variables: {
-            userId: user.id
+            id: this.progExercise.id
           }
         }]
       }).subscribe(
@@ -46,90 +47,61 @@ export class PerformanceLogService {
           if (result.errors) {
             this.alertService.graphQLErrorAlertHandler(result.errors);
           } else {
-            let message: string = "Programed exercise " + result.data.addPerformanceLog.name + " been successfully created.";
+            let message: string = "Performance log " + result.data.addPerformanceLog.id + " been successfully created.";
             this.alertService.addSuccessAlert(message);
           }
         });
     else
-      this.alertService.addErrorAlert("User not logged in.");
+      this.alertService.addErrorAlert("Programed exercise is undefined !");
   }
 
   modifyPerformanceLog(progExercisesForm: FormGroup) {
-    const user: User | undefined = this.userLoggedService.currentUser.value
-    if (user)
+    if (this.progExercise)
       this.apollo.mutate({
         mutation: MOD_PERFORMANCE_LOG,
         variables: {
-          inputProgExercise: progExercisesForm.value,
+          inputPerformanceLog: progExercisesForm.value,
         },
         refetchQueries: [{
-          query: GET_USER_PROG_EXERCISES,
+          query: GET_PROG_EXERCISE_BY_ID,
           variables: {
-            userId: user.id
+            id: this.progExercise.id
           }
         }]
       }).subscribe((result: MutationResult): void => {
         if (result.errors) {
           this.alertService.graphQLErrorAlertHandler(result.errors);
         } else {
-          let message: string = "Programed exercise " + result.data.modifyPerformanceLog.name + " been successfully updated.";
+          let message: string = "Performance log " + result.data.modifyPerformanceLog.id + " been successfully updated.";
           this.alertService.addSuccessAlert(message);
         }
       });
     else
-      this.alertService.addErrorAlert("User not logged in.");
+      this.alertService.addErrorAlert("Programed exercise is undefined !");
   }
 
-  modifyProgExerciseTrustLabel(progExercisesForm: FormGroup) {
-    const user: User | undefined = this.userLoggedService.currentUser.value
-    if (user)
-      this.apollo.mutate({
-        mutation: MOD_PROG_EXERCISE_TRUST_LABEL,
-        variables: {
-          inputProgExerciseTrustLabel: progExercisesForm.value,
-        },
-        refetchQueries: [{
-          query: GET_USER_PROG_EXERCISES,
-          variables: {
-            userId: user.id
-          }
-        }]
-      }).subscribe((result: MutationResult): void => {
-        if (result.errors) {
-          this.alertService.graphQLErrorAlertHandler(result.errors);
-        } else {
-          let message: string = "Programed exercise " + result.data.modifyProgExerciseTrustLabel.name + " been successfully updated.";
-          this.alertService.addSuccessAlert(message);
-        }
-      });
-    else
-      this.alertService.addErrorAlert("User not logged in.");
-  }
-
-  deletePerformanceLog(progExercise: ProgExercise) {
-    const user: User | undefined = this.userLoggedService.currentUser.value
-    if (user) {
+  deletePerformanceLog(performanceLog: PerformanceLog) {
+    if (this.progExercise)
       this.apollo.mutate({
         mutation: DEL_PERFORMANCE_LOG,
         variables: {
-          progExerciseId: progExercise.id,
+          performanceLogId: performanceLog.id,
         },
         refetchQueries: [{
-          query: GET_USER_PROG_EXERCISES,
+          query: GET_PROG_EXERCISE_BY_ID,
           variables: {
-            userId: user.id
+            id: performanceLog.id
           }
         }]
       }).subscribe((result: MutationResult): void => {
         if (result.errors) {
           this.alertService.graphQLErrorAlertHandler(result.errors);
         } else {
-          let message: string = "Programed exercise " + progExercise.name + " has been successfully deleted.";
+          let message: string = "Performance log " + performanceLog.id + " has been successfully deleted.";
           this.alertService.addSuccessAlert(message);
         }
       });
-    } else {
-      this.alertService.addErrorAlert("User not logged in.");
-    }
+    else
+      this.alertService.addErrorAlert("Programed exercise is undefined !");
   }
 }
