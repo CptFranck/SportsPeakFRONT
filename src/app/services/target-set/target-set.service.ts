4160c6815ee8eb211e08dee6c9b5dc82e6518a0f
CptFranck <francois.caillet71@gmail.com>
2024-09-11 12:06:14 +0200

    Add refetch queries to target set service

--- src/app/services/target-set/target-set.service.ts
+++ src/app/services/target-set/target-set.service.ts
@@ -9,77 +9,125 @@ import {
   MOD_TARGET_SET_STATE
 } from "../../graphql/operations/target-set.operations";
 import {TargetSet} from "../../interface/dto/target-set";
+import {ProgExercise} from "../../interface/dto/prog-exercise";
+import {ProgExerciseService} from "../prog-exercise/prog-exercise.service";
+import {GET_PROG_EXERCISE_BY_ID} from "../../graphql/operations/prog-exercise.operations";
 
 @Injectable({
   providedIn: 'root'
 })
 export class TargetSetService {
 
+  private progExercise: ProgExercise | undefined;
   private apollo: Apollo = inject(Apollo);
   private alertService: AlertService = inject(AlertService);
+  private progExerciseService: ProgExerciseService = inject(ProgExerciseService);
+
+  constructor() {
+    this.progExerciseService.progExercise.subscribe((progExercise: ProgExercise | undefined) => {
+      if (progExercise)
+        this.progExercise = progExercise;
+    })
+  }
 
   addTargetSet(targetSetForm: FormGroup) {
-    return this.apollo.mutate({
-      mutation: ADD_TARGET_SET,
-      variables: {
-        inputNewTargetSet: targetSetForm.value,
-      },
-    }).subscribe(
-      (result: MutationResult): void => {
-        if (result.errors) {
-          this.alertService.graphQLErrorAlertHandler(result.errors);
-        } else {
-          let message: string = "Set " + result.data.addTargetSet.index + " been successfully created.";
-          this.alertService.addSuccessAlert(message);
-        }
-      });
+    if (this.progExercise)
+      this.apollo.mutate({
+        mutation: ADD_TARGET_SET,
+        variables: {
+          inputNewTargetSet: targetSetForm.value,
+        },
+        refetchQueries: [{
+          query: GET_PROG_EXERCISE_BY_ID,
+          variables: {
+            id: this.progExercise.id
+          }
+        }]
+      }).subscribe(
+        (result: MutationResult): void => {
+          if (result.errors) {
+            this.alertService.graphQLErrorAlertHandler(result.errors);
+          } else {
+            let message: string = "Set " + result.data.addTargetSet.index + " been successfully created.";
+            this.alertService.addSuccessAlert(message);
+          }
+        });
+    else
+      this.alertService.addErrorAlert("Programed exercise is undefined !");
   }
 
   modifyTargetSet(targetSetForm: FormGroup) {
-    return this.apollo.mutate({
-      mutation: MOD_TARGET_SET,
-      variables: {
-        inputTargetSet: targetSetForm.value,
-      },
-    }).subscribe((result: MutationResult): void => {
-      if (result.errors) {
-        this.alertService.graphQLErrorAlertHandler(result.errors);
-      } else {
-        let message: string = "Set " + result.data.modifyTargetSet.index + " been successfully updated.";
-        this.alertService.addSuccessAlert(message);
-      }
-    });
+    if (this.progExercise)
+      this.apollo.mutate({
+        mutation: MOD_TARGET_SET,
+        variables: {
+          inputTargetSet: targetSetForm.value,
+        },
+        refetchQueries: [{
+          query: GET_PROG_EXERCISE_BY_ID,
+          variables: {
+            id: this.progExercise.id
+          }
+        }]
+      }).subscribe((result: MutationResult): void => {
+        if (result.errors) {
+          this.alertService.graphQLErrorAlertHandler(result.errors);
+        } else {
+          let message: string = "Set " + result.data.modifyTargetSet.index + " been successfully updated.";
+          this.alertService.addSuccessAlert(message);
+        }
+      });
+    else
+      this.alertService.addErrorAlert("Programed exercise is undefined !");
   }
 
   modifyTargetSetState(targetSetStateForm: FormGroup) {
-    return this.apollo.mutate({
-      mutation: MOD_TARGET_SET_STATE,
-      variables: {
-        inputTargetSetState: targetSetStateForm.value,
-      },
-    }).subscribe((result: MutationResult): void => {
-      if (result.errors) {
-        this.alertService.graphQLErrorAlertHandler(result.errors);
-      } else {
-        let message: string = "Set state been successfully updated.";
-        this.alertService.addSuccessAlert(message);
-      }
-    });
+    if (this.progExercise)
+      this.apollo.mutate({
+        mutation: MOD_TARGET_SET_STATE,
+        variables: {
+          inputTargetSetState: targetSetStateForm.value,
+        },
+        refetchQueries: [{
+          query: GET_PROG_EXERCISE_BY_ID,
+          variables: {
+            id: this.progExercise.id
+          }
+        }]
+      }).subscribe((result: MutationResult): void => {
+        if (result.errors) {
+          this.alertService.graphQLErrorAlertHandler(result.errors);
+        } else {
+          let message: string = "Set state been successfully updated.";
+          this.alertService.addSuccessAlert(message);
+        }
+      });
+    else
+      this.alertService.addErrorAlert("Programed exercise is undefined !");
   }
 
   deleteTargetSet(targetSet: TargetSet) {
-    return this.apollo.mutate({
-      mutation: DEL_TARGET_SET,
-      variables: {
-        targetSetId: targetSet.id,
-      },
-    }).subscribe((result: MutationResult): void => {
-      if (result.errors) {
-        this.alertService.graphQLErrorAlertHandler(result.errors);
-      } else {
-        let message: string = "Set " + targetSet.index + " has been successfully deleted.";
-        this.alertService.addSuccessAlert(message);
-      }
-    });
+    if (this.progExercise)
+      this.apollo.mutate({
+        mutation: DEL_TARGET_SET,
+        variables: {
+          targetSetId: targetSet.id,
+        },
+        refetchQueries: [{
+          query: GET_PROG_EXERCISE_BY_ID,
+          variables: {
+            id: this.progExercise.id
+          }
+        }]
+      }).subscribe((result: MutationResult): void => {
+        if (result.errors) {
+          this.alertService.graphQLErrorAlertHandler(result.errors);
+        } else {
+          let message: string = "Set " + targetSet.index + " has been successfully deleted.";
+          this.alertService.addSuccessAlert(message);
+        }
+      });
+    else
+      this.alertService.addErrorAlert("Programed exercise is undefined !");
   }
 }
