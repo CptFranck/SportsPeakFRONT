5e074c8c197d3d0bae167c43a3dbad78fb9d0187
CptFranck <francois.caillet71@gmail.com>
2024-09-12 17:29:21 +0200

    Update performance logs card

--- src/app/components/card/performance-log/performance-logs-card/performance-logs-card.component.ts
+++ src/app/components/card/performance-log/performance-logs-card/performance-logs-card.component.ts
@@ -1,7 +1,9 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 import {ModalButtonComponent} from "../../../modal/modal-button/modal-button.component";
 import {PerformanceLog} from "../../../../interface/dto/performance-log";
 import {JsonPipe} from "@angular/common";
+import {FormIndicator} from "../../../../interface/utils/form-indicator";
+import {ActionType} from "../../../../interface/enum/action-type";
 
 @Component({
   selector: 'app-performance-logs-card',
@@ -16,9 +18,26 @@ export class PerformanceLogsCardComponent implements OnInit {
 
   performanceLogDate!: Date;
 
+  @Input() modalId!: string;
   @Input() performanceLog!: PerformanceLog;
+
+  @Output() actionPerformanceLog: EventEmitter<FormIndicator> = new EventEmitter<FormIndicator>();
 
   ngOnInit() {
     this.performanceLogDate = new Date(this.performanceLog?.logDate);
+  }
+
+  modPerformanceLog($event: any) {
+    this.actionPerformanceLog.emit({
+      actionType: ActionType.updatePerformance,
+      object: $event
+    });
+  }
+
+  delPerformanceLog($event: any) {
+    this.actionPerformanceLog.emit({
+      actionType: ActionType.deletePerformance,
+      object: $event
+    });
   }
 }
