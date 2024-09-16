67a4e319ee71f9161dedf20a0fdb263c68276127
CptFranck <francois.caillet71@gmail.com>
2024-09-16 12:56:35 +0200

    Minor changes

--- src/app/page/my-fitness-plan/my-fitness-plan/my-fitness-plan.component.ts
+++ src/app/page/my-fitness-plan/my-fitness-plan/my-fitness-plan.component.ts
@@ -1,11 +1,10 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-my-fitness-plan',
   standalone: true,
   imports: [],
   templateUrl: './my-fitness-plan.component.html',
-  styleUrl: './my-fitness-plan.component.css'
 })
 export class MyFitnessPlanComponent {
 
