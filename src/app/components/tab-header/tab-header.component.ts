65eb043014d3d3e07f1b8c0bfbeb8323f26ab822
CptFranck <francois.caillet71@gmail.com>
2024-09-17 15:53:04 +0200

    Modify tab header

--- src/app/components/tab-header/tab-header.component.ts
+++ src/app/components/tab-header/tab-header.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, Input} from '@angular/core';
 import {NgForOf} from "@angular/common";
 import {tabOption} from "../../interface/components/tab/tabOption";
 
@@ -9,23 +9,8 @@ import {tabOption} from "../../interface/components/tab/tabOption";
     NgForOf
   ],
   templateUrl: './tab-header.component.html',
-  styleUrl: './tab-header.component.css',
 })
-export class TabHeaderComponent implements OnInit {
-  ulClass!: string
-  dataBsToggle!: string
-
+export class TabHeaderComponent {
   @Input() tabId!: string;
-  @Input() type: string = "tab";
   @Input() tabOptions!: tabOption[];
-
-  ngOnInit() {
-    if (this.type === "tab") {
-      this.ulClass = "nav-tabs"
-      this.dataBsToggle = "tab";
-    } else if (this.type === "pill") {
-      this.ulClass = "nav-pills"
-      this.dataBsToggle = "pill";
-    }
-  }
 }
