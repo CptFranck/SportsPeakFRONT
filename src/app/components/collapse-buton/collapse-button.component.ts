70b82544543eceb9edcdd838d864cd05ecb7b31e
CptFranck <francois.caillet71@gmail.com>
2024-09-18 15:12:44 +0200

    Update collapse button

--- src/app/components/collapse-buton/collapse-button.component.ts
+++ src/app/components/collapse-buton/collapse-button.component.ts
@@ -1,4 +1,5 @@
 import {Component, EventEmitter, Input, Output} from '@angular/core';
+import {CollapseBlockComponent} from "../collapse-block/collapse-block.component";
 
 @Component({
   selector: 'app-collapse-button',
@@ -7,13 +8,27 @@ import {Component, EventEmitter, Input, Output} from '@angular/core';
   templateUrl: './collapse-button.component.html',
 })
 export class CollapseButtonComponent {
-  @Input() collapseId!: string;
-  @Input() btnClass!: string;
   @Input() value!: any;
+  @Input() btnClass!: string;
+  @Input() collapseId!: string;
+  @Input() onlyOpen: boolean = false
+  @Input() onlyClose: boolean = false
 
   @Output() onClickEvent: EventEmitter<any> = new EventEmitter();
 
+  @Input() collapseBlockRef?: CollapseBlockComponent;
+
   onClick() {
-    this.onClickEvent.emit(this.value);
+    if (this.value && this.onClickEvent) {
+      this.onClickEvent.emit(this.value);
+    }
+    if (this.collapseBlockRef) {
+      if (this.onlyOpen)
+        this.collapseBlockRef.show();
+      else if (this.onlyClose)
+        this.collapseBlockRef.hide()
+      else
+        this.collapseBlockRef.toggle()
+    }
   }
 }
