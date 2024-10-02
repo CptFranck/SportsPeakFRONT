254c19fb58b57855fe1b4f813326e37f48419e40
CptFranck <francois.caillet71@gmail.com>
2024-10-02 10:54:32 +0200

    Update performance logs chart component with accordion

--- src/app/components/chart/performance-logs-charts/performance-logs-charts.component.ts
+++ src/app/components/chart/performance-logs-charts/performance-logs-charts.component.ts
@@ -5,18 +5,25 @@ import {DictionaryItem} from "../../../interface/utils/dictionary-item";
 import {PerformanceLog} from "../../../interface/dto/performance-log";
 import {NgForOf} from "@angular/common";
 import {PerformanceLogsSetChartComponent} from "../performance-logs-set-chart/performance-logs-set-chart.component";
+import {CollapseGroupItemComponent} from "../../collapse-group/collapse-group-item.component";
+import {
+  PerformanceLogsCardComponent
+} from "../../card/performance-log/performance-logs-card/performance-logs-card.component";
 
 @Component({
   selector: 'app-performance-logs-charts',
   standalone: true,
   imports: [
     NgForOf,
-    PerformanceLogsSetChartComponent
+    PerformanceLogsSetChartComponent,
+    CollapseGroupItemComponent,
+    PerformanceLogsCardComponent
   ],
   templateUrl: './performance-logs-charts.component.html',
 })
 export class PerformanceLogsChartsComponent {
 
+  accordionParentIdGraph: any;
   performanceLogsInputSortedBySet!: DictionaryItem<PerformanceLog[]>[];
   performanceLogsInputSortedByLogDate!: DictionaryItem<PerformanceLog[]>[];
 
