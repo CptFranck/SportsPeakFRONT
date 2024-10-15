2e984f33e3c33daace5e06bc8248480829e1690e
CptFranck <francois.caillet71@gmail.com>
2024-10-15 17:53:12 +0200

    move target set log sorted by set

--- src/app/components/modal-component/performance-log/performance-log-sorted-by-set/performance-log-sorted-by-set.component.ts
+++ src/app/components/modal-component/performance-log/performance-log-sorted-by-set/performance-log-sorted-by-set.component.ts
@@ -2,12 +2,12 @@ import {Component, EventEmitter, Input, Output} from '@angular/core';
 import {DatePipe, NgForOf, NgIf} from "@angular/common";
 import {
   PerformanceLogsCardComponent
-} from "../../../../../../components/card/performance-log/performance-logs-card/performance-logs-card.component";
-import {DictionaryItem} from "../../../../../../interface/utils/dictionary-item";
-import {PerformanceLog} from "../../../../../../interface/dto/performance-log";
-import {CollapseBlockComponent} from "../../../../../../components/collapse-block/collapse-block.component";
-import {FormIndicator} from "../../../../../../interface/utils/form-indicator";
-import {ifFirstShow, ifNotFirstCollapse} from "../../../../../../utils/accordion-function";
+} from "../../../card/performance-log/performance-logs-card/performance-logs-card.component";
+import {DictionaryItem} from "../../../../interface/utils/dictionary-item";
+import {PerformanceLog} from "../../../../interface/dto/performance-log";
+import {CollapseBlockComponent} from "../../../collapse-block/collapse-block.component";
+import {FormIndicator} from "../../../../interface/utils/form-indicator";
+import {ifFirstShow, ifNotFirstCollapse} from "../../../../utils/accordion-function";
 
 @Component({
   selector: 'app-performance-log-sorted-by-set',
