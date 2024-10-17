1705236fd1adc63406af40df97b3d26d67a320a1
CptFranck <francois.caillet71@gmail.com>
2024-10-17 14:50:30 +0200

    Update auth component to use tab header component

--- src/app/page/auth/auth/auth.component.ts
+++ src/app/page/auth/auth/auth.component.ts
@@ -1,18 +1,28 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {LoginFormComponent} from "../../../components/form/auth/login-form/login-form.component";
 import {RegisterFormComponent} from "../../../components/form/auth/register-form/register-form.component";
+import {TabHeaderComponent} from "../../../components/tab-header/tab-header.component";
+import {tabOption} from "../../../interface/components/tab/tabOption";
 
 @Component({
   selector: 'app-login',
   standalone: true,
   imports: [
     LoginFormComponent,
-    RegisterFormComponent
+    RegisterFormComponent,
+    TabHeaderComponent
   ],
   styleUrl: "auth.component.css",
   templateUrl: './auth.component.html',
 })
-export class AuthComponent {
-  loginTabId: string = "loginTabId"
-  registerTabId: string = "registerTabId"
+export class AuthComponent implements OnInit {
+  tabId: string = "targetLogsTab";
+  tabOptions: tabOption[] = [];
+
+  ngOnInit() {
+    this.tabOptions = [
+      {id: "loginTabId", title: "Login", active: "active"},
+      {id: "registerTabId", title: "Register", active: ""},
+    ]
+  }
 }
