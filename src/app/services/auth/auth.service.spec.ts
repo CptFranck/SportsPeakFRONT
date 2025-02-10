84b866d79e5e2d8bfab4c40a244542f904e265ce
CptFranck <francois.caillet71@gmail.com>
2025-02-10 20:01:24 +0100

    Fix should create test on user AuthService service

--- src/app/services/auth/auth.service.spec.ts
+++ src/app/services/auth/auth.service.spec.ts
@@ -1,12 +1,19 @@
-import { TestBed } from '@angular/core/testing';
+import {TestBed} from '@angular/core/testing';
 
-import { AuthService } from './auth.service';
+import {AuthService} from './auth.service';
+import {ApolloTestingModule} from "apollo-angular/testing";
+import {HttpClientTestingModule} from "@angular/common/http/testing";
 
 describe('AuthService', () => {
   let service: AuthService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [
+        ApolloTestingModule,
+        HttpClientTestingModule
+      ],
+    });
     service = TestBed.inject(AuthService);
   });
 
