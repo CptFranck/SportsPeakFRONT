09e0725b0cccd8c824eaa47c8df7951f6b7af57b
CptFranck <francois.caillet71@gmail.com>
2025-02-10 19:50:26 +0100

    Fix should create test on exercise delete form component

--- src/app/components/form/exercise/exercise-delete-form/exercise-delete-form.component.spec.ts
+++ src/app/components/form/exercise/exercise-delete-form/exercise-delete-form.component.spec.ts
@@ -1,17 +1,19 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { ExerciseDeleteFormComponent } from './exercise-delete-form.component';
+import {ExerciseDeleteFormComponent} from './exercise-delete-form.component';
+import {ExerciseService} from "../../../../services/exercise/exercise.service";
 
 describe('ExerciseDeleteFormComponent', () => {
   let component: ExerciseDeleteFormComponent;
   let fixture: ComponentFixture<ExerciseDeleteFormComponent>;
-
+  let mockExerciseService: jasmine.SpyObj<ExerciseService>;
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      providers: [{provide: ExerciseService, useValue: mockExerciseService}],
       imports: [ExerciseDeleteFormComponent]
     })
-    .compileComponents();
-    
+      .compileComponents();
+
     fixture = TestBed.createComponent(ExerciseDeleteFormComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
