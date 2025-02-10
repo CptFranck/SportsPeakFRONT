a30c14474b79e8db9f6de4caa7724daf39fd2b35
CptFranck <francois.caillet71@gmail.com>
2025-02-10 19:52:35 +0100

    Fix should create test on muscle selector component

--- src/app/components/selectors/muscle-selector/muscle-selector.component.spec.ts
+++ src/app/components/selectors/muscle-selector/muscle-selector.component.spec.ts
@@ -1,17 +1,25 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { MuscleSelectorComponent } from './muscle-selector.component';
+import {MuscleSelectorComponent} from './muscle-selector.component';
+import {MuscleService} from "../../../services/muscle/muscle.service";
+import {Muscle} from "../../../interface/dto/muscle";
+import {BehaviorSubject} from "rxjs";
 
 describe('MuscleSelectorComponent', () => {
   let component: MuscleSelectorComponent;
   let fixture: ComponentFixture<MuscleSelectorComponent>;
+  let mockMuscleService: jasmine.SpyObj<MuscleService> =
+    jasmine.createSpyObj('MuscleService', ['muscles', 'isLoading']);
+  mockMuscleService.muscles = new BehaviorSubject<Muscle[]>([]);
+  mockMuscleService.isLoading = new BehaviorSubject<boolean>(true);
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      providers: [{provide: MuscleService, useValue: mockMuscleService}],
       imports: [MuscleSelectorComponent]
     })
-    .compileComponents();
-    
+      .compileComponents();
+
     fixture = TestBed.createComponent(MuscleSelectorComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
