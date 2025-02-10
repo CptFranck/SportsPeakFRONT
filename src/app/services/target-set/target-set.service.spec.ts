92d3e33735a2d2eb826417f57f12f05f3892b98b
CptFranck <francois.caillet71@gmail.com>
2025-02-10 20:05:48 +0100

    Fix should create test on user TargetSetService service

--- src/app/services/target-set/target-set.service.spec.ts
+++ src/app/services/target-set/target-set.service.spec.ts
@@ -1,12 +1,21 @@
-import { TestBed } from '@angular/core/testing';
+import {TestBed} from '@angular/core/testing';
 
-import { TargetSetService } from './target-set.service';
+import {TargetSetService} from './target-set.service';
+import {ApolloTestingModule} from "apollo-angular/testing";
+import {HttpClientTestingModule} from "@angular/common/http/testing";
 
 describe('TargetSetService', () => {
   let service: TargetSetService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule(
+      {
+        imports: [
+          ApolloTestingModule,
+          HttpClientTestingModule
+        ],
+      }
+    );
     service = TestBed.inject(TargetSetService);
   });
 
