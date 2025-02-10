f113517eab7c82ad3e1d87fba6396d92d6d9594f
CptFranck <francois.caillet71@gmail.com>
2025-02-10 20:05:28 +0100

    Fix should create test on user RoleService service

--- src/app/services/role/role.service.spec.ts
+++ src/app/services/role/role.service.spec.ts
@@ -1,12 +1,19 @@
-import { TestBed } from '@angular/core/testing';
+import {TestBed} from '@angular/core/testing';
 
-import { RoleService } from './role.service';
+import {RoleService} from './role.service';
+import {ApolloTestingModule} from "apollo-angular/testing";
+import {HttpClientTestingModule} from "@angular/common/http/testing";
 
 describe('RoleService', () => {
   let service: RoleService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [
+        ApolloTestingModule,
+        HttpClientTestingModule
+      ],
+    });
     service = TestBed.inject(RoleService);
   });
 
