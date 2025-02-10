b5abb3d3febfd5ba633fdde3bad371f6ee6e8e80
CptFranck <francois.caillet71@gmail.com>
2025-02-10 19:56:35 +0100

    Fix should create test on user selector component

--- src/app/components/selectors/user-selector/user-selector.component.spec.ts
+++ src/app/components/selectors/user-selector/user-selector.component.spec.ts
@@ -1,17 +1,25 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { UserSelectorComponent } from './user-selector.component';
+import {UserSelectorComponent} from './user-selector.component';
+import {BehaviorSubject} from "rxjs";
+import {UserService} from "../../../services/user/user.service";
+import {User} from "../../../interface/dto/user";
 
 describe('UserSelectorComponent', () => {
   let component: UserSelectorComponent;
   let fixture: ComponentFixture<UserSelectorComponent>;
+  let mockUserService: jasmine.SpyObj<UserService> =
+    jasmine.createSpyObj('UserService', ['muscles', 'isLoading']);
+  mockUserService.users = new BehaviorSubject<User[]>([]);
+  mockUserService.isLoading = new BehaviorSubject<boolean>(true);
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      providers: [{provide: UserService, useValue: mockUserService}],
       imports: [UserSelectorComponent]
     })
-    .compileComponents();
-    
+      .compileComponents();
+
     fixture = TestBed.createComponent(UserSelectorComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
