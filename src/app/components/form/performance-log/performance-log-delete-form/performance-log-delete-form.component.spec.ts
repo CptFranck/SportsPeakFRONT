851fca989225fe290b11143fa6650a80b73487b1
CptFranck <francois.caillet71@gmail.com>
2025-02-10 19:54:37 +0100

    Fix should create test on performance log delete form component

--- src/app/components/form/performance-log/performance-log-delete-form/performance-log-delete-form.component.spec.ts
+++ src/app/components/form/performance-log/performance-log-delete-form/performance-log-delete-form.component.spec.ts
@@ -1,17 +1,21 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { PerformanceLogDeleteFormComponent } from './performance-log-delete-form.component';
+import {PerformanceLogDeleteFormComponent} from './performance-log-delete-form.component';
+import {MuscleService} from "../../../../services/muscle/muscle.service";
+import {PerformanceLogService} from "../../../../services/performance-log/performance-log.service";
 
 describe('PerformanceLogDeleteFormComponent', () => {
   let component: PerformanceLogDeleteFormComponent;
   let fixture: ComponentFixture<PerformanceLogDeleteFormComponent>;
+  let mockPerformanceLogService: jasmine.SpyObj<MuscleService>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      providers: [{provide: PerformanceLogService, useValue: mockPerformanceLogService}],
       imports: [PerformanceLogDeleteFormComponent]
     })
-    .compileComponents();
-    
+      .compileComponents();
+
     fixture = TestBed.createComponent(PerformanceLogDeleteFormComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
