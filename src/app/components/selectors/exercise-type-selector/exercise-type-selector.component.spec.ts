0f999d1033933732f2b7417f2cc24a8b90d85d2f
CptFranck <francois.caillet71@gmail.com>
2025-02-10 19:51:38 +0100

    Fix should create test on exercise type selector component

--- src/app/components/selectors/exercise-type-selector/exercise-type-selector.component.spec.ts
+++ src/app/components/selectors/exercise-type-selector/exercise-type-selector.component.spec.ts
@@ -1,17 +1,26 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { ExerciseTypeSelectorComponent } from './exercise-type-selector.component';
+import {ExerciseTypeSelectorComponent} from './exercise-type-selector.component';
+import {BehaviorSubject} from "rxjs";
+import {ExerciseTypeService} from "../../../services/exercise-type/exercise-type.service";
+import {ExerciseType} from "../../../interface/dto/exercise-type";
 
 describe('ExerciseTypeSelectorComponent', () => {
   let component: ExerciseTypeSelectorComponent;
   let fixture: ComponentFixture<ExerciseTypeSelectorComponent>;
+  let mockExerciseTypeService: jasmine.SpyObj<ExerciseTypeService> =
+    jasmine.createSpyObj('MuscleService', ['muscles', 'isLoading']);
+  mockExerciseTypeService.exerciseTypes = new BehaviorSubject<ExerciseType[]>([]);
+  mockExerciseTypeService.isLoading = new BehaviorSubject<boolean>(true);
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      providers: [{provide: ExerciseTypeService, useValue: mockExerciseTypeService}],
+
       imports: [ExerciseTypeSelectorComponent]
     })
-    .compileComponents();
-    
+      .compileComponents();
+
     fixture = TestBed.createComponent(ExerciseTypeSelectorComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
