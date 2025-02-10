c8828acbbf6ca121d721eee8ed1e2b1bc24f143b
CptFranck <francois.caillet71@gmail.com>
2025-02-10 19:51:03 +0100

    Fix should create test on exercise selector component

--- src/app/components/selectors/exercise-selector/exercise-selector.component.spec.ts
+++ src/app/components/selectors/exercise-selector/exercise-selector.component.spec.ts
@@ -1,13 +1,22 @@
 import {ComponentFixture, TestBed} from '@angular/core/testing';
 
 import {ExerciseSelectorComponent} from './exercise-selector.component';
+import {MuscleService} from "../../../services/muscle/muscle.service";
+import {BehaviorSubject} from "rxjs";
+import {ExerciseService} from "../../../services/exercise/exercise.service";
+import {Exercise} from "../../../interface/dto/exercise";
 
 describe('SelectExercisesComponent', () => {
   let component: ExerciseSelectorComponent;
   let fixture: ComponentFixture<ExerciseSelectorComponent>;
+  let mockExerciseService: jasmine.SpyObj<ExerciseService> =
+    jasmine.createSpyObj('MuscleService', ['muscles', 'isLoading']);
+  mockExerciseService.exercises = new BehaviorSubject<Exercise[]>([]);
+  mockExerciseService.isLoading = new BehaviorSubject<boolean>(true);
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      providers: [{provide: ExerciseService, useValue: mockExerciseService}],
       imports: [ExerciseSelectorComponent]
     })
       .compileComponents();
