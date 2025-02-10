c463580631dfe3ae386156dbc5647d69fc2f7453
CptFranck <francois.caillet71@gmail.com>
2025-02-10 19:55:39 +0100

    Fix should create test on register form component

--- src/app/components/form/auth/register-form/register-form.component.spec.ts
+++ src/app/components/form/auth/register-form/register-form.component.spec.ts
@@ -1,17 +1,20 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { RegisterFormComponent } from './register-form.component';
+import {RegisterFormComponent} from './register-form.component';
+import {AuthService} from "../../../../services/auth/auth.service";
 
 describe('RegisterFormComponent', () => {
   let component: RegisterFormComponent;
   let fixture: ComponentFixture<RegisterFormComponent>;
+  let mockAuthService: jasmine.SpyObj<AuthService>
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      providers: [{provide: AuthService, useValue: mockAuthService}],
       imports: [RegisterFormComponent]
     })
-    .compileComponents();
-    
+      .compileComponents();
+
     fixture = TestBed.createComponent(RegisterFormComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
