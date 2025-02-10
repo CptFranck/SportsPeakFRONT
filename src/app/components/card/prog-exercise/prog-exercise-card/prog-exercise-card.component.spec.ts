45957fab22334892555f4c6b7be722effcf49091
CptFranck <francois.caillet71@gmail.com>
2025-02-10 19:55:11 +0100

    Fix should create test on prog exercise card component

--- src/app/components/card/prog-exercise/prog-exercise-card/prog-exercise-card.component.spec.ts
+++ src/app/components/card/prog-exercise/prog-exercise-card/prog-exercise-card.component.spec.ts
@@ -1,6 +1,11 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { ProgExerciseCardComponent } from './prog-exercise-card.component';
+import {ProgExerciseCardComponent} from './prog-exercise-card.component';
+import {provideRouter} from "@angular/router";
+import {generateTestExercise, generateTestProgExercise, generateTestUser} from "../../../../utils/testFunctions";
+import {User} from "../../../../interface/dto/user";
+import {Exercise} from "../../../../interface/dto/exercise";
+import {ProgExercise} from "../../../../interface/dto/prog-exercise";
 
 describe('ProgExerciseCardComponent', () => {
   let component: ProgExerciseCardComponent;
@@ -8,16 +13,26 @@ describe('ProgExerciseCardComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [ProgExerciseCardComponent]
+      imports: [ProgExerciseCardComponent],
+      providers: [provideRouter([])],
     })
-    .compileComponents();
-    
+      .compileComponents();
+
+    const userMock: User = generateTestUser();
+    const exerciseMock: Exercise = generateTestExercise();
+    const mockProgExercise: ProgExercise = generateTestProgExercise(userMock, exerciseMock);
+
     fixture = TestBed.createComponent(ProgExerciseCardComponent);
     component = fixture.componentInstance;
+
+    component.progExercise = mockProgExercise
     fixture.detectChanges();
   });
 
   it('should create', () => {
+
+    fixture.detectChanges();
+
     expect(component).toBeTruthy();
   });
 });
