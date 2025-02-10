fab72e38c7c120a68ad8a32c632e7f5081698ce5
CptFranck <francois.caillet71@gmail.com>
2025-02-10 20:01:35 +0100

    Fix should create test on user ExerciseService service

--- src/app/services/exercise/exercise.service.spec.ts
+++ src/app/services/exercise/exercise.service.spec.ts
@@ -1,12 +1,19 @@
-import { TestBed } from '@angular/core/testing';
+import {TestBed} from '@angular/core/testing';
 
-import { ExerciseService } from './exercise.service';
+import {ExerciseService} from './exercise.service';
+import {ApolloTestingModule} from "apollo-angular/testing";
+import {HttpClientTestingModule} from "@angular/common/http/testing";
 
 describe('ExerciseService', () => {
   let service: ExerciseService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [
+        ApolloTestingModule,
+        HttpClientTestingModule
+      ],
+    });
     service = TestBed.inject(ExerciseService);
   });
 
