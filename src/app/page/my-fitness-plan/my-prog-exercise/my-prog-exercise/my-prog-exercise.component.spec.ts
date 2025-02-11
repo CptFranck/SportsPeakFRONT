5f34c618243328e5780dd4cfe395dc3f2e5e7ce7
CptFranck <francois.caillet71@gmail.com>
2025-02-11 18:53:24 +0100

    add missing mocked service to MyProgExerciseComponent

--- src/app/page/my-fitness-plan/my-prog-exercise/my-prog-exercise/my-prog-exercise.component.spec.ts
+++ src/app/page/my-fitness-plan/my-prog-exercise/my-prog-exercise/my-prog-exercise.component.spec.ts
@@ -1,17 +1,42 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { MyProgExerciseComponent } from './my-prog-exercise.component';
+import {MyProgExerciseComponent} from './my-prog-exercise.component';
+import {provideRouter} from "@angular/router";
+import {BehaviorSubject} from "rxjs";
+import {ProgExerciseService} from "../../../../services/prog-exercise/prog-exercise.service";
+import {ProgExercise} from "../../../../interface/dto/prog-exercise";
+import {ExerciseService} from "../../../../services/exercise/exercise.service";
+import {Exercise} from "../../../../interface/dto/exercise";
+import {TargetSetService} from "../../../../services/target-set/target-set.service";
 
 describe('MyProgExerciseComponent', () => {
   let component: MyProgExerciseComponent;
   let fixture: ComponentFixture<MyProgExerciseComponent>;
 
+  let mockProgExerciseService: jasmine.SpyObj<ProgExerciseService> =
+    jasmine.createSpyObj('MuscleService', ['progExercise', 'isLoading']);
+  mockProgExerciseService.progExercise = new BehaviorSubject<ProgExercise | undefined>(undefined);
+  mockProgExerciseService.isLoading = new BehaviorSubject<boolean>(true);
+
+  let mockExerciseService: jasmine.SpyObj<ExerciseService> =
+    jasmine.createSpyObj('ExerciseService', ['exercises', 'isLoading']);
+  mockExerciseService.exercises = new BehaviorSubject<Exercise[]>([]);
+  mockExerciseService.isLoading = new BehaviorSubject<boolean>(true);
+
+  let mockTargetSetService: jasmine.SpyObj<TargetSetService>;
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [MyProgExerciseComponent]
+      providers: [
+        provideRouter([]),
+        {provide: ExerciseService, useValue: mockExerciseService},
+        {provide: TargetSetService, useValue: mockTargetSetService},
+        {provide: ProgExerciseService, useValue: mockProgExerciseService},
+      ],
+      imports: [MyProgExerciseComponent],
     })
-    .compileComponents();
-    
+      .compileComponents();
+
     fixture = TestBed.createComponent(MyProgExerciseComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
