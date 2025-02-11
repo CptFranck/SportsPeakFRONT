f4e4fc358c46ceca666fafb16cfdcd6e27381698
CptFranck <francois.caillet71@gmail.com>
2025-02-11 18:37:17 +0100

    add missing mocked service to UserComponent

--- src/app/page/user-account/user/user.component.spec.ts
+++ src/app/page/user-account/user/user.component.spec.ts
@@ -1,17 +1,23 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { UserComponent } from './user.component';
+import {UserComponent} from './user.component';
+import {UserService} from "../../../services/user/user.service";
 
 describe('UserComponent', () => {
   let component: UserComponent;
   let fixture: ComponentFixture<UserComponent>;
 
+  let mockUserService: jasmine.SpyObj<UserService>;
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      providers: [
+        {provide: UserService, useValue: mockUserService}
+      ],
       imports: [UserComponent]
     })
-    .compileComponents();
-    
+      .compileComponents();
+
     fixture = TestBed.createComponent(UserComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
