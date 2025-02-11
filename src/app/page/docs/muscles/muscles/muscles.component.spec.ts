1f4be66552b07f7cf01fe697f4e3304c1c226c57
CptFranck <francois.caillet71@gmail.com>
2025-02-11 18:54:05 +0100

    add missing mocked service to MusclesComponent

--- src/app/page/docs/muscles/muscles/muscles.component.spec.ts
+++ src/app/page/docs/muscles/muscles/muscles.component.spec.ts
@@ -1,17 +1,36 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { MusclesComponent } from './muscles.component';
+import {MusclesComponent} from './muscles.component';
+import {MuscleService} from "../../../../services/muscle/muscle.service";
+import {BehaviorSubject} from "rxjs";
+import {Muscle} from "../../../../interface/dto/muscle";
+import {ExerciseService} from "../../../../services/exercise/exercise.service";
+import {Exercise} from "../../../../interface/dto/exercise";
 
 describe('MusclesComponent', () => {
   let component: MusclesComponent;
   let fixture: ComponentFixture<MusclesComponent>;
 
+  let mockExerciseService: jasmine.SpyObj<ExerciseService> =
+    jasmine.createSpyObj('ExerciseService', ['exercises', 'isLoading']);
+  mockExerciseService.exercises = new BehaviorSubject<Exercise[]>([]);
+  mockExerciseService.isLoading = new BehaviorSubject<boolean>(true);
+
+  let mockMuscleService: jasmine.SpyObj<MuscleService> =
+    jasmine.createSpyObj('MuscleService', ['muscles', 'isLoading']);
+  mockMuscleService.muscles = new BehaviorSubject<Muscle[]>([]);
+  mockMuscleService.isLoading = new BehaviorSubject<boolean>(true);
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [MusclesComponent]
+      providers: [
+        {provide: MuscleService, useValue: mockMuscleService},
+        {provide: ExerciseService, useValue: mockExerciseService},
+      ],
+      imports: [MusclesComponent],
     })
-    .compileComponents();
-    
+      .compileComponents();
+
     fixture = TestBed.createComponent(MusclesComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
