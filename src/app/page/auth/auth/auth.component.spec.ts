44d6208095e6e98896b7e60f443d8872c1f0411c
CptFranck <francois.caillet71@gmail.com>
2025-02-11 19:04:56 +0100

    add missing mocked service LoginComponent

--- src/app/page/auth/auth/auth.component.spec.ts
+++ src/app/page/auth/auth/auth.component.spec.ts
@@ -1,13 +1,22 @@
 import {ComponentFixture, TestBed} from '@angular/core/testing';
 
 import {AuthComponent} from './auth.component';
+import {AuthService} from "../../../services/auth/auth.service";
+import {BehaviorSubject} from "rxjs";
 
 describe('LoginComponent', () => {
   let component: AuthComponent;
   let fixture: ComponentFixture<AuthComponent>;
 
+  let mockAuthService: jasmine.SpyObj<AuthService> =
+    jasmine.createSpyObj('AuthService', ['isAuthenticated']);
+  mockAuthService.isAuthenticated = new BehaviorSubject<boolean>(false);
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      providers: [
+        {provide: AuthService, useValue: mockAuthService}
+      ],
       imports: [AuthComponent]
     })
       .compileComponents();
