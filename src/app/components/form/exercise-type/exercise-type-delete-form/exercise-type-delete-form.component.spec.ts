e1acf61d2807aa80f5ab757b370681e572a97d4d
CptFranck <francois.caillet71@gmail.com>
2025-02-11 19:00:51 +0100

    add missing mocked service ExerciseTypeDeleteFormComponent

--- src/app/components/form/exercise-type/exercise-type-delete-form/exercise-type-delete-form.component.spec.ts
+++ src/app/components/form/exercise-type/exercise-type-delete-form/exercise-type-delete-form.component.spec.ts
@@ -1,17 +1,23 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { ExerciseTypeDeleteFormComponent } from './exercise-type-delete-form.component';
+import {ExerciseTypeDeleteFormComponent} from './exercise-type-delete-form.component';
+import {ExerciseTypeService} from "../../../../services/exercise-type/exercise-type.service";
 
 describe('ExerciseTypeDeleteFormComponent', () => {
   let component: ExerciseTypeDeleteFormComponent;
   let fixture: ComponentFixture<ExerciseTypeDeleteFormComponent>;
 
+  let mockExerciseTypeService: jasmine.SpyObj<ExerciseTypeService>;
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      providers: [
+        {provide: ExerciseTypeService, useValue: mockExerciseTypeService},
+      ],
       imports: [ExerciseTypeDeleteFormComponent]
     })
-    .compileComponents();
-    
+      .compileComponents();
+
     fixture = TestBed.createComponent(ExerciseTypeDeleteFormComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
