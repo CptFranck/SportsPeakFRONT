1f0daf14a1e3bd5b36a98f70b270c05cf7fb9391
CptFranck <francois.caillet71@gmail.com>
2025-02-11 18:47:50 +0100

    add missing mocked service to PrivilegeDeleteFormsComponent

--- src/app/components/form/privilege/privilege-delete-form/privilege-delete-form.component.spec.ts
+++ src/app/components/form/privilege/privilege-delete-form/privilege-delete-form.component.spec.ts
@@ -1,13 +1,19 @@
 import {ComponentFixture, TestBed} from '@angular/core/testing';
 
 import {PrivilegeDeleteFormComponent} from './privilege-delete-form.component';
+import {PrivilegeService} from "../../../../services/privilege/privilege.service";
 
 describe('PrivilegeDeleteFormsComponent', () => {
   let component: PrivilegeDeleteFormComponent;
   let fixture: ComponentFixture<PrivilegeDeleteFormComponent>;
 
+  let mockPrivilegeService: jasmine.SpyObj<PrivilegeService>;
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      providers: [
+        {provide: PrivilegeService, useValue: mockPrivilegeService},
+      ],
       imports: [PrivilegeDeleteFormComponent]
     })
       .compileComponents();
