33236293f7973c30fa99050b385bfdf3a6c4c56a
CptFranck <francois.caillet71@gmail.com>
2025-02-11 18:54:53 +0100

    add missing mocked service to DeleteMuscleComponent

--- src/app/components/form/muscle/muscle-delete-form/muscle-delete-form.component.spec.ts
+++ src/app/components/form/muscle/muscle-delete-form/muscle-delete-form.component.spec.ts
@@ -1,13 +1,19 @@
 import {ComponentFixture, TestBed} from '@angular/core/testing';
 
 import {MuscleDeleteFormComponent} from './muscle-delete-form.component';
+import {MuscleService} from "../../../../services/muscle/muscle.service";
 
 describe('DeleteMuscleComponent', () => {
   let component: MuscleDeleteFormComponent;
   let fixture: ComponentFixture<MuscleDeleteFormComponent>;
 
+  let mockMuscleService: jasmine.SpyObj<MuscleService>;
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      providers: [
+        {provide: MuscleService, useValue: mockMuscleService}
+      ],
       imports: [MuscleDeleteFormComponent]
     })
       .compileComponents();
