369849f489ee80436d26fac13f1a64710bb9f6d1
CptFranck <francois.caillet71@gmail.com>
2025-02-11 18:40:32 +0100

    add missing mocked service to TargetSetDeleteFormComponent

--- src/app/components/form/target-set/target-set-delete-form/target-set-delete-form.component.spec.ts
+++ src/app/components/form/target-set/target-set-delete-form/target-set-delete-form.component.spec.ts
@@ -1,17 +1,23 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { TargetSetDeleteFormComponent } from './target-set-delete-form.component';
+import {TargetSetDeleteFormComponent} from './target-set-delete-form.component';
+import {TargetSetService} from "../../../../services/target-set/target-set.service";
 
 describe('TargetSetDeleteFormComponent', () => {
   let component: TargetSetDeleteFormComponent;
   let fixture: ComponentFixture<TargetSetDeleteFormComponent>;
 
+  let mockTargetSetService: jasmine.SpyObj<TargetSetService>;
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      providers: [
+        {provide: TargetSetService, useValue: mockTargetSetService},
+      ],
       imports: [TargetSetDeleteFormComponent]
     })
-    .compileComponents();
-    
+      .compileComponents();
+
     fixture = TestBed.createComponent(TargetSetDeleteFormComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
