1c112c00639798a0eac95f3834161f59e322ee1c
CptFranck <francois.caillet71@gmail.com>
2025-02-11 19:59:14 +0100

    add missing input ProgExerciseCardDetailsComponent

--- src/app/components/card/prog-exercise/prog-exercise-card-details/prog-exercise-card-details.component.spec.ts
+++ src/app/components/card/prog-exercise/prog-exercise-card-details/prog-exercise-card-details.component.spec.ts
@@ -1,19 +1,37 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { ProgExerciseCardDetailsComponent } from './prog-exercise-card-details.component';
+import {ProgExerciseCardDetailsComponent} from './prog-exercise-card-details.component';
+import {ProgExerciseService} from "../../../../services/prog-exercise/prog-exercise.service";
+import {BehaviorSubject} from "rxjs";
+import {ProgExercise} from "../../../../interface/dto/prog-exercise";
+import {generateTestExercise, generateTestProgExercise, generateTestUser} from "../../../../utils/testFunctions";
+import {User} from "../../../../interface/dto/user";
+import {Exercise} from "../../../../interface/dto/exercise";
 
 describe('ProgExerciseCardDetailsComponent', () => {
   let component: ProgExerciseCardDetailsComponent;
   let fixture: ComponentFixture<ProgExerciseCardDetailsComponent>;
 
+  let mockProgExerciseService: jasmine.SpyObj<ProgExerciseService> =
+    jasmine.createSpyObj('MuscleService', ['progExercise', 'isLoading']);
+  mockProgExerciseService.progExercise = new BehaviorSubject<ProgExercise | undefined>(undefined);
+  mockProgExerciseService.isLoading = new BehaviorSubject<boolean>(true);
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      providers: [
+        {provide: ProgExerciseService, useValue: mockProgExerciseService},
+      ],
       imports: [ProgExerciseCardDetailsComponent]
     })
-    .compileComponents();
-    
+      .compileComponents();
+
     fixture = TestBed.createComponent(ProgExerciseCardDetailsComponent);
     component = fixture.componentInstance;
+
+    let mockUser: User = generateTestUser();
+    let mockExercise: Exercise = generateTestExercise();
+    component.progExercise = generateTestProgExercise(mockUser, mockExercise);
     fixture.detectChanges();
   });
 
