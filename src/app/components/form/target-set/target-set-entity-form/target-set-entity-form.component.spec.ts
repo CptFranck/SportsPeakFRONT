a39e3ecb249858a5441bf12124b2a51ea091882f
CptFranck <francois.caillet71@gmail.com>
2025-02-11 18:38:45 +0100

    add missing mocked service to TargetSetEntityFormComponent

--- src/app/components/form/target-set/target-set-entity-form/target-set-entity-form.component.spec.ts
+++ src/app/components/form/target-set/target-set-entity-form/target-set-entity-form.component.spec.ts
@@ -1,17 +1,23 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { TargetSetEntityFormComponent } from './target-set-entity-form.component';
+import {TargetSetEntityFormComponent} from './target-set-entity-form.component';
+import {TargetSetService} from "../../../../services/target-set/target-set.service";
 
 describe('TargetSetEntityFormComponent', () => {
   let component: TargetSetEntityFormComponent;
   let fixture: ComponentFixture<TargetSetEntityFormComponent>;
 
+  let mockTargetSetService: jasmine.SpyObj<TargetSetService>;
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      providers: [
+        {provide: TargetSetService, useValue: mockTargetSetService}
+      ],
       imports: [TargetSetEntityFormComponent]
     })
-    .compileComponents();
-    
+      .compileComponents();
+
     fixture = TestBed.createComponent(TargetSetEntityFormComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
