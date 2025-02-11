31a017551e08963ff365c66ab41584203618d7e1
CptFranck <francois.caillet71@gmail.com>
2025-02-11 18:35:41 +0100

    add missing mocked service to UserRoleFormComponent

--- src/app/components/form/user-management/user-entity-form/user-roles-form.component.spec.ts
+++ src/app/components/form/user-management/user-entity-form/user-roles-form.component.spec.ts
@@ -1,13 +1,28 @@
 import {ComponentFixture, TestBed} from '@angular/core/testing';
 
 import {UserRolesFormComponent} from './user-roles-form.component';
+import {UserService} from "../../../../services/user/user.service";
+import {BehaviorSubject} from "rxjs";
+import {RoleService} from "../../../../services/role/role.service";
+import {Role} from "../../../../interface/dto/role";
 
-describe('UserEntityFormComponent', () => {
+describe('UserRolesFormComponent', () => {
   let component: UserRolesFormComponent;
   let fixture: ComponentFixture<UserRolesFormComponent>;
 
+  let mockUserService: jasmine.SpyObj<UserService>;
+
+  let mockRoleService: jasmine.SpyObj<RoleService> =
+    jasmine.createSpyObj('RoleService', ['roles', 'isLoading']);
+  mockRoleService.roles = new BehaviorSubject<Role[]>([]);
+  mockRoleService.isLoading = new BehaviorSubject<boolean>(true);
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      providers: [
+        {provide: UserService, useValue: mockUserService},
+        {provide: RoleService, useValue: mockRoleService},
+      ],
       imports: [UserRolesFormComponent]
     })
       .compileComponents();
