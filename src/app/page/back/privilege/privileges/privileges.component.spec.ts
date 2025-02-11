707003e03885fdbcb4734a0c76aaad8db690c3bf
CptFranck <francois.caillet71@gmail.com>
2025-02-11 18:46:28 +0100

    add missing mocked service to PrivilegesComponent

--- src/app/page/back/privilege/privileges/privileges.component.spec.ts
+++ src/app/page/back/privilege/privileges/privileges.component.spec.ts
@@ -1,17 +1,36 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { PrivilegesComponent } from './privileges.component';
+import {PrivilegesComponent} from './privileges.component';
+import {BehaviorSubject} from "rxjs";
+import {PrivilegeService} from "../../../../services/privilege/privilege.service";
+import {Privilege} from "../../../../interface/dto/privilege";
+import {RoleService} from "../../../../services/role/role.service";
+import {Role} from "../../../../interface/dto/role";
 
 describe('PrivilegesComponent', () => {
   let component: PrivilegesComponent;
   let fixture: ComponentFixture<PrivilegesComponent>;
 
+  let mockPrivilegeService: jasmine.SpyObj<PrivilegeService> =
+    jasmine.createSpyObj('PrivilegeService', ['privileges', 'isLoading']);
+  mockPrivilegeService.privileges = new BehaviorSubject<Privilege[]>([]);
+  mockPrivilegeService.isLoading = new BehaviorSubject<boolean>(true);
+
+  let mockRoleService: jasmine.SpyObj<RoleService> =
+    jasmine.createSpyObj('RoleService', ['roles', 'isLoading']);
+  mockRoleService.roles = new BehaviorSubject<Role[]>([]);
+  mockRoleService.isLoading = new BehaviorSubject<boolean>(true);
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      providers: [
+        {provide: RoleService, useValue: mockRoleService},
+        {provide: PrivilegeService, useValue: mockPrivilegeService},
+      ],
       imports: [PrivilegesComponent]
     })
-    .compileComponents();
-    
+      .compileComponents();
+
     fixture = TestBed.createComponent(PrivilegesComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
