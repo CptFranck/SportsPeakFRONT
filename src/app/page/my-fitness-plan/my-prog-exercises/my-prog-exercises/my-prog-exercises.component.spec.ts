720e31d1263c95335311f1d343513f72e92d7bc9
CptFranck <francois.caillet71@gmail.com>
2025-02-11 18:51:42 +0100

    add missing mocked service to MyProgExercisesComponent

--- src/app/page/my-fitness-plan/my-prog-exercises/my-prog-exercises/my-prog-exercises.component.spec.ts
+++ src/app/page/my-fitness-plan/my-prog-exercises/my-prog-exercises/my-prog-exercises.component.spec.ts
@@ -1,17 +1,36 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { MyProgExercisesComponent } from './my-prog-exercises.component';
+import {MyProgExercisesComponent} from './my-prog-exercises.component';
+import {BehaviorSubject} from "rxjs";
+import {ProgExerciseService} from "../../../../services/prog-exercise/prog-exercise.service";
+import {ProgExercise} from "../../../../interface/dto/prog-exercise";
+import {ExerciseService} from "../../../../services/exercise/exercise.service";
+import {Exercise} from "../../../../interface/dto/exercise";
 
 describe('MyProgExercisesComponent', () => {
   let component: MyProgExercisesComponent;
   let fixture: ComponentFixture<MyProgExercisesComponent>;
 
+  let mockExerciseService: jasmine.SpyObj<ExerciseService> =
+    jasmine.createSpyObj('ExerciseService', ['exercises', 'isLoading']);
+  mockExerciseService.exercises = new BehaviorSubject<Exercise[]>([]);
+  mockExerciseService.isLoading = new BehaviorSubject<boolean>(true);
+
+  let mockProgExerciseService: jasmine.SpyObj<ProgExerciseService> =
+    jasmine.createSpyObj('ProgExerciseService', ['progExercise', 'isLoading']);
+  mockProgExerciseService.userProgExercises = new BehaviorSubject<ProgExercise[]>([]);
+  mockProgExerciseService.isLoading = new BehaviorSubject<boolean>(true);
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      providers: [
+        {provide: ExerciseService, useValue: mockExerciseService},
+        {provide: ProgExerciseService, useValue: mockProgExerciseService}
+      ],
       imports: [MyProgExercisesComponent]
     })
-    .compileComponents();
-    
+      .compileComponents();
+
     fixture = TestBed.createComponent(MyProgExercisesComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
