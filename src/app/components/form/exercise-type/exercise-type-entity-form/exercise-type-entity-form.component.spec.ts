dd038ffaf888d4172af7beac13ec30603f91dda9
CptFranck <francois.caillet71@gmail.com>
2025-02-11 19:00:20 +0100

    add missing mocked service ExerciseTypeEntityFormComponent

--- src/app/components/form/exercise-type/exercise-type-entity-form/exercise-type-entity-form.component.spec.ts
+++ src/app/components/form/exercise-type/exercise-type-entity-form/exercise-type-entity-form.component.spec.ts
@@ -1,17 +1,32 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { ExerciseTypeEntityFormComponent } from './exercise-type-entity-form.component';
+import {ExerciseTypeEntityFormComponent} from './exercise-type-entity-form.component';
+import {ExerciseTypeService} from "../../../../services/exercise-type/exercise-type.service";
+import {BehaviorSubject} from "rxjs";
+import {ExerciseService} from "../../../../services/exercise/exercise.service";
+import {Exercise} from "../../../../interface/dto/exercise";
 
 describe('ExerciseTypeEntityFormComponent', () => {
   let component: ExerciseTypeEntityFormComponent;
   let fixture: ComponentFixture<ExerciseTypeEntityFormComponent>;
 
+  let mockExerciseTypeService: jasmine.SpyObj<ExerciseTypeService>;
+
+  let mockExerciseService: jasmine.SpyObj<ExerciseService> =
+    jasmine.createSpyObj('ExerciseService', ['exercises', 'isLoading']);
+  mockExerciseService.exercises = new BehaviorSubject<Exercise[]>([]);
+  mockExerciseService.isLoading = new BehaviorSubject<boolean>(true);
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      providers: [
+        {provide: ExerciseService, useValue: mockExerciseService},
+        {provide: ExerciseTypeService, useValue: mockExerciseTypeService},
+      ],
       imports: [ExerciseTypeEntityFormComponent]
     })
-    .compileComponents();
-    
+      .compileComponents();
+
     fixture = TestBed.createComponent(ExerciseTypeEntityFormComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
