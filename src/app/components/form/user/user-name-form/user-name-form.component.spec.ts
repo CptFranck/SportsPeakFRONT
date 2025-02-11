50677291dec9efefe6a1b721916fae56505ec050
CptFranck <francois.caillet71@gmail.com>
2025-02-11 19:16:43 +0100

    add missing mocked service UserNameFormComponent

--- src/app/components/form/user/user-name-form/user-name-form.component.spec.ts
+++ src/app/components/form/user/user-name-form/user-name-form.component.spec.ts
@@ -1,17 +1,23 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { UserNameFormComponent } from './user-name-form.component';
+import {UserNameFormComponent} from './user-name-form.component';
+import {UserService} from "../../../../services/user/user.service";
 
 describe('UserNameFormComponent', () => {
   let component: UserNameFormComponent;
   let fixture: ComponentFixture<UserNameFormComponent>;
 
+  let mockUserService: jasmine.SpyObj<UserService>;
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      providers: [
+        {provide: UserService, useValue: mockUserService}
+      ],
       imports: [UserNameFormComponent]
     })
-    .compileComponents();
-    
+      .compileComponents();
+
     fixture = TestBed.createComponent(UserNameFormComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
