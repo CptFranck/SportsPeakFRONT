8b3d8438fc9f600f3accf3739060286ebdec4766
CptFranck <francois.caillet71@gmail.com>
2025-02-11 18:50:37 +0100

    add missing mocked service to NavBarDocMenuComponent

--- src/app/components/nav-bar-element/nav-bar-main-menu-components/nav-bar-doc-menu/nav-bar-doc-menu.component.spec.ts
+++ src/app/components/nav-bar-element/nav-bar-main-menu-components/nav-bar-doc-menu/nav-bar-doc-menu.component.spec.ts
@@ -1,13 +1,17 @@
 import {ComponentFixture, TestBed} from '@angular/core/testing';
 
 import {NavBarDocMenuComponent} from './nav-bar-doc-menu.component';
+import {provideRouter} from "@angular/router";
 
-describe('NavBarExploreMenuComponent', () => {
+describe('NavBarDocMenuComponent', () => {
   let component: NavBarDocMenuComponent;
   let fixture: ComponentFixture<NavBarDocMenuComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      providers: [
+        provideRouter([])
+      ],
       imports: [NavBarDocMenuComponent]
     })
       .compileComponents();
