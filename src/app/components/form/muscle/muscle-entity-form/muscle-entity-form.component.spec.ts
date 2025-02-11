f76143a9f28488284d56b9e94e7eae2b992919b6
CptFranck <francois.caillet71@gmail.com>
2025-02-11 19:04:29 +0100

    add missing mocked service MuscleFormComponent

--- src/app/components/form/muscle/muscle-entity-form/muscle-entity-form.component.spec.ts
+++ src/app/components/form/muscle/muscle-entity-form/muscle-entity-form.component.spec.ts
@@ -1,13 +1,28 @@
 import {ComponentFixture, TestBed} from '@angular/core/testing';
 
 import {MuscleEntityFormComponent} from './muscle-entity-form.component';
+import {MuscleService} from "../../../../services/muscle/muscle.service";
+import {ExerciseService} from "../../../../services/exercise/exercise.service";
+import {BehaviorSubject} from "rxjs";
+import {Exercise} from "../../../../interface/dto/exercise";
 
 describe('MuscleFormComponent', () => {
   let component: MuscleEntityFormComponent;
   let fixture: ComponentFixture<MuscleEntityFormComponent>;
 
+  let mockMuscleService: jasmine.SpyObj<MuscleService>;
+
+  let mockExerciseService: jasmine.SpyObj<ExerciseService> =
+    jasmine.createSpyObj('ExerciseService', ['exercises', 'isLoading']);
+  mockExerciseService.exercises = new BehaviorSubject<Exercise[]>([]);
+  mockExerciseService.isLoading = new BehaviorSubject<boolean>(true);
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      providers: [
+        {provide: MuscleService, useValue: mockMuscleService},
+        {provide: ExerciseService, useValue: mockExerciseService}
+      ],
       imports: [MuscleEntityFormComponent]
     })
       .compileComponents();
