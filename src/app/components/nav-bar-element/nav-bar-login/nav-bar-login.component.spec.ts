c218181f51a6221580501b0dcaeb9b90e33832f7
CptFranck <francois.caillet71@gmail.com>
2025-02-11 18:48:47 +0100

    add missing mocked service to NavBarLoginComponent

--- src/app/components/nav-bar-element/nav-bar-login/nav-bar-login.component.spec.ts
+++ src/app/components/nav-bar-element/nav-bar-login/nav-bar-login.component.spec.ts
@@ -1,17 +1,28 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { NavBarLoginComponent } from './nav-bar-login.component';
+import {NavBarLoginComponent} from './nav-bar-login.component';
+import {AuthService} from "../../../services/auth/auth.service";
+import {BehaviorSubject} from "rxjs";
+import {provideRouter} from "@angular/router";
 
 describe('NavBarLoginComponent', () => {
   let component: NavBarLoginComponent;
   let fixture: ComponentFixture<NavBarLoginComponent>;
 
+  let mockAuthService: jasmine.SpyObj<AuthService> =
+    jasmine.createSpyObj('AuthService', ['isAuthenticated']);
+  mockAuthService.isAuthenticated = new BehaviorSubject<boolean>(false);
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      providers: [
+        {provide: AuthService, useValue: mockAuthService},
+        provideRouter([])
+      ],
       imports: [NavBarLoginComponent]
     })
-    .compileComponents();
-    
+      .compileComponents();
+
     fixture = TestBed.createComponent(NavBarLoginComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
