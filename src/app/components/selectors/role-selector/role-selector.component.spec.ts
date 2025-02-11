e5a627408412757ca80b8f19e675ce032e3f2bfe
CptFranck <francois.caillet71@gmail.com>
2025-02-11 18:42:42 +0100

    add missing mocked service to RoleSelectorComponent

--- src/app/components/selectors/role-selector/role-selector.component.spec.ts
+++ src/app/components/selectors/role-selector/role-selector.component.spec.ts
@@ -1,13 +1,24 @@
 import {ComponentFixture, TestBed} from '@angular/core/testing';
 
 import {RoleSelectorComponent} from './role-selector.component';
+import {RoleService} from "../../../services/role/role.service";
+import {BehaviorSubject} from "rxjs";
+import {Role} from "../../../interface/dto/role";
 
-describe('RoleComponent', () => {
+describe('RoleSelectorComponent', () => {
   let component: RoleSelectorComponent;
   let fixture: ComponentFixture<RoleSelectorComponent>;
 
+  let mockRoleService: jasmine.SpyObj<RoleService> =
+    jasmine.createSpyObj('RoleService', ['roles', 'isLoading']);
+  mockRoleService.roles = new BehaviorSubject<Role[]>([]);
+  mockRoleService.isLoading = new BehaviorSubject<boolean>(true);
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      providers: [
+        {provide: RoleService, useValue: mockRoleService},
+      ],
       imports: [RoleSelectorComponent]
     })
       .compileComponents();
