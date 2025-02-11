4247161efb9da2bbeddd91d87702dff595255d2a
CptFranck <francois.caillet71@gmail.com>
2025-02-11 18:34:04 +0100

    add missing mocked service to UsersComponent

--- src/app/page/back/user-management/users-management/users-management.component.spec.ts
+++ src/app/page/back/user-management/users-management/users-management.component.spec.ts
@@ -1,13 +1,32 @@
 import {ComponentFixture, TestBed} from '@angular/core/testing';
 
 import {UsersManagementComponent} from './users-management.component';
+import {BehaviorSubject} from "rxjs";
+import {UserService} from "../../../../services/user/user.service";
+import {User} from "../../../../interface/dto/user";
+import {RoleService} from "../../../../services/role/role.service";
+import {Role} from "../../../../interface/dto/role";
 
 describe('UsersComponent', () => {
   let component: UsersManagementComponent;
   let fixture: ComponentFixture<UsersManagementComponent>;
 
+  let mockUserService: jasmine.SpyObj<UserService> =
+    jasmine.createSpyObj('UserService', ['users', 'isLoading']);
+  mockUserService.users = new BehaviorSubject<User[]>([]);
+  mockUserService.isLoading = new BehaviorSubject<boolean>(true);
+
+  let mockRoleService: jasmine.SpyObj<RoleService> =
+    jasmine.createSpyObj('RoleService', ['roles', 'isLoading']);
+  mockRoleService.roles = new BehaviorSubject<Role[]>([]);
+  mockRoleService.isLoading = new BehaviorSubject<boolean>(true);
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      providers: [
+        {provide: UserService, useValue: mockUserService},
+        {provide: RoleService, useValue: mockRoleService},
+      ],
       imports: [UsersManagementComponent]
     })
       .compileComponents();
