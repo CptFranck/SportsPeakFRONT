0224eccfa54d97fb433a4521a6b14c1469757498
CptFranck <francois.caillet71@gmail.com>
2025-02-11 18:42:04 +0100

    add missing mocked service to RolesComponent

--- src/app/page/back/role/roles/roles.component.spec.ts
+++ src/app/page/back/role/roles/roles.component.spec.ts
@@ -1,17 +1,36 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { RolesComponent } from './roles.component';
+import {RolesComponent} from './roles.component';
+import {BehaviorSubject} from "rxjs";
+import {RoleService} from "../../../../services/role/role.service";
+import {Role} from "../../../../interface/dto/role";
+import {PrivilegeService} from "../../../../services/privilege/privilege.service";
+import {Privilege} from "../../../../interface/dto/privilege";
 
 describe('RolesComponent', () => {
   let component: RolesComponent;
   let fixture: ComponentFixture<RolesComponent>;
 
+  let mockRoleService: jasmine.SpyObj<RoleService> =
+    jasmine.createSpyObj('RoleService', ['roles', 'isLoading']);
+  mockRoleService.roles = new BehaviorSubject<Role[]>([]);
+  mockRoleService.isLoading = new BehaviorSubject<boolean>(true);
+
+  let mockPrivilegeService: jasmine.SpyObj<PrivilegeService> =
+    jasmine.createSpyObj('PrivilegeService', ['privileges', 'isLoading']);
+  mockPrivilegeService.privileges = new BehaviorSubject<Privilege[]>([]);
+  mockPrivilegeService.isLoading = new BehaviorSubject<boolean>(true);
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      providers: [
+        {provide: RoleService, useValue: mockRoleService},
+        {provide: PrivilegeService, useValue: mockPrivilegeService},
+      ],
       imports: [RolesComponent]
     })
-    .compileComponents();
-    
+      .compileComponents();
+
     fixture = TestBed.createComponent(RolesComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
