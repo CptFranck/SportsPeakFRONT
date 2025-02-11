b5ccc1786cd0386c750b444a073ce9e95ac4ab1d
CptFranck <francois.caillet71@gmail.com>
2025-02-11 18:44:10 +0100

    add missing mocked service to RoleDeleteFormComponent

--- src/app/components/form/role/role-delete-form/role-delete-form.component.spec.ts
+++ src/app/components/form/role/role-delete-form/role-delete-form.component.spec.ts
@@ -1,17 +1,21 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { RoleDeleteFormComponent } from './role-delete-form.component';
+import {RoleDeleteFormComponent} from './role-delete-form.component';
+import {RoleService} from "../../../../services/role/role.service";
 
 describe('RoleDeleteFormComponent', () => {
   let component: RoleDeleteFormComponent;
   let fixture: ComponentFixture<RoleDeleteFormComponent>;
 
+  let mockRoleService: jasmine.SpyObj<RoleService>;
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      providers: [{provide: RoleService, useValue: mockRoleService}],
       imports: [RoleDeleteFormComponent]
     })
-    .compileComponents();
-    
+      .compileComponents();
+
     fixture = TestBed.createComponent(RoleDeleteFormComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
