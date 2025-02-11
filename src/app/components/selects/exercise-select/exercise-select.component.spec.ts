5be1f2585fb339002b17c3ae8a8cdee444c2f9a6
CptFranck <francois.caillet71@gmail.com>
2025-02-11 19:02:32 +0100

    add missing mocked service ExerciseSelectComponent

--- src/app/components/selects/exercise-select/exercise-select.component.spec.ts
+++ src/app/components/selects/exercise-select/exercise-select.component.spec.ts
@@ -1,17 +1,26 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
-import { ExerciseSelectComponent } from './exercise-select.component';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
+import {ExerciseSelectComponent} from './exercise-select.component';
+import {BehaviorSubject} from "rxjs";
+import {ExerciseService} from "../../../services/exercise/exercise.service";
+import {Exercise} from "../../../interface/dto/exercise";
 
 describe('ExerciseSelectComponent', () => {
   let component: ExerciseSelectComponent;
   let fixture: ComponentFixture<ExerciseSelectComponent>;
+  let mockExerciseService: jasmine.SpyObj<ExerciseService> =
+    jasmine.createSpyObj('ExerciseService', ['exercises', 'isLoading']);
+  mockExerciseService.exercises = new BehaviorSubject<Exercise[]>([]);
+  mockExerciseService.isLoading = new BehaviorSubject<boolean>(true);
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      providers: [
+        {provide: ExerciseService, useValue: mockExerciseService}
+      ],
       imports: [ExerciseSelectComponent]
     })
-    .compileComponents();
-    
+      .compileComponents();
+
     fixture = TestBed.createComponent(ExerciseSelectComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
