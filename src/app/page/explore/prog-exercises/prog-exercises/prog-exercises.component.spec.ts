5b435db40fc13352b4ad183ed51f0575e3599a7e
CptFranck <francois.caillet71@gmail.com>
2025-02-11 18:44:31 +0100

    add missing mocked service to ProgExercisesComponent

--- src/app/page/explore/prog-exercises/prog-exercises/prog-exercises.component.spec.ts
+++ src/app/page/explore/prog-exercises/prog-exercises/prog-exercises.component.spec.ts
@@ -1,17 +1,27 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { ProgExercisesComponent } from './prog-exercises.component';
+import {ProgExercisesComponent} from './prog-exercises.component';
+import {ProgExerciseService} from "../../../../services/prog-exercise/prog-exercise.service";
+import {BehaviorSubject} from "rxjs";
+import {ProgExercise} from "../../../../interface/dto/prog-exercise";
 
 describe('ProgExercisesComponent', () => {
   let component: ProgExercisesComponent;
   let fixture: ComponentFixture<ProgExercisesComponent>;
+  let mockProgExerciseService: jasmine.SpyObj<ProgExerciseService> =
+    jasmine.createSpyObj('ProgExerciseService', ['progExercises', 'isLoading']);
+  mockProgExerciseService.progExercises = new BehaviorSubject<ProgExercise[]>([]);
+  mockProgExerciseService.isLoading = new BehaviorSubject<boolean>(true);
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      providers: [
+        {provide: ProgExerciseService, useValue: mockProgExerciseService}
+      ],
       imports: [ProgExercisesComponent]
     })
-    .compileComponents();
-    
+      .compileComponents();
+
     fixture = TestBed.createComponent(ProgExercisesComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
