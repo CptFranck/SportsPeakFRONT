fac423d1476a77c7438cb75dee4a21990f2169b0
CptFranck <francois.caillet71@gmail.com>
2025-02-11 19:54:07 +0100

    add missing mocked service TargetSetStateFormComponent

--- src/app/components/form/target-set/target-set-state-form/target-set-state-form.component.spec.ts
+++ src/app/components/form/target-set/target-set-state-form/target-set-state-form.component.spec.ts
@@ -1,19 +1,28 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { TargetSetStateFormComponent } from './target-set-state-form.component';
+import {TargetSetStateFormComponent} from './target-set-state-form.component';
+import {TargetSetService} from "../../../../services/target-set/target-set.service";
+import {generateTestTargetSet} from "../../../../utils/testFunctions";
 
 describe('TargetSetStateFormComponent', () => {
   let component: TargetSetStateFormComponent;
   let fixture: ComponentFixture<TargetSetStateFormComponent>;
 
+  let mockTargetSetService: jasmine.SpyObj<TargetSetService>;
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      providers: [
+        {provide: TargetSetService, useValue: mockTargetSetService},
+      ],
       imports: [TargetSetStateFormComponent]
     })
-    .compileComponents();
-    
+      .compileComponents();
+
     fixture = TestBed.createComponent(TargetSetStateFormComponent);
     component = fixture.componentInstance;
+
+    component.targetSet = generateTestTargetSet();
     fixture.detectChanges();
   });
 
