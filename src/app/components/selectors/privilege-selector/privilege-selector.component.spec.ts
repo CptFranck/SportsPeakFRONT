7a12b75699e05293fc58459e9ac1d58b9bafee6e
CptFranck <francois.caillet71@gmail.com>
2025-02-11 18:46:49 +0100

    add missing mocked service to PrivilegeSelectorComponent

--- src/app/components/selectors/privilege-selector/privilege-selector.component.spec.ts
+++ src/app/components/selectors/privilege-selector/privilege-selector.component.spec.ts
@@ -1,17 +1,28 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { PrivilegeSelectorComponent } from './privilege-selector.component';
+import {PrivilegeSelectorComponent} from './privilege-selector.component';
+import {PrivilegeService} from "../../../services/privilege/privilege.service";
+import {BehaviorSubject} from "rxjs";
+import {Privilege} from "../../../interface/dto/privilege";
 
 describe('PrivilegeSelectorComponent', () => {
   let component: PrivilegeSelectorComponent;
   let fixture: ComponentFixture<PrivilegeSelectorComponent>;
 
+  let mockPrivilegeService: jasmine.SpyObj<PrivilegeService> =
+    jasmine.createSpyObj('PrivilegeService', ['privileges', 'isLoading']);
+  mockPrivilegeService.privileges = new BehaviorSubject<Privilege[]>([]);
+  mockPrivilegeService.isLoading = new BehaviorSubject<boolean>(true);
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      providers: [
+        {provide: PrivilegeService, useValue: mockPrivilegeService},
+      ],
       imports: [PrivilegeSelectorComponent]
     })
-    .compileComponents();
-    
+      .compileComponents();
+
     fixture = TestBed.createComponent(PrivilegeSelectorComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
