e256c5d876bcec8852303ebbc8f674858f175717
CptFranck <francois.caillet71@gmail.com>
2025-02-12 11:28:16 +0100

    add missing input AlertComponent

--- src/app/components/alert/alert.component.spec.ts
+++ src/app/components/alert/alert.component.spec.ts
@@ -1,6 +1,7 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { AlertComponent } from './alert.component';
+import {AlertComponent} from './alert.component';
+import {AlertType} from "../../interface/enum/alert-type";
 
 describe('AlertComponent', () => {
   let component: AlertComponent;
@@ -10,10 +11,18 @@ describe('AlertComponent', () => {
     await TestBed.configureTestingModule({
       imports: [AlertComponent]
     })
-    .compileComponents();
-    
+      .compileComponents();
+
     fixture = TestBed.createComponent(AlertComponent);
     component = fixture.componentInstance;
+
+    component.alert = {
+      id: 0,
+      title: "title",
+      message: "message",
+      type: AlertType.success,
+      closed: false
+    }
     fixture.detectChanges();
   });
 
