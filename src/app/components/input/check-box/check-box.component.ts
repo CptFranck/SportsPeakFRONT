b23d833fad1d0cd58c730a2f456a70ae4f36e2e2
CptFranck <francois.caillet71@gmail.com>
2025-03-28 15:14:50 +0100

    change input decorators to signal input and local variable in check-box component

--- src/app/components/input/check-box/check-box.component.ts
+++ src/app/components/input/check-box/check-box.component.ts
@@ -1,21 +1,21 @@
-import {Component, EventEmitter, Input, Output} from '@angular/core';
+import {Component, EventEmitter, Input, input, Output, signal} from '@angular/core';
 
 @Component({
-    selector: 'app-check-box',
-    imports: [],
-    templateUrl: './check-box.component.html'
+  selector: 'app-check-box',
+  templateUrl: './check-box.component.html'
 })
 export class CheckBoxComponent {
 
-  label!: string;
-  checked !: boolean
-  @Input() labelCheck !: string;
-  @Input() labelUnchecked: string | undefined;
+  label = signal<string>("");
+  checked = signal<boolean>(false)
+
+  readonly labelCheck = input.required<string>();
+  readonly labelUnchecked = input<string>();
 
   @Output() actionPerformanceLog: EventEmitter<void> = new EventEmitter();
 
   @Input() set checkedInput(checked: boolean) {
-    this.checked = checked;
+    this.checked.set(checked);
     this.updateLabel();
   }
 
@@ -24,9 +24,12 @@ export class CheckBoxComponent {
   }
 
   updateLabel() {
-    if (this.checked)
-      this.label = this.labelCheck;
-    else if (this.labelUnchecked)
-      this.label = this.labelUnchecked;
+    const labelUnchecked = this.labelUnchecked();
+    if (this.checked())
+      this.label.set(this.labelCheck());
+    else if (labelUnchecked)
+      this.label.set(labelUnchecked);
+    else
+      this.label.set(this.labelCheck());
   }
 }
