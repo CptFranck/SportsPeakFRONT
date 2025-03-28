eb2ed458efcb8dcceb1c55028db2cf35916b6598
CptFranck <francois.caillet71@gmail.com>
2025-03-28 17:41:11 +0100

    change input decorators to signal input and local variable in duration input component

--- src/app/components/input/duration-inputs/duration-input.component.ts
+++ src/app/components/input/duration-inputs/duration-input.component.ts
@@ -1,25 +1,22 @@
-import {Component, Input} from '@angular/core';
+import {Component, computed, input} from '@angular/core';
 import {FormGroup, ReactiveFormsModule} from "@angular/forms";
 import {InputControlComponent} from "../../input-control/input-control.component";
 import {NgIf} from "@angular/common";
 
 @Component({
-    selector: 'app-duration-input',
-    imports: [
-        InputControlComponent,
-        NgIf,
-        ReactiveFormsModule
-    ],
-    templateUrl: './duration-input.component.html'
+  selector: 'app-duration-input',
+  imports: [
+    InputControlComponent,
+    NgIf,
+    ReactiveFormsModule
+  ],
+  templateUrl: './duration-input.component.html'
 })
 export class DurationInputComponent {
-  durationForm!: FormGroup;
-  rootFormGroup!: FormGroup;
+  readonly fieldName = input.required<string>();
+  readonly formGroup = input.required<FormGroup>();
 
-  @Input() fieldName!: string;
-
-  @Input() set rootFormGroupInput(value: FormGroup) {
-    this.rootFormGroup = value;
-    this.durationForm = this.rootFormGroup.get(this.fieldName) as FormGroup;
-  }
+  durationForm = computed<FormGroup>(() =>
+    this.formGroup().get(this.fieldName()) as FormGroup
+  );
 }
