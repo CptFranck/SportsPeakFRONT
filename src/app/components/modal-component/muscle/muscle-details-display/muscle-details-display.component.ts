dc12cad7f5a5c53317b48f584f5617805275998d
CptFranck <francois.caillet71@gmail.com>
2025-04-02 17:33:32 +0200

    change input decorators to signal input and local variable in muscle detail display component

--- src/app/components/modal-component/muscle/muscle-details-display/muscle-details-display.component.ts
+++ src/app/components/modal-component/muscle/muscle-details-display/muscle-details-display.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input} from '@angular/core';
+import {Component, input} from '@angular/core';
 import {Muscle} from "../../../../interface/dto/muscle";
 import {NgForOf, NgIf} from "@angular/common";
 import {ReactiveFormsModule} from "@angular/forms";
@@ -13,6 +13,5 @@ import {ReactiveFormsModule} from "@angular/forms";
   templateUrl: './muscle-details-display.component.html'
 })
 export class MuscleDetailsDisplayComponent {
-  @Input() muscle: Muscle | undefined;
-  @Input() action!: string;
+  readonly muscle = input<Muscle>();
 }
