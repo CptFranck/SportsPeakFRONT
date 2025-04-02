612e65acfbb2e4589ed799551a45f867246bbf6c
CptFranck <francois.caillet71@gmail.com>
2025-04-02 17:07:45 +0200

    change input decorators to signal input and local variable in exercise type detail component

--- src/app/components/modal-component/exercise-type/exercise-type-details-display/exercise-type-details-display.component.ts
+++ src/app/components/modal-component/exercise-type/exercise-type-details-display/exercise-type-details-display.component.ts
@@ -1,16 +1,15 @@
-import {Component, Input} from '@angular/core';
+import {Component, input} from '@angular/core';
 import {NgForOf, NgIf} from "@angular/common";
 import {ExerciseType} from "../../../../interface/dto/exercise-type";
 
 @Component({
-    selector: 'app-exercise-type-details-display',
-    imports: [
-        NgForOf,
-        NgIf
-    ],
-    templateUrl: './exercise-type-details-display.component.html'
+  selector: 'app-exercise-type-details-display',
+  imports: [
+    NgForOf,
+    NgIf
+  ],
+  templateUrl: './exercise-type-details-display.component.html'
 })
 export class ExerciseTypeDetailsDisplayComponent {
-  @Input() exerciseType: ExerciseType | undefined;
-  @Input() action!: string;
+  readonly exerciseType = input<ExerciseType>();
 }
