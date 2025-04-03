c253a53896b12b0206bfd1f7758bacf3cb268278
CptFranck <francois.caillet71@gmail.com>
2025-04-03 18:13:12 +0200

    update modal button component with signal input

--- src/app/components/modal/modal-button/modal-button.component.ts
+++ src/app/components/modal/modal-button/modal-button.component.ts
@@ -1,35 +1,35 @@
-import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {Component, EventEmitter, input, OnInit, Output, signal} from '@angular/core';
 import {TooltipComponent} from "../../tooltip/tooltip.component";
 
 @Component({
-    selector: 'app-modal-button',
-    imports: [
-        TooltipComponent
-    ],
-    templateUrl: './modal-button.component.html'
+  selector: 'app-modal-button',
+  imports: [
+    TooltipComponent
+  ],
+  templateUrl: './modal-button.component.html'
 })
 export class ModalButtonComponent implements OnInit {
-  AllWidthStyle: string = "";
+  allWidthStyle = signal<string>("");
 
-  @Input() disabled: boolean = false;
-  @Input() modalId!: string;
-  @Input() modalValue: any | undefined;
-  @Input() btnClass: string = "btn-success"
-  @Input() allWidth: boolean = false;
-  @Input() useTooltip: boolean = false;
-  @Input() tooltipText: string = "Modal tooltip text";
+  readonly disabled = input<boolean>(false);
+  readonly modalId = input.required<string>();
+  readonly modalValue = input<any>();
+  readonly btnClass = input<string>("btn-success");
+  readonly allWidth = input<boolean>(false);
+  readonly useTooltip = input<boolean>(false);
+  readonly tooltipText = input<string>("Modal tooltip text");
 
   @Output() onCLickModalButton: EventEmitter<any> = new EventEmitter();
 
 
   onClick() {
     if (this.onCLickModalButton) {
-      this.onCLickModalButton.emit(this.modalValue)
+      this.onCLickModalButton.emit(this.modalValue())
     }
   }
 
   ngOnInit(): void {
-    if (this.allWidth)
-      this.AllWidthStyle = "width:100%"
+    if (this.allWidth())
+      this.allWidthStyle.set("width:100%")
   }
 }
