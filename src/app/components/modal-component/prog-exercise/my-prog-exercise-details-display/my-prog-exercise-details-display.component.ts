66388039155979d27b212e143d37f65bd8129e9f
CptFranck <francois.caillet71@gmail.com>
2025-04-03 19:39:45 +0200

    update my prog exercise details component with signal input

--- src/app/components/modal-component/prog-exercise/my-prog-exercise-details-display/my-prog-exercise-details-display.component.ts
+++ src/app/components/modal-component/prog-exercise/my-prog-exercise-details-display/my-prog-exercise-details-display.component.ts
@@ -1,10 +1,9 @@
-import {Component, Input} from '@angular/core';
+import {Component, computed, input} from '@angular/core';
 import {NgForOf, NgIf} from "@angular/common";
-import {ProgExercise} from "../../../../interface/dto/prog-exercise";
-
 import {TargetSet} from "../../../../interface/dto/target-set";
 import {Dictionary} from "../../../../interface/utils/dictionary";
 import {TargetSetInformation} from "../../../../interface/utils/target-set-row-detail";
+import {ProgExercise} from "../../../../interface/dto/prog-exercise";
 import {getProgExerciseTime} from "../../../../utils/prog-exercise-functions";
 import {
   getTargetSetTimeToString,
@@ -14,29 +13,33 @@ import {
 import {getStringTime} from "../../../../utils/duration-functions";
 
 @Component({
-    selector: 'app-my-prog-exercise-details-display',
-    imports: [
-        NgForOf,
-        NgIf
-    ],
-    templateUrl: './my-prog-exercise-details-display.component.html'
+  selector: 'app-my-prog-exercise-details-display',
+  imports: [
+    NgForOf,
+    NgIf
+  ],
+  templateUrl: './my-prog-exercise-details-display.component.html'
 })
 export class MyProgExerciseDetailsDisplayComponent {
-  progExercise: ProgExercise | undefined;
-  exerciseTime: string = "";
-  displayedTargetSets: TargetSet[] = [];
-  targetSetsInformation: Dictionary<TargetSetInformation> = {}
-
-  @Input() action!: string;
-
-  @Input() set progExerciseInput(value: ProgExercise | undefined) {
-
-    this.progExercise = value;
-    if (value) {
-      this.exerciseTime = getProgExerciseTime(value);
-      this.displayedTargetSets = getUpToDateTargetSets(value).sort(sortLastTargetSetsByIndex);
-      this.displayedTargetSets.forEach((targetSet: TargetSet, key: number, array: TargetSet[]) => {
-        this.targetSetsInformation[targetSet.id] = {
+  progExercise = input.required<ProgExercise | undefined>();
+  exerciseTime = computed<string>(() => {
+    const progExercise = this.progExercise();
+    if (progExercise)
+      return getProgExerciseTime(progExercise);
+    return "";
+  });
+  displayedTargetSets = computed<TargetSet[]>(() => {
+    const progExercise = this.progExercise();
+    if (progExercise)
+      return getUpToDateTargetSets(progExercise).sort(sortLastTargetSetsByIndex);
+    return [];
+  });
+  targetSetsInformation = computed<Dictionary<TargetSetInformation>>(() => {
+    const progExercise = this.progExercise();
+    const targetSetInformation: Dictionary<TargetSetInformation> = {};
+    if (progExercise)
+      this.displayedTargetSets().forEach((targetSet: TargetSet, key: number, array: TargetSet[]) => {
+        targetSetInformation[targetSet.id] = {
           setRep: targetSet?.setNumber + " set(s) of " + targetSet?.repetitionNumber + " reps",
           weight: " " + targetSet.weight + " " + targetSet.weightUnit,
           effortTime: getStringTime(targetSet.physicalExertionUnitTime),
@@ -44,6 +47,6 @@ export class MyProgExerciseDetailsDisplayComponent {
           setTime: getTargetSetTimeToString(targetSet, (array.length - 1 === key)),
         }
       })
-    }
-  }
+    return targetSetInformation;
+  });
 }
