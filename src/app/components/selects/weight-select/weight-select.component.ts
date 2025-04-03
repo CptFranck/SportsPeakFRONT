027b858707a4499bc6e2c891dc176a28354f1d9e
CptFranck <francois.caillet71@gmail.com>
2025-04-03 13:21:19 +0200

    change input decorators to signal input and local variable in weight selector component

--- src/app/components/selects/weight-select/weight-select.component.ts
+++ src/app/components/selects/weight-select/weight-select.component.ts
@@ -1,22 +1,22 @@
-import {Component, forwardRef, Input} from '@angular/core';
+import {Component, forwardRef, signal} from '@angular/core';
 import {ControlValueAccessor, NG_VALUE_ACCESSOR} from "@angular/forms";
 import {SelectOption} from "../../../interface/components/select/selectOption";
 import {SelectComponent} from "../../select/select.component";
 import {WeightUnit} from "../../../interface/enum/weightUnit";
 
 @Component({
-    selector: 'app-weight-select',
-    imports: [
-        SelectComponent
-    ],
-    providers: [
-        {
-            provide: NG_VALUE_ACCESSOR,
-            useExisting: forwardRef(() => WeightSelectComponent),
-            multi: true,
-        }
-    ],
-    templateUrl: './weight-select.component.html'
+  selector: 'app-weight-select',
+  imports: [
+    SelectComponent
+  ],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => WeightSelectComponent),
+      multi: true,
+    }
+  ],
+  templateUrl: './weight-select.component.html'
 })
 export class WeightSelectComponent implements ControlValueAccessor {
 
@@ -28,16 +28,16 @@ export class WeightSelectComponent implements ControlValueAccessor {
     value: WeightUnit.POUND,
   }];
 
-  @Input() weightUnit!: string;
+  weightUnit = signal<string>(WeightUnit.KILOGRAMME);
 
   onChange: (value: string) => void = () => {
   };
 
-  onTouched: ($event: boolean) => void = () => {
+  onTouched: () => void = () => {
   };
 
   writeValue(weightUnit: string): void {
-    this.weightUnit = weightUnit;
+    this.weightUnit.set(weightUnit);
   }
 
   registerOnChange(fn: (value: string) => void): void {
@@ -48,10 +48,8 @@ export class WeightSelectComponent implements ControlValueAccessor {
     this.onTouched = fn;
   }
 
-  setWeightUnitId(weightUnit: string | undefined): void {
-    if (weightUnit) {
-      this.weightUnit = weightUnit;
-      this.onChange(weightUnit)
-    }
+  setWeightUnitId(weightUnit: string): void {
+    this.weightUnit.set(weightUnit);
+    this.onChange(weightUnit)
   }
 }
