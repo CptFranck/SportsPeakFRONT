f72f4f707cb1c2cefa190ed9c5d515a5e9218fdc
CptFranck <francois.caillet71@gmail.com>
2025-04-03 12:32:50 +0200

    change input decorators to signal input and local variable in exercise  type selector component

--- src/app/components/selectors/exercise-type-selector/exercise-type-selector.component.ts
+++ src/app/components/selectors/exercise-type-selector/exercise-type-selector.component.ts
@@ -1,4 +1,4 @@
-import {Component, forwardRef, inject, Input, OnDestroy, OnInit} from '@angular/core';
+import {Component, forwardRef, inject, OnDestroy, OnInit, signal} from '@angular/core';
 import {ControlValueAccessor, NG_VALUE_ACCESSOR} from "@angular/forms";
 import {MultiSelectOption} from "../../../interface/components/multi-select/multiSelectOption";
 import {ExerciseTypeService} from "../../../services/exercise-type/exercise-type.service";
@@ -7,33 +7,27 @@ import {MultiSelectComponent} from "../../multi-select/multi-select.component";
 import {Subject, takeUntil} from "rxjs";
 
 @Component({
-    selector: 'app-exercise-type-selector',
-    imports: [
-        MultiSelectComponent
-    ],
-    providers: [
-        {
-            provide: NG_VALUE_ACCESSOR,
-            useExisting: forwardRef(() => ExerciseTypeSelectorComponent),
-            multi: true,
-        }
-    ],
-    templateUrl: './exercise-type-selector.component.html'
+  selector: 'app-exercise-type-selector',
+  imports: [
+    MultiSelectComponent
+  ],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => ExerciseTypeSelectorComponent),
+      multi: true,
+    }
+  ],
+  templateUrl: './exercise-type-selector.component.html'
 })
 export class ExerciseTypeSelectorComponent implements OnInit, OnDestroy, ControlValueAccessor {
-  loading: boolean = true;
+  loading = true;
   exerciseTypes: MultiSelectOption[] = [];
 
-  @Input() exerciseTypeIds: number[] = [];
+  readonly exerciseTypeIds = signal<number[]>([]);
 
-  private readonly unsubscribe$: Subject<void> = new Subject<void>();
-  private readonly exerciseTypeService: ExerciseTypeService = inject(ExerciseTypeService);
-
-  onChange: (value: number[]) => void = () => {
-  };
-
-  onTouched: ($event: boolean) => void = () => {
-  };
+  private readonly unsubscribe$ = new Subject<void>();
+  private readonly exerciseTypeService = inject(ExerciseTypeService);
 
   ngOnInit(): void {
     this.exerciseTypeService.exerciseTypes
@@ -60,8 +54,14 @@ export class ExerciseTypeSelectorComponent implements OnInit, OnDestroy, Control
     this.unsubscribe$.complete();
   }
 
+  onChange: (value: number[]) => void = () => {
+  };
+
+  onTouched: () => void = () => {
+  };
+
   writeValue(exerciseTypeIds: number[]): void {
-    this.exerciseTypeIds = exerciseTypeIds;
+    this.exerciseTypeIds.set([...exerciseTypeIds]);
   }
 
   registerOnChange(fn: (value: number[]) => void): void {
@@ -73,7 +73,7 @@ export class ExerciseTypeSelectorComponent implements OnInit, OnDestroy, Control
   }
 
   setExerciseIds(exerciseTypeIds: number[]) {
-    this.exerciseTypeIds = exerciseTypeIds;
+    this.exerciseTypeIds.set([...exerciseTypeIds])
     this.onChange(exerciseTypeIds);
   }
 }
