66fc88e9e7cced4e0e904726e284715cb65cab20
CptFranck <francois.caillet71@gmail.com>
2025-04-03 16:53:08 +0200

    change input decorators to signal input and local variable in select component

--- src/app/components/select/select.component.ts
+++ src/app/components/select/select.component.ts
@@ -1,28 +1,30 @@
 import {Component, EventEmitter, input, model, Output} from '@angular/core';
 import {SelectOption} from "../../interface/components/select/selectOption";
-import {NgForOf} from "@angular/common";
+import {NgForOf, NgIf} from "@angular/common";
 import {FormsModule} from "@angular/forms";
 
 @Component({
   selector: 'app-select',
   imports: [
     NgForOf,
-    FormsModule
+    FormsModule,
+    NgIf
   ],
   templateUrl: './select.component.html'
 })
 export class SelectComponent {
-  value = model<string>();
+  value = model<string | null>(null);
   readonly options = input.required<SelectOption[]>();
+  readonly defaultOption = input<boolean>(false);
 
   @Output() onTouched = new EventEmitter<boolean>();
-  @Output() onChange = new EventEmitter<string>();
+  @Output() onChange = new EventEmitter<string | null>();
 
   onSelect(event: Event) {
     if (event.target instanceof HTMLSelectElement) {
-      let value: string | undefined = event.target.value;
-      if (value === "undefined")
-        value = undefined;
+      let value: string | null = event.target.value;
+      if (value === "null")
+        value = null;
       this.onChange.emit(value);
     }
   }
