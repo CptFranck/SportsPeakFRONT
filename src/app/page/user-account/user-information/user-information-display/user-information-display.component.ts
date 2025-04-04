7cb6bed594a388f9baeda1e6ae3f824e347807e0
CptFranck <francois.caillet71@gmail.com>
2025-04-04 16:15:55 +0200

    update user information display form component with signal input

--- src/app/page/user-account/user-information/user-information-display/user-information-display.component.ts
+++ src/app/page/user-account/user-information/user-information-display/user-information-display.component.ts
@@ -1,4 +1,4 @@
-import {Component, EventEmitter, Input, Output} from '@angular/core';
+import {Component, EventEmitter, input, Output} from '@angular/core';
 import {User} from "../../../../interface/dto/user";
 import {FormIndicator} from "../../../../interface/utils/form-indicator";
 import {ActionType} from "../../../../interface/enum/action-type";
@@ -6,26 +6,26 @@ import {ModificationField} from "../../../../interface/enum/modification-field";
 import {ModalButtonComponent} from "../../../../components/modal/modal-button/modal-button.component";
 
 @Component({
-    selector: 'app-user-information-display',
-    imports: [
-        ModalButtonComponent
-    ],
-    templateUrl: './user-information-display.component.html'
+  selector: 'app-user-information-display',
+  imports: [
+    ModalButtonComponent
+  ],
+  templateUrl: './user-information-display.component.html'
 })
 export class UserInformationDisplayComponent {
 
-  btnClass: string = "btn btn-secondary"
+  btnClass = "btn btn-secondary"
 
-  @Input() user!: User | undefined;
-  @Input() modalId!: string;
+  readonly user = input.required<User | undefined>();
+  readonly modalId = input.required<string>();
 
-  @Output() userAction: EventEmitter<FormIndicator> = new EventEmitter<FormIndicator>();
+  @Output() userAction = new EventEmitter<FormIndicator>();
 
   onChangeEmail() {
     this.userAction.emit({
       actionType: ActionType.update,
       modificationField: ModificationField.email,
-      object: this.user
+      object: this.user()
     });
   }
 
@@ -33,7 +33,7 @@ export class UserInformationDisplayComponent {
     this.userAction.emit({
       actionType: ActionType.update,
       modificationField: ModificationField.username,
-      object: this.user
+      object: this.user()
     });
   }
 
@@ -41,7 +41,7 @@ export class UserInformationDisplayComponent {
     this.userAction.emit({
       actionType: ActionType.update,
       modificationField: ModificationField.name,
-      object: this.user
+      object: this.user()
     });
   }
 }
