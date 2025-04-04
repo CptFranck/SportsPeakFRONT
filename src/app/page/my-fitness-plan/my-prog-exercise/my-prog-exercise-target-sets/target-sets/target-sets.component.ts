073f8bd3934bc7bef3621e548aeebc32baddd344
CptFranck <francois.caillet71@gmail.com>
2025-04-04 17:45:20 +0200

    update target sets component with signal input

--- src/app/page/my-fitness-plan/my-prog-exercise/my-prog-exercise-target-sets/target-sets/target-sets.component.ts
+++ src/app/page/my-fitness-plan/my-prog-exercise/my-prog-exercise-target-sets/target-sets/target-sets.component.ts
@@ -1,33 +1,31 @@
-import {Component, EventEmitter, Input, Output} from '@angular/core';
+import {Component, computed, EventEmitter, input, Output} from '@angular/core';
 import {NgForOf, NgIf} from "@angular/common";
 import {FormIndicator} from "../../../../../interface/utils/form-indicator";
 import {ProgExerciseTargetSets} from "../../../../../interface/utils/progExerciseTargetSets";
-import {Dictionary} from "../../../../../interface/utils/dictionary";
-import {TargetSet} from "../../../../../interface/dto/target-set";
 import {ActionType} from "../../../../../interface/enum/action-type";
 import {ProgExercise} from "../../../../../interface/dto/prog-exercise";
-import {getProgExerciseTargetSet,} from "../../../../../utils/prog-exercise-functions";
-import {getUpToDateTargetSets, sortLastTargetSetsByIndex} from "../../../../../utils/target-set-functions";
 import {
   TargetSetCardComponent
 } from "../../../../../components/card/target-set/target-set-card/target-set-card.component";
 import {TabHeaderComponent} from "../../../../../components/tab-header/tab-header.component";
 import {TabOption} from "../../../../../interface/components/tab/tabOption";
+import {getUpToDateTargetSets, sortLastTargetSetsByIndex} from "../../../../../utils/target-set-functions";
+import {getProgExerciseTargetSet} from "../../../../../utils/prog-exercise-functions";
+import {Dictionary} from "../../../../../interface/utils/dictionary";
+import {TargetSet} from "../../../../../interface/dto/target-set";
 
 @Component({
-    selector: 'app-target-sets',
-    imports: [
-        NgForOf,
-        NgIf,
-        TargetSetCardComponent,
-        TabHeaderComponent
-    ],
-    templateUrl: './target-sets.component.html'
+  selector: 'app-target-sets',
+  imports: [
+    NgForOf,
+    NgIf,
+    TargetSetCardComponent,
+    TabHeaderComponent
+  ],
+  templateUrl: './target-sets.component.html'
 })
 export class TargetSetsComponent {
   tabId: string = "targetSetTab";
-  progExercise: ProgExercise | undefined;
-  isLastTargetSetUsed: Dictionary<boolean> = {};
   targetSetUsedId: string = "TargetSetUsedId";
   targetSetUnusedId: string = "TargetSetUnusedId";
   targetSetHiddenId: string = "TargetSetHiddenId";
@@ -37,30 +35,31 @@ export class TargetSetsComponent {
     {id: this.targetSetHiddenId, title: "Hidden", active: ""},
   ];
 
-  @Input() targetSetModalId!: string;
-  @Input() performanceLogModalId!: string;
-  @Input() progExerciseTargetSets: ProgExerciseTargetSets =
-    {targetSetUsed: [], targetSetUnused: [], targetSetHidden: []};
+  readonly progExercise = input.required<ProgExercise | undefined>();
+  readonly targetSetModalId = input.required<string>();
+  readonly performanceLogModalId = input.required<string>();
+
+  readonly progExerciseTargetSets = computed<ProgExerciseTargetSets>(() => {
+    const progExercise = this.progExercise();
+    if (progExercise) {
+      const targetSets = getUpToDateTargetSets(progExercise).sort(sortLastTargetSetsByIndex);
+      return getProgExerciseTargetSet(targetSets);
+    }
+    return {targetSetUsed: [], targetSetUnused: [], targetSetHidden: []}
+  });
+
+  readonly isLastTargetSetUsed = computed<Dictionary<boolean>>(() => {
+    const isLastTargetSetUsed: Dictionary<boolean> = {};
+    this.progExerciseTargetSets().targetSetUsed.forEach((targetSet: TargetSet, key: number, array: TargetSet[]) => {
+      isLastTargetSetUsed[targetSet.id] = (array.length - 1) === key;
+    })
+    return isLastTargetSetUsed;
+  });
 
   @Output() actionTargetSets: EventEmitter<FormIndicator> = new EventEmitter<FormIndicator>();
   @Output() actionPerformanceLogs: EventEmitter<FormIndicator> = new EventEmitter<FormIndicator>();
 
   protected readonly ActionType = ActionType;
-
-  @Input() set progExerciseInput(progExercise: ProgExercise | undefined) {
-    if (progExercise) {
-      this.progExercise = progExercise;
-      let targetSets: TargetSet[] = getUpToDateTargetSets(progExercise).sort(sortLastTargetSetsByIndex);
-      this.progExerciseTargetSets = getProgExerciseTargetSet(targetSets);
-    }
-  }
-
-  @Input() set progExerciseTargetSetsInput(progExerciseTargetSets: ProgExerciseTargetSets) {
-    this.progExerciseTargetSets = progExerciseTargetSets;
-    progExerciseTargetSets.targetSetUsed.forEach((targetSet: TargetSet, key: number, array: TargetSet[]) => {
-      this.isLastTargetSetUsed[targetSet.id] = (array.length - 1) === key;
-    })
-  }
 
   setTargetSet(event: FormIndicator) {
     this.actionTargetSets.emit(event)
