6ea855807856f6a8a0804e5ba3137bb67f0f79ee
CptFranck <francois.caillet71@gmail.com>
2025-04-04 11:30:03 +0200

    update muscle modal component with signal input

--- src/app/page/explore/prog-exercises/prog-exercise-modal/prog-exercise-modal.component.ts
+++ src/app/page/explore/prog-exercises/prog-exercise-modal/prog-exercise-modal.component.ts
@@ -1,6 +1,4 @@
-import {Component, inject, Input, OnDestroy, OnInit, TemplateRef, ViewChild} from '@angular/core';
-import {UserLoggedService} from "../../../../services/user-logged/user-logged.service";
-import {ModalButtonComponent} from "../../../../components/modal/modal-button/modal-button.component";
+import {Component, input, TemplateRef, ViewChild} from '@angular/core';
 import {ModalComponent} from "../../../../components/modal/modal/modal.component";
 import {NgIf} from "@angular/common";
 import {ActionType} from "../../../../interface/enum/action-type";
@@ -8,46 +6,23 @@ import {ProgExercise} from "../../../../interface/dto/prog-exercise";
 import {
   MyProgExerciseDetailsDisplayComponent
 } from "../../../../components/modal-component/prog-exercise/my-prog-exercise-details-display/my-prog-exercise-details-display.component";
-import {Subject, takeUntil} from "rxjs";
 
 @Component({
-    selector: 'app-prog-exercise-modal',
-    imports: [
-        ModalButtonComponent,
-        ModalComponent,
-        NgIf,
-        MyProgExerciseDetailsDisplayComponent
-    ],
-    templateUrl: './prog-exercise-modal.component.html'
+  selector: 'app-prog-exercise-modal',
+  imports: [
+    ModalComponent,
+    NgIf,
+    MyProgExerciseDetailsDisplayComponent
+  ],
+  templateUrl: './prog-exercise-modal.component.html'
 })
-export class ProgExerciseModalComponent implements OnInit, OnDestroy {
-  isAdmin: boolean = false;
-
-  @Input() modalTitle!: string;
-  @Input() muscleModalId!: string;
-  @Input() progExercise: ProgExercise | undefined;
-  @Input() action!: ActionType;
+export class ProgExerciseModalComponent {
+  readonly modalTitle = input.required<string>();
+  readonly progExerciseModalId = input.required<string>();
+  readonly progExercise = input.required<ProgExercise | undefined>();
+  readonly action = input.required<ActionType>();
 
   @ViewChild("modalTemplate") modalTemplate!: TemplateRef<any>;
 
   protected readonly ActionType = ActionType;
-  private readonly unsubscribe$: Subject<void> = new Subject<void>();
-  private readonly userLoggedService: UserLoggedService = inject(UserLoggedService);
-
-  onClick(value: undefined) {
-    this.progExercise = value;
-    this.action = ActionType.create;
-    this.modalTitle = "Add new muscle";
-  }
-
-  ngOnInit(): void {
-    this.userLoggedService.currentUser
-      .pipe(takeUntil(this.unsubscribe$))
-      .subscribe(() => this.isAdmin = this.userLoggedService.isAdmin());
-  }
-
-  ngOnDestroy() {
-    this.unsubscribe$.next();
-    this.unsubscribe$.complete();
-  }
 }
