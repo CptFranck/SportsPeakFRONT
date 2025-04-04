b95be830831229da3202e2c7e8697c44a43e9008
CptFranck <francois.caillet71@gmail.com>
2025-04-04 15:09:12 +0200

    update user password form component with signal input

--- src/app/components/form/user/user-password-form/user-password-form.component.ts
+++ src/app/components/form/user/user-password-form/user-password-form.component.ts
@@ -1,62 +1,32 @@
-import {Component, inject, Input, OnDestroy, OnInit} from '@angular/core';
+import {Component, computed, inject, input, OnDestroy, OnInit, signal} from '@angular/core';
 import {User} from "../../../../interface/dto/user";
 import {FormControl, FormGroup, ReactiveFormsModule, Validators} from "@angular/forms";
 import {Observable, Subject, takeUntil} from "rxjs";
 import {UserService} from "../../../../services/user/user.service";
 import {InputControlComponent} from "../../../input-control/input-control.component";
 import {NgIf} from "@angular/common";
-import {confirmValidator} from "../../../../validators/confirmValidator";
 import {ModificationField} from "../../../../interface/enum/modification-field";
 import {ActionType} from "../../../../interface/enum/action-type";
+import {confirmValidator} from "../../../../validators/confirmValidator";
 
 @Component({
-    selector: 'app-user-password-form',
-    imports: [
-        InputControlComponent,
-        ReactiveFormsModule,
-        NgIf
-    ],
-    templateUrl: './user-password-form.component.html'
+  selector: 'app-user-password-form',
+  imports: [
+    InputControlComponent,
+    ReactiveFormsModule,
+    NgIf
+  ],
+  templateUrl: './user-password-form.component.html'
 })
 export class UserPasswordFormComponent implements OnInit, OnDestroy {
-  user: User | undefined;
-  userForm: FormGroup | null = null;
-  submitInvalidForm: boolean = false;
+  user = input.required<User | undefined>();
 
-  @Input() btnCloseRef!: HTMLButtonElement;
-  @Input() submitEventActionType$!: Observable<ActionType> | undefined;
-  @Input() modification!: ModificationField;
-
-  private readonly unsubscribe$: Subject<void> = new Subject<void>();
-  private readonly userService: UserService = inject(UserService);
-
-  @Input() set userInput(value: User | undefined) {
-    this.user = value;
-    this.initializeUserPasswordForm();
-  }
-
-  ngOnInit() {
-    this.initializeUserPasswordForm()
-    if (this.submitEventActionType$)
-      this.submitEventActionType$
-        .pipe(takeUntil(this.unsubscribe$))
-        .subscribe((actionType: ActionType) => {
-          if (actionType === ActionType.update && this.modification === ModificationField.password)
-            this.onSubmit();
-        });
-  }
-
-  ngOnDestroy() {
-    this.unsubscribe$.next();
-    this.unsubscribe$.complete();
-  }
-
-  initializeUserPasswordForm() {
-    this.userForm = new FormGroup({
+  userForm = computed<FormGroup>(() => {
+    const userForm: FormGroup = new FormGroup({
         oldPassword: new FormControl(
           "",
-          [Validators.required
-          ]),
+          [Validators.required],
+        ),
         newPassword: new FormControl(
           "",
           [Validators.required,
@@ -73,19 +43,46 @@ export class UserPasswordFormComponent implements OnInit, OnDestroy {
         validators: confirmValidator('newPassword', 'confirmPassword')
       }
     );
+    const user = this.user();
+    if (user)
+      userForm.addControl("id", new FormControl(user.id));
+    return userForm;
+  });
 
-    if (this.user)
-      this.userForm.addControl("id", new FormControl(this.user.id));
+  submitInvalidForm = signal<boolean>(false);
+
+  readonly btnCloseRef = input.required<HTMLButtonElement>();
+  readonly submitEventActionType$ = input.required<Observable<ActionType> | undefined>();
+  readonly modification = input.required<ModificationField>();
+
+  private readonly unsubscribe$ = new Subject<void>();
+  private readonly userService = inject(UserService);
+
+  ngOnInit() {
+    const submitEventActionType$ = this.submitEventActionType$();
+    if (submitEventActionType$)
+      submitEventActionType$
+        .pipe(takeUntil(this.unsubscribe$))
+        .subscribe((actionType: ActionType) => {
+          if (actionType === ActionType.update && this.modification() === ModificationField.password)
+            this.onSubmit();
+        });
+  }
+
+  ngOnDestroy() {
+    this.unsubscribe$.next();
+    this.unsubscribe$.complete();
   }
 
   onSubmit() {
-    if (!this.userForm) return;
-    if (this.userForm.valid) {
-      this.submitInvalidForm = false;
-      this.userService.modifyUserPassword(this.userForm);
-      this.btnCloseRef.click();
+    const userForm = this.userForm();
+    if (userForm.valid) {
+      this.submitInvalidForm.set(false);
+      console.log(userForm.value);
+      this.userService.modifyUserPassword(userForm);
+      this.btnCloseRef().click();
     } else {
-      this.submitInvalidForm = true;
+      this.submitInvalidForm.set(true);
     }
   }
 }
