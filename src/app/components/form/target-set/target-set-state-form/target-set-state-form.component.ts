820fcd5464bb1f80b45da526e3a29290c7291aaa
CptFranck <francois.caillet71@gmail.com>
2025-04-04 18:34:23 +0200

    update target set state form component with signal input

--- src/app/components/form/target-set/target-set-state-form/target-set-state-form.component.ts
+++ src/app/components/form/target-set/target-set-state-form/target-set-state-form.component.ts
@@ -1,50 +1,48 @@
-import {Component, inject, Input, OnInit} from '@angular/core';
+import {Component, computed, inject, input, signal} from '@angular/core';
 import {NgIf} from "@angular/common";
 import {FormControl, FormGroup, ReactiveFormsModule, Validators} from "@angular/forms";
-import {TargetSetSateSelectComponent} from "../../../selects/target-set-state-select/target-set-sate-select.component";
+import {
+  TargetSetStateSelectComponent
+} from "../../../selects/target-set-state-select/target-set-state-select.component";
 import {TargetSet} from "../../../../interface/dto/target-set";
 import {TargetSetService} from "../../../../services/target-set/target-set.service";
 
 @Component({
-    selector: 'app-target-set-state-form',
-    imports: [
-        NgIf,
-        TargetSetSateSelectComponent,
-        ReactiveFormsModule
-    ],
-    templateUrl: './target-set-state-form.component.html'
+  selector: 'app-target-set-state-form',
+  imports: [
+    NgIf,
+    TargetSetStateSelectComponent,
+    ReactiveFormsModule
+  ],
+  templateUrl: './target-set-state-form.component.html'
 })
-export class TargetSetStateFormComponent implements OnInit {
-  modify: boolean = false;
-  targetSetForm: FormGroup | null = null;
+export class TargetSetStateFormComponent {
+  modify = signal<boolean>(false);
 
-  @Input() targetSet!: TargetSet;
+  readonly targetSet = input.required<TargetSet>();
+
+  readonly targetSetForm = computed<FormGroup>(() => {
+    const targetSetForm: FormGroup = new FormGroup(
+      {
+        state: new FormControl(
+          this.targetSet().state,
+          [Validators.required]),
+      });
+    targetSetForm.addControl("id", new FormControl(this.targetSet().id));
+    return targetSetForm;
+  });
 
   private readonly targetSetService: TargetSetService = inject(TargetSetService);
 
-  ngOnInit() {
-    this.initializeTargetSetForm();
-  }
-
-  initializeTargetSetForm() {
-    this.targetSetForm = new FormGroup(
-      {
-        state: new FormControl(
-          this.targetSet.state,
-          [Validators.required]),
-      });
-    this.targetSetForm.addControl("id", new FormControl(this.targetSet.id));
-  }
-
   openForm(): void {
-    this.modify = true;
+    this.modify.set(true);
   }
 
   submit() {
-    if (!this.targetSetForm) return;
-    if (this.targetSetForm.valid) {
-      this.targetSetService.modifyTargetSetState(this.targetSetForm);
-      this.modify = false;
+    const targetSetForm = this.targetSetForm()
+    if (targetSetForm.valid) {
+      this.targetSetService.modifyTargetSetState(targetSetForm);
+      this.modify.set(false);
     }
   }
 }
