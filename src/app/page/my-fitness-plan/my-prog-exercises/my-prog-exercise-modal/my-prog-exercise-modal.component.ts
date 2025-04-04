dce547aa6f950708ad98667e23fa49b1933a9d73
CptFranck <francois.caillet71@gmail.com>
2025-04-04 12:00:31 +0200

    update my prog exercise modal component with signal input

--- src/app/page/my-fitness-plan/my-prog-exercises/my-prog-exercise-modal/my-prog-exercise-modal.component.ts
+++ src/app/page/my-fitness-plan/my-prog-exercises/my-prog-exercise-modal/my-prog-exercise-modal.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input, TemplateRef, ViewChild} from '@angular/core';
+import {Component, input, model, TemplateRef, ViewChild} from '@angular/core';
 import {ModalButtonComponent} from "../../../../components/modal/modal-button/modal-button.component";
 import {ModalComponent} from "../../../../components/modal/modal/modal.component";
 import {NgIf} from "@angular/common";
@@ -15,30 +15,30 @@ import {
 } from "../../../../components/modal-component/prog-exercise/my-prog-exercise-performance/my-prog-exercise-performance.component";
 
 @Component({
-    selector: 'app-my-prog-exercise-modal',
-    imports: [
-        ModalButtonComponent,
-        ModalComponent,
-        NgIf,
-        MyProgExerciseDetailsDisplayComponent,
-        MyProgExerciseEntityFormComponent,
-        MyProgExercisePerformanceComponent,
-    ],
-    templateUrl: './my-prog-exercise-modal.component.html'
+  selector: 'app-my-prog-exercise-modal',
+  imports: [
+    ModalButtonComponent,
+    ModalComponent,
+    NgIf,
+    MyProgExerciseDetailsDisplayComponent,
+    MyProgExerciseEntityFormComponent,
+    MyProgExercisePerformanceComponent,
+  ],
+  templateUrl: './my-prog-exercise-modal.component.html'
 })
 export class MyProgExerciseModalComponent {
-  @Input() modalTitle!: string;
-  @Input() muscleModalId!: string;
-  @Input() progExercise: ProgExercise | undefined;
-  @Input() action!: ActionType;
+  readonly modalTitle = model.required<string>();
+  readonly muscleModalId = input.required<string>();
+  readonly progExercise = model.required<ProgExercise | undefined>();
+  readonly action = model.required<ActionType>();
 
   @ViewChild("progExerciseModalTemplate") modalTemplate!: TemplateRef<any>;
 
   protected readonly ActionType = ActionType;
 
   onClick(value: undefined) {
-    this.progExercise = value;
-    this.action = ActionType.create;
-    this.modalTitle = "Add new programed exercise";
+    this.progExercise.set(value);
+    this.action.set(ActionType.create);
+    this.modalTitle.set("Add new programed exercise");
   }
 }
