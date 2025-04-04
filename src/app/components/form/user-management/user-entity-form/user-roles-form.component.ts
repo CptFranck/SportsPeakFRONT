40ca664a21e97bbcd285e9fa54cba5b4393799e1
CptFranck <francois.caillet71@gmail.com>
2025-04-04 13:46:22 +0200

    update user roles form component with signal input

--- src/app/components/form/user-management/user-entity-form/user-roles-form.component.ts
+++ src/app/components/form/user-management/user-entity-form/user-roles-form.component.ts
@@ -1,56 +1,60 @@
-import {Component, inject, Input, OnDestroy, OnInit} from '@angular/core';
+import {Component, computed, inject, input, OnDestroy, OnInit, signal} from '@angular/core';
 import {FormControl, FormGroup, ReactiveFormsModule, Validators} from "@angular/forms";
 import {Observable, Subject, takeUntil} from "rxjs";
 import {NgIf} from "@angular/common";
 import {InputControlComponent} from "../../../input-control/input-control.component";
 import {User} from "../../../../interface/dto/user";
 import {UserService} from "../../../../services/user/user.service";
-import {Role} from "../../../../interface/dto/role";
 import {RoleSelectorComponent} from "../../../selectors/role-selector/role-selector.component";
-import {UserLoggedService} from "../../../../services/user-logged/user-logged.service";
 import {ActionType} from "../../../../interface/enum/action-type";
+import {Role} from "../../../../interface/dto/role";
 
 @Component({
-    selector: 'app-user-roles-form',
-    imports: [
-        InputControlComponent,
-        NgIf,
-        ReactiveFormsModule,
-        RoleSelectorComponent,
-    ],
-    templateUrl: './user-roles-form.component.html'
+  selector: 'app-user-roles-form',
+  imports: [
+    InputControlComponent,
+    NgIf,
+    ReactiveFormsModule,
+    RoleSelectorComponent,
+  ],
+  templateUrl: './user-roles-form.component.html'
 })
 export class UserRolesFormComponent implements OnInit, OnDestroy {
-  user: User | undefined;
-  isAdmin: boolean = false;
-  userForm: FormGroup | null = null;
-  submitInvalidForm: boolean = false;
 
-  @Input() btnCloseRef!: HTMLButtonElement;
-  @Input() submitEvents!: Observable<ActionType> | undefined;
+  user = input.required<User | undefined>();
 
-  private readonly unsubscribe$: Subject<void> = new Subject<void>();
-  private readonly userService: UserService = inject(UserService);
-  private readonly userLoggedService: UserLoggedService = inject(UserLoggedService);
+  userForm = computed<FormGroup | null>(() => {
+    const user = this.user();
+    const userRoleIds: number[] = user?.lastName ?
+      user?.roles?.map((role: Role) => role.id) : [];
 
-  @Input() set userInput(value: User | undefined) {
-    this.user = value;
-    this.initializeUserRoleForm();
-  }
+    const userForm: FormGroup = new FormGroup({
+      roleIds: new FormControl(
+        userRoleIds, Validators.required
+      )
+    });
+    if (user)
+      userForm.addControl("id", new FormControl(user.id));
+    return userForm;
+  })
+
+  submitInvalidForm = signal<boolean>(false);
+
+  readonly btnCloseRef = input.required<HTMLButtonElement>();
+  readonly submitEvents = input.required<Observable<ActionType> | undefined>();
+
+  private readonly unsubscribe$ = new Subject<void>();
+  private readonly userService = inject(UserService);
 
   ngOnInit() {
-    this.userLoggedService.currentUser
-      .pipe(takeUntil(this.unsubscribe$))
-      .subscribe(() =>
-        this.isAdmin = this.userLoggedService.isAdmin());
-    if (this.submitEvents)
-      this.submitEvents
+    const submitEvents = this.submitEvents();
+    if (submitEvents)
+      submitEvents
         .pipe(takeUntil(this.unsubscribe$))
         .subscribe((actionType: ActionType) => {
           if (actionType === ActionType.update)
             this.onSubmit();
         });
-    this.initializeUserRoleForm();
   }
 
   ngOnDestroy() {
@@ -58,30 +62,15 @@ export class UserRolesFormComponent implements OnInit, OnDestroy {
     this.unsubscribe$.complete();
   }
 
-  initializeUserRoleForm() {
-    const roleIdsValidator =
-      this.isAdmin ? null : Validators.required;
-    const userRoleIds: number[] = this.user?.lastName ?
-      this.user.roles?.map((role: Role) => role.id) : [];
-
-    this.userForm = new FormGroup({
-      roleIds: new FormControl(
-        userRoleIds, roleIdsValidator
-      )
-    });
-
-    if (this.user)
-      this.userForm.addControl("id", new FormControl(this.user.id));
-  }
-
   onSubmit() {
-    if (!this.userForm) return;
-    if (this.userForm.valid) {
-      this.submitInvalidForm = false;
-      this.userService.modifyUserRoles(this.userForm);
-      this.btnCloseRef.click();
+    const userForm = this.userForm();
+    if (!userForm) return;
+    if (userForm.valid) {
+      this.submitInvalidForm.set(false);
+      this.userService.modifyUserRoles(userForm);
+      this.btnCloseRef().click();
     } else {
-      this.submitInvalidForm = true;
+      this.submitInvalidForm.set(true);
     }
   }
 }
