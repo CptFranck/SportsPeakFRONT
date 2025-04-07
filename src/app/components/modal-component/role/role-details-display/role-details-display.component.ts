b3ccfdcbde6ab71f145c017d9e7f3e74a72143a4
CptFranck <francois.caillet71@gmail.com>
2025-04-07 18:13:19 +0200

    update role detail display component with signal input

--- src/app/components/modal-component/role/role-details-display/role-details-display.component.ts
+++ src/app/components/modal-component/role/role-details-display/role-details-display.component.ts
@@ -1,16 +1,15 @@
-import {Component, Input} from '@angular/core';
+import {Component, input} from '@angular/core';
 import {NgForOf, NgIf} from "@angular/common";
 import {Role} from "../../../../interface/dto/role";
 
 @Component({
-    selector: 'app-role-details-display',
-    imports: [
-        NgForOf,
-        NgIf
-    ],
-    templateUrl: './role-details-display.component.html'
+  selector: 'app-role-details-display',
+  imports: [
+    NgForOf,
+    NgIf
+  ],
+  templateUrl: './role-details-display.component.html'
 })
 export class RoleDetailsDisplayComponent {
-  @Input() role: Role | undefined;
-  @Input() action!: string;
+  readonly role = input<Role>();
 }
