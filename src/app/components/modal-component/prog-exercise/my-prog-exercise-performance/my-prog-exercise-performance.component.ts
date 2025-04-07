fbe3994a96f8ef091194dbd6a5fca8a0e113ce53
CptFranck <francois.caillet71@gmail.com>
2025-04-07 17:48:56 +0200

    update my prog exercise performance component with signal input

--- src/app/components/modal-component/prog-exercise/my-prog-exercise-performance/my-prog-exercise-performance.component.ts
+++ src/app/components/modal-component/prog-exercise/my-prog-exercise-performance/my-prog-exercise-performance.component.ts
@@ -1,17 +1,17 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, computed, input, signal} from '@angular/core';
 import {ProgExercise} from "../../../../interface/dto/prog-exercise";
 import {ifFirstShow, ifNotFirstCollapse} from "../../../../utils/accordion-function";
 import {NgForOf} from "@angular/common";
-import {TargetSet} from "../../../../interface/dto/target-set";
-import {getUpToDateTargetSets, sortLastTargetSetsByIndex} from "../../../../utils/target-set-functions";
-import {TargetSetState} from "../../../../interface/enum/targetSetState";
-import {sortPerformanceLogsBySet} from "../../../../utils/performance-log-functions";
 import {PerformanceLog} from "../../../../interface/dto/performance-log";
 import {DictionaryItem} from "../../../../interface/utils/dictionary-item";
 import {
   PerformanceLogsSetChartComponent
 } from "../../../chart/performance-logs-set-chart/performance-logs-set-chart.component";
 import {RangeInputComponent} from "../../../input/range-input/range-input.component";
+import {getUpToDateTargetSets, sortLastTargetSetsByIndex} from "../../../../utils/target-set-functions";
+import {TargetSet} from "../../../../interface/dto/target-set";
+import {TargetSetState} from "../../../../interface/enum/targetSetState";
+import {sortPerformanceLogsBySet} from "../../../../utils/performance-log-functions";
 
 @Component({
   selector: 'app-my-prog-exercise-performance',
@@ -22,36 +22,35 @@ import {RangeInputComponent} from "../../../input/range-input/range-input.compon
   ],
   templateUrl: './my-prog-exercise-performance.component.html'
 })
-export class MyProgExercisePerformanceComponent implements OnInit {
-  progExercise!: ProgExercise;
-  rowColNumber: number = 3;
-  rowColNumberClass: string = "row-cols-";
-  accordionAllPerformance: string = "accordionAllPerformance";
-  targetSetsPerformanceLogsSortedByLogDate: DictionaryItem<DictionaryItem<PerformanceLog[]>[]>[] = []
+export class MyProgExercisePerformanceComponent {
+  progExercise = input.required<ProgExercise | undefined>();
 
-  protected readonly ifFirstShow = ifFirstShow;
-  protected readonly ifNotFirstCollapse = ifNotFirstCollapse;
-
-  @Input() set progExerciseInput(progExercise: ProgExercise | undefined) {
+  targetSetsPerformanceLogsSortedByLogDate = computed<DictionaryItem<DictionaryItem<PerformanceLog[]>[]>[]>(() => {
+    const progExercise = this.progExercise();
+    const targetSetsPerformanceLogsSortedByLogDate: DictionaryItem<DictionaryItem<PerformanceLog[]>[]>[] = [];
     if (progExercise) {
-      this.progExercise = progExercise;
       let targetSets: TargetSet[] = getUpToDateTargetSets(progExercise)
         .sort(sortLastTargetSetsByIndex)
         .filter((targetSets: TargetSet) => targetSets.state === TargetSetState.USED);
       targetSets.forEach((targetSet: TargetSet) => {
-        this.targetSetsPerformanceLogsSortedByLogDate.push({
+        targetSetsPerformanceLogsSortedByLogDate.push({
           key: targetSet.index.toString(),
-          value: sortPerformanceLogsBySet(this.progExercise, targetSet)
+          value: sortPerformanceLogsBySet(progExercise, targetSet)
         })
       })
     }
-  }
+    return targetSetsPerformanceLogsSortedByLogDate;
+  })
 
-  ngOnInit() {
-    this.rowColNumberClass = this.rowColNumberClass + this.rowColNumber.toString();
-  }
+  rowColNumber = signal<number>(3);
+  rowColNumberClass = computed<string>(() => "row-cols-" + this.rowColNumber());
 
-  changeRowColNumber($event: number) {
-    this.rowColNumberClass = this.rowColNumberClass.replace(this.rowColNumberClass.slice(-1), $event.toString())
+  readonly accordionAllPerformance: string = "accordionAllPerformance";
+
+  protected readonly ifFirstShow = ifFirstShow;
+  protected readonly ifNotFirstCollapse = ifNotFirstCollapse;
+
+  changeRowColNumber(value: number) {
+    this.rowColNumber.set(value)
   }
 }
