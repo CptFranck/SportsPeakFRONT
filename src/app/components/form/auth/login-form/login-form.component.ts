8a245cd44738433f35ce1679528bbdd53cd94881
CptFranck <francois.caillet71@gmail.com>
2025-04-09 13:48:46 +0200

    update my prog exercise entity form component with input signal

--- src/app/components/form/auth/login-form/login-form.component.ts
+++ src/app/components/form/auth/login-form/login-form.component.ts
@@ -1,49 +1,39 @@
-import {Component, inject, OnInit} from '@angular/core';
+import {Component, inject, signal} from '@angular/core';
 import {FormControl, FormGroup, ReactiveFormsModule, Validators} from "@angular/forms";
 import {AuthService} from "../../../../services/auth/auth.service";
 import {InputControlComponent} from "../../../input-control/input-control.component";
-import {NgIf} from "@angular/common";
 
 @Component({
-    selector: 'app-login-form',
-    imports: [
-        InputControlComponent,
-        NgIf,
-        ReactiveFormsModule
-    ],
-    templateUrl: './login-form.component.html'
+  selector: 'app-login-form',
+  imports: [
+    InputControlComponent,
+    ReactiveFormsModule
+  ],
+  templateUrl: './login-form.component.html'
 })
-export class LoginFormComponent implements OnInit {
-  loginForm: FormGroup | null = null;
-  submitInvalidForm: boolean = false;
+export class LoginFormComponent {
+  loginForm = signal<FormGroup>(new FormGroup({
+    email: new FormControl(
+      // "",
+      "admin@sportspeak.com",
+      [Validators.required,
+        Validators.email]),
+    password: new FormControl(
+      // "",
+      "ChangeMeInProd1!",
+      [Validators.required]),
+  }));
+  submitInvalidForm = signal<boolean>(false);
 
   private readonly authService: AuthService = inject(AuthService);
 
-  ngOnInit() {
-    this.initializeLoginForm();
-  }
-
-  initializeLoginForm() {
-    this.loginForm = new FormGroup({
-      email: new FormControl(
-        // "",
-        "admin@sportspeak.com",
-        [Validators.required,
-          Validators.email]),
-      password: new FormControl(
-        // "",
-        "ChangeMeInProd1!",
-        [Validators.required]),
-    });
-  }
-
   onSubmitLoginForm() {
-    if (!this.loginForm) return;
-    if (this.loginForm.valid) {
-      this.submitInvalidForm = false;
-      this.authService.login(this.loginForm);
+    const loginForm = this.loginForm();
+    if (loginForm.valid) {
+      this.submitInvalidForm.set(false);
+      this.authService.login(loginForm);
     } else {
-      this.submitInvalidForm = true;
+      this.submitInvalidForm.set(true);
     }
   }
 }
