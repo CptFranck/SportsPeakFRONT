80c8ff20c85f856e31462a7b795e70b400ba4a35
CptFranck <francois.caillet71@gmail.com>
2025-04-09 12:27:59 +0200

    update exercise type entity form component with input signal

--- src/app/components/form/exercise-type/exercise-type-entity-form/exercise-type-entity-form.component.ts
+++ src/app/components/form/exercise-type/exercise-type-entity-form/exercise-type-entity-form.component.ts
@@ -1,72 +1,39 @@
-import {Component, inject, Input, OnDestroy, OnInit} from '@angular/core';
+import {Component, computed, inject, input, OnDestroy, OnInit, signal} from '@angular/core';
 import {FormControl, FormGroup, ReactiveFormsModule, Validators} from "@angular/forms";
 import {Subject, takeUntil} from "rxjs";
 import {InputControlComponent} from "../../../input-control/input-control.component";
 import {ExerciseType} from "../../../../interface/dto/exercise-type";
-import {NgIf} from "@angular/common";
 import {ExerciseTypeService} from "../../../../services/exercise-type/exercise-type.service";
-import {Exercise} from "../../../../interface/dto/exercise";
 import {ExerciseSelectorComponent} from "../../../selectors/exercise-selector/exercise-selector.component";
 import {UserLoggedService} from "../../../../services/user-logged/user-logged.service";
 import {ActionType} from "../../../../interface/enum/action-type";
+import {Exercise} from "../../../../interface/dto/exercise";
 
 @Component({
-    selector: 'app-exercise-type-entity-form',
-    imports: [
-        InputControlComponent,
-        ReactiveFormsModule,
-        ExerciseSelectorComponent,
-        NgIf
-    ],
-    templateUrl: './exercise-type-entity-form.component.html'
+  selector: 'app-exercise-type-entity-form',
+  imports: [
+    InputControlComponent,
+    ReactiveFormsModule,
+    ExerciseSelectorComponent
+  ],
+  templateUrl: './exercise-type-entity-form.component.html'
 })
 export class ExerciseTypeEntityFormComponent implements OnInit, OnDestroy {
-  exerciseType: ExerciseType | undefined;
-  exerciseTypeForm: FormGroup | null = null;
-  submitInvalidForm: boolean = false;
-  isAdmin: boolean = false;
+  isAdmin = false;
 
-  @Input() btnCloseRef!: HTMLButtonElement;
-  @Input() submitEventActionType$!: Subject<ActionType> | undefined;
+  readonly btnCloseRef = input.required<HTMLButtonElement>();
+  readonly exerciseType = input.required<ExerciseType | undefined>();
+  readonly submitEventActionType$ = input.required<Subject<ActionType> | undefined>();
 
-  private readonly unsubscribe$: Subject<void> = new Subject<void>();
-  private readonly userLoggedService: UserLoggedService = inject(UserLoggedService);
-  private readonly exerciseTypeService: ExerciseTypeService = inject(ExerciseTypeService);
+  exerciseTypeForm = computed<FormGroup>(() => {
+    const exerciseIdsValidator = this.isAdmin ? null : Validators.required;
 
-  @Input() set exerciseTypeInput(value: ExerciseType | undefined) {
-    this.exerciseType = value;
-    this.initializeExerciseTypeForm();
-  }
+    const exerciseType = this.exerciseType()
+    const exerciseTypeName: string = exerciseType ? exerciseType.name : "";
+    const exerciseTypeGoal: string = exerciseType ? exerciseType.goal : "";
+    const muscleExerciseIds: number[] = exerciseType?.exercises ? exerciseType.exercises?.map((ex: Exercise) => ex.id) : [];
 
-  ngOnInit() {
-    this.userLoggedService.currentUser
-      .pipe(takeUntil(this.unsubscribe$))
-      .subscribe(() =>
-        this.isAdmin = this.userLoggedService.isAdmin());
-    if (this.submitEventActionType$)
-      this.submitEventActionType$
-        .pipe(takeUntil(this.unsubscribe$))
-        .subscribe((actionType: ActionType) => {
-          if (actionType === ActionType.create || actionType === ActionType.update)
-            this.onSubmit();
-        });
-    this.initializeExerciseTypeForm();
-  }
-
-  ngOnDestroy() {
-    this.unsubscribe$.next();
-    this.unsubscribe$.complete();
-  }
-
-  initializeExerciseTypeForm() {
-    const exerciseIdsValidator =
-      this.isAdmin ? null : Validators.required;
-    const exerciseTypeName: string = this.exerciseType ? this.exerciseType.name : "";
-    const exerciseTypeGoal: string = this.exerciseType ? this.exerciseType.goal : "";
-    const muscleExerciseIds: number[] = this.exerciseType?.exercises ?
-      this.exerciseType.exercises?.map((ex: Exercise) => ex.id) : [];
-
-    this.exerciseTypeForm = new FormGroup({
+    const exerciseTypeForm: FormGroup = new FormGroup({
       name: new FormControl(exerciseTypeName,
         [Validators.required,
           Validators.minLength(3),
@@ -81,22 +48,50 @@ export class ExerciseTypeEntityFormComponent implements OnInit, OnDestroy {
       ),
     });
 
-    if (this.exerciseType)
-      this.exerciseTypeForm.addControl("id", new FormControl(this.exerciseType.id));
+    if (exerciseType)
+      exerciseTypeForm.addControl("id", new FormControl(exerciseType.id));
+
+    return exerciseTypeForm;
+  });
+  submitInvalidForm = signal<boolean>(false);
+
+  private readonly unsubscribe$ = new Subject<void>();
+  private readonly userLoggedService = inject(UserLoggedService);
+  private readonly exerciseTypeService = inject(ExerciseTypeService);
+
+
+  ngOnInit() {
+    this.userLoggedService.currentUser
+      .pipe(takeUntil(this.unsubscribe$))
+      .subscribe(() =>
+        this.isAdmin = this.userLoggedService.isAdmin());
+    const submitEventActionType$ = this.submitEventActionType$();
+    if (submitEventActionType$)
+      submitEventActionType$
+        .pipe(takeUntil(this.unsubscribe$))
+        .subscribe((actionType: ActionType) => {
+          if (actionType === ActionType.create || actionType === ActionType.update)
+            this.onSubmit();
+        });
+  }
+
+  ngOnDestroy() {
+    this.unsubscribe$.next();
+    this.unsubscribe$.complete();
   }
 
   onSubmit() {
-    if (!this.exerciseTypeForm) return;
-    if (this.exerciseTypeForm.valid) {
-      this.submitInvalidForm = false;
-      if (!this.exerciseTypeForm.value.id) {
-        this.exerciseTypeService.addExerciseType(this.exerciseTypeForm);
+    const exerciseTypeForm = this.exerciseTypeForm();
+    if (exerciseTypeForm.valid) {
+      this.submitInvalidForm.set(false);
+      if (!exerciseTypeForm.value.id) {
+        this.exerciseTypeService.addExerciseType(exerciseTypeForm);
       } else {
-        this.exerciseTypeService.modifyExerciseType(this.exerciseTypeForm);
+        this.exerciseTypeService.modifyExerciseType(exerciseTypeForm);
       }
-      this.btnCloseRef.click();
+      this.btnCloseRef().click();
     } else {
-      this.submitInvalidForm = true;
+      this.submitInvalidForm.set(true);
     }
   }
 }
