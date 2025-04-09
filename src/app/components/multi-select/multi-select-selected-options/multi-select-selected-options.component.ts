9bd74286389c281bd84c7c6ef05b5d4cb1b06b75
CptFranck <francois.caillet71@gmail.com>
2025-04-09 18:08:20 +0200

    update multi select selected options component with input signal

--- src/app/components/multi-select/multi-select-selected-options/multi-select-selected-options.component.ts
+++ src/app/components/multi-select/multi-select-selected-options/multi-select-selected-options.component.ts
@@ -1,21 +1,21 @@
-import {Component, EventEmitter, Input, Output} from '@angular/core';
+import {Component, EventEmitter, input, Output} from '@angular/core';
 import {NgForOf, NgIf} from "@angular/common";
 import {MultiSelectOptionSelected} from "../../../interface/components/multi-select/multiSelectOptionSelected";
 
 @Component({
-    selector: 'app-multi-select-selected-options',
-    imports: [
-        NgForOf,
-        NgIf
-    ],
-    templateUrl: './multi-select-selected-options.component.html',
-    styleUrl: './multi-select-selected-options.component.css'
+  selector: 'app-multi-select-selected-options',
+  imports: [
+    NgForOf,
+    NgIf
+  ],
+  templateUrl: './multi-select-selected-options.component.html',
+  styleUrl: './multi-select-selected-options.component.css'
 })
 export class MultiSelectSelectedOptionsComponent {
-  @Input() selectedOptions!: number[]
-  @Input() displayedSelectedOptions!: MultiSelectOptionSelected[];
+  readonly selectedOptions = input.required<number[]>();
+  readonly displayedSelectedOptions = input.required<MultiSelectOptionSelected[]>();
 
-  @Output() onRemoveTag: EventEmitter<any> = new EventEmitter();
+  @Output() onRemoveTag = new EventEmitter();
 
   onCLickRemoveTag($event: MouseEvent) {
     this.onRemoveTag.emit($event);
