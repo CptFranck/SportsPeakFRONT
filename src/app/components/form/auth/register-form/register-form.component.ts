f34940e8ec3d1dea8ffed6e3425d776fd6c7ca04
CptFranck <francois.caillet71@gmail.com>
2025-04-09 14:00:39 +0200

    update register form component with input signal

--- src/app/components/form/auth/register-form/register-form.component.ts
+++ src/app/components/form/auth/register-form/register-form.component.ts
@@ -1,23 +1,20 @@
-import {Component, inject, OnInit} from '@angular/core';
+import {Component, inject, signal} from '@angular/core';
 import {FormControl, FormGroup, ReactiveFormsModule, Validators} from "@angular/forms";
 import {AuthService} from "../../../../services/auth/auth.service";
 import {InputControlComponent} from "../../../input-control/input-control.component";
-import {NgIf} from "@angular/common";
 import {confirmValidator} from "../../../../validators/confirmValidator";
 
 @Component({
-    selector: 'app-register-form',
-    imports: [
-        InputControlComponent,
-        ReactiveFormsModule,
-        NgIf
-    ],
-    templateUrl: './register-form.component.html'
+  selector: 'app-register-form',
+  imports: [
+    InputControlComponent,
+    ReactiveFormsModule
+  ],
+  templateUrl: './register-form.component.html'
 })
-export class RegisterFormComponent implements OnInit {
-  submitInvalidForm: boolean = false;
-  registerFormGroup: FormGroup | null = null;
-  draft = {
+export class RegisterFormComponent {
+  submitInvalidForm = signal<boolean>(false);
+  private draft = {
     "firstName": "Admin",
     "lastName": "Admin",
     "username": "admin",
@@ -25,64 +22,58 @@ export class RegisterFormComponent implements OnInit {
     "password": "ChangeMeInProd1!",
     "confirmPassword": "ChangeMeInProd1!"
   }
-  private readonly authService: AuthService = inject(AuthService);
+  registerFormGroup = signal<FormGroup>(new FormGroup({
+      firstName: new FormControl(
+        // "",
+        this.draft.firstName,
+        [Validators.required,
+          Validators.minLength(1),
+          Validators.maxLength(50)]),
+      lastName: new FormControl(
+        // "",
+        this.draft.lastName,
+        [Validators.required,
+          Validators.minLength(1),
+          Validators.maxLength(50)]),
+      username: new FormControl(
+        // "",
+        this.draft.username,
+        [Validators.required,
+          Validators.minLength(1),
+          Validators.maxLength(50),
+          Validators.pattern('([a-zA-Z0-9_]+)[^\\s\\t\\n\\r]{1,}')]),
+      email: new FormControl(
+        // "",
+        this.draft.email,
+        [Validators.required,
+          Validators.email]),
+      password: new FormControl(
+        // "",
+        this.draft.password,
+        [Validators.required,
+          Validators.minLength(10),
+          Validators.maxLength(50),
+          Validators.pattern('^(?=.*\\d)(?=.*[a-z]+)(?=.*[A-Z]+)[^\\s\\t\\n\\r]{10,}$')
+        ]),
+      confirmPassword: new FormControl(
+        // "",
+        this.draft.confirmPassword,
+        [Validators.required],
+      ),
+    },
+    {
+      validators: confirmValidator('password', 'confirmPassword')
+    }));
 
-  ngOnInit() {
-    this.initializeRegisterForm();
-  }
-
-  initializeRegisterForm() {
-    this.registerFormGroup = new FormGroup({
-        firstName: new FormControl(
-          // "",
-          this.draft.firstName,
-          [Validators.required,
-            Validators.minLength(1),
-            Validators.maxLength(50)]),
-        lastName: new FormControl(
-          // "",
-          this.draft.lastName,
-          [Validators.required,
-            Validators.minLength(1),
-            Validators.maxLength(50)]),
-        username: new FormControl(
-          // "",
-          this.draft.username,
-          [Validators.required,
-            Validators.minLength(1),
-            Validators.maxLength(50),
-            Validators.pattern('([a-zA-Z0-9_]+)[^\\s\\t\\n\\r]{1,}')]),
-        email: new FormControl(
-          // "",
-          this.draft.email,
-          [Validators.required,
-            Validators.email]),
-        password: new FormControl(
-          // "",
-          this.draft.password,
-          [Validators.required,
-            Validators.minLength(10),
-            Validators.maxLength(50),
-            Validators.pattern('^(?=.*\\d)(?=.*[a-z]+)(?=.*[A-Z]+)[^\\s\\t\\n\\r]{10,}$')
-          ]),
-        confirmPassword: new FormControl(
-          // "",
-          this.draft.confirmPassword,
-          [Validators.required],
-        ),
-      },
-      {
-        validators: confirmValidator('password', 'confirmPassword')
-      });
-  }
+  private readonly authService = inject(AuthService);
 
   onSubmitRegisterForm() {
-    if (!this.registerFormGroup) return
-    if (this.registerFormGroup.valid) {
-      this.submitInvalidForm = false;
-      this.authService.register(this.registerFormGroup)
+    const registerFormGroup = this.registerFormGroup();
+    if (registerFormGroup.valid) {
+      this.submitInvalidForm.set(false);
+      this.authService.register(registerFormGroup)
     } else {
-      this.submitInvalidForm = true;
+      this.submitInvalidForm.set(true);
     }
   }
 }
