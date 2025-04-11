3f49c1ddcfc07a6a4156e9cba9aa1004d0ea4399
CptFranck <francois.caillet71@gmail.com>
2025-04-11 12:57:45 +0200

    update users management component about all signal

--- src/app/page/back/user-management/users-management/users-management.component.ts
+++ src/app/page/back/user-management/users-management/users-management.component.ts
@@ -1,4 +1,4 @@
-import {Component, inject, OnDestroy, OnInit, TemplateRef, ViewChild} from '@angular/core';
+import {Component, inject, OnDestroy, OnInit, signal} from '@angular/core';
 import {UsersManagementArrayComponent} from "../users-management-array/users-management-array.component";
 import {UsersManagementModalComponent} from "../user-management-modal/users-management-modal.component";
 import {LoadingComponent} from "../../../../components/loading/loading.component";
@@ -7,35 +7,43 @@ import {ActionType} from "../../../../interface/enum/action-type";
 import {UserService} from "../../../../services/user/user.service";
 import {FormIndicator} from "../../../../interface/utils/form-indicator";
 import {Subject, takeUntil} from "rxjs";
+import {SearchBarComponent} from "../../../../components/search-bar/search-bar.component";
 
 @Component({
   selector: 'app-users',
   imports: [
     LoadingComponent,
     UsersManagementArrayComponent,
-    UsersManagementModalComponent
+    UsersManagementModalComponent,
+    SearchBarComponent
   ],
   templateUrl: './users-management.component.html'
 })
 export class UsersManagementComponent implements OnInit, OnDestroy {
-  loading: boolean = true;
-  users: User[] = [];
-  user: User | undefined;
-  action: ActionType = ActionType.create;
-  modalTitle: string = "";
-  muscleModalId: string = "userModal";
-  @ViewChild("modalTemplate") modalTemplate!: TemplateRef<any>;
+  loading = signal<boolean>(true);
+  displayedUsers = signal<User[]>([]);
+  action = signal<ActionType>(ActionType.create);
+  modalTitle = signal<string>("");
+  user = signal<User | undefined>(undefined);
 
-  private readonly unsubscribe$: Subject<void> = new Subject<void>();
-  private readonly userService: UserService = inject(UserService);
+  readonly muscleModalId: string = "userModal";
+
+  private searchInput = "";
+  private users: User[] = [];
+
+  private readonly unsubscribe$ = new Subject<void>();
+  private readonly userService = inject(UserService);
 
   ngOnInit(): void {
     this.userService.users
       .pipe(takeUntil(this.unsubscribe$))
-      .subscribe((users: User[]) => this.users = users);
+      .subscribe((users: User[]) => {
+        this.users = users;
+        this.updateDisplayedUsers();
+      });
     this.userService.isLoading
       .pipe(takeUntil(this.unsubscribe$))
-      .subscribe((isLoading: boolean) => this.loading = isLoading);
+      .subscribe((isLoading: boolean) => this.loading.set(isLoading));
   }
 
   ngOnDestroy() {
@@ -44,8 +52,33 @@ export class UsersManagementComponent implements OnInit, OnDestroy {
   }
 
   setUser(formIndicator: FormIndicator) {
-    this.user = formIndicator.object;
-    this.action = formIndicator.actionType;
-    this.modalTitle = formIndicator.object.username;
+    this.user.set(formIndicator.object);
+    this.action.set(formIndicator.actionType);
+    this.modalTitle.set(formIndicator.object.username);
+  }
+
+  searchUser(input: string) {
+    this.searchInput = input;
+    this.updateDisplayedUsers();
+  }
+
+  updateDisplayedUsers() {
+    const localInput = this.searchInput.toLowerCase();
+    if (this.searchInput === "") {
+      this.displayedUsers.set(this.users);
+      return;
+    }
+    const exercisesFiltered = this.filterUsers(localInput);
+    this.displayedUsers.set(exercisesFiltered);
+  }
+
+  filterUsers(localInput: string) {
+
+    return this.users.filter((user: User) => {
+      return user.firstName.toLowerCase().includes(localInput) ||
+        user.lastName.toLowerCase().includes(localInput) ||
+        user.email.toLowerCase().includes(localInput) ||
+        user.username.toLowerCase().includes(localInput);
+    })
   }
 }
