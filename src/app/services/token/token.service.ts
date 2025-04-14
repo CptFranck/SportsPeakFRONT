461abf95ea433cd50070a35bdf5cf31fbc17a8b7
CptFranck <francois.caillet71@gmail.com>
2025-04-14 19:07:04 +0200

    update token service

--- src/app/services/token/token.service.ts
+++ src/app/services/token/token.service.ts
@@ -1,52 +1,59 @@
 import {inject, Injectable} from '@angular/core';
-import {BehaviorSubject} from "rxjs";
 import {LocalStorageService} from "../local-storage/local-storage.service";
 import {AuthToken} from "../../interface/dto/token";
+import {Auth} from "../../interface/dto/auth";
 
 @Injectable({
   providedIn: 'root'
 })
 export class TokenService {
 
-  authToken: BehaviorSubject<AuthToken | null> = new BehaviorSubject<AuthToken | null>(null);
-  private readonly localStorageService: LocalStorageService = inject(LocalStorageService);
+  authToken: AuthToken | null = null;
+  private readonly localStorageService = inject(LocalStorageService);
 
   constructor() {
     let token: AuthToken | null = this.getSavedToken();
-    if (token)
-      if (!this.isTokenExpired(token))
-        this.authToken.next(token);
+    if (this.isTokenValid(token))
+      this.authToken = token;
+    else
+      this.removeCurrentToken();
   }
 
   getCurrentToken() {
-    return this.authToken.value;
+    return this.authToken;
   }
 
-  setCurrentToken(authToken: AuthToken) {
-    this.authToken.next(authToken);
-    let authTokenJson: string = JSON.stringify(authToken);
+  setCurrentToken(data: Auth) {
+    const authToken = this.createAuthToken(data);
+    this.authToken = authToken;
+    let authTokenJson = JSON.stringify(authToken);
     this.localStorageService.saveData("authToken", authTokenJson);
   }
 
   removeCurrentToken() {
-    this.authToken.next(null);
+    this.authToken = null;
     this.localStorageService.removeData("authToken");
   }
 
-  private isTokenExpired(authToken: AuthToken) {
-    if (authToken) {
-      const isExpired: boolean = new Date(authToken.expiration) < new Date();
-      if (isExpired)
-        return true
+  isTokenValid(authToken: AuthToken | null): boolean {
+    if (authToken !== null) {
+      return new Date(authToken.expiration) > new Date();
     }
     return false;
   }
 
+  private createAuthToken(data: Auth): AuthToken {
+    return {
+      tokenType: data.tokenType,
+      accessToken: data.accessToken,
+      expiration: new Date(data.expiration)
+    };
+  }
+
   private getSavedToken(): AuthToken | null {
-    let authTokenJson: string | null = this.localStorageService.getData("authToken")
-    if (authTokenJson) {
+    let authTokenJson: string | null = this.localStorageService.getData("authToken");
+    if (authTokenJson)
       return JSON.parse(authTokenJson);
-    }
     return null;
   }
 }
