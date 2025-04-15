b31708cff6cf5aeda51da99c8fa2bc00a060fe77
CptFranck <francois.caillet71@gmail.com>
2025-04-15 17:28:58 +0200

    Minor fix

--- src/app/components/alert/alert.component.ts
+++ src/app/components/alert/alert.component.ts
@@ -14,7 +14,9 @@ import {Dictionary} from "../../interface/utils/dictionary";
 export class AlertComponent implements OnInit {
   readonly alert = input.required<Alert>();
   readonly timeout = input<number>(3);
+  readonly activeTimeout = input<boolean>(true);
 
+  disableCloseButton = signal<boolean>(false);
   transitionStyle = signal<Dictionary<string>>({
     'transition': `opacity ${this.timeout()}s ease-out`,
     'opacity': '1'
@@ -26,30 +28,35 @@ export class AlertComponent implements OnInit {
   private removeTimeout: any;
 
   ngOnInit() {
-    this.fadeTimeout = setTimeout(() => {
-      this.transitionStyle.update(value => ({...value, ['opacity']: '0'}));
-      this.removeTimeout = setTimeout(() => {
-        this.removedAlert.emit(this.alert())
-      }, 3000)
-    }, 100);
+    if (this.activeTimeout())
+      this.fadeTimeout = setTimeout(() => {
+        this.transitionStyle.update(value => ({...value, ['opacity']: '0'}));
+        this.removeTimeout = setTimeout(() => {
+          this.removedAlert.emit(this.alert())
+        }, 3000)
+      }, 100);
   }
 
   pauseFadeOut() {
-    clearTimeout(this.fadeTimeout);
-    clearTimeout(this.removeTimeout);
-    this.transitionStyle.set({'transition': 'none', 'opacity': '1',});
-    setTimeout(() => {
-      this.transitionStyle.update(value => ({...value, ['transition']: `opacity ${this.timeout()}s ease-out`}));
-    }, 100)
+    if (this.activeTimeout()) {
+      clearTimeout(this.fadeTimeout);
+      clearTimeout(this.removeTimeout);
+      this.transitionStyle.set({'transition': 'none', 'opacity': '1',});
+      setTimeout(() => this.transitionStyle.update(value => ({
+        ...value,
+        ['transition']: `opacity ${this.timeout()}s ease-out`
+      })), 100)
+    }
   }
 
   removeAlert() {
+    this.disableCloseButton.set(true);
+    clearTimeout(this.fadeTimeout);
+    clearTimeout(this.removeTimeout);
     this.transitionStyle.set({
       'transition': `opacity .2s ease-out`,
       'opacity': '0',
     });
-    setTimeout(() => {
-      this.removedAlert.emit(this.alert())
-    }, 200)
+    setTimeout(() => this.removedAlert.emit(this.alert()), 400)
   }
 }
