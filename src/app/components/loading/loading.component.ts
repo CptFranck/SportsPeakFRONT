9af86d55b988771ab769d91946a61c31d627218a
CptFranck <francois.caillet71@gmail.com>
2025-04-16 18:25:46 +0200

    rework loading component

--- src/app/components/loading/loading.component.ts
+++ src/app/components/loading/loading.component.ts
@@ -1,6 +1,6 @@
 import {Component, input} from '@angular/core';
-import {animate, style, transition, trigger} from "@angular/animations";
 import {NgIf} from "@angular/common";
+import {collapseHeight} from "../../animation/collapseHeigh";
 
 @Component({
   selector: 'app-loading',
@@ -8,18 +8,7 @@ import {NgIf} from "@angular/common";
     NgIf
   ],
   templateUrl: './loading.component.html',
-  animations: [
-    trigger('collapseHeight', [
-      transition(':enter', [
-        style({height: '0', opacity: 0, overflow: 'hidden'}),
-        animate('300ms ease-in-out', style({height: '*', opacity: 1}))
-      ]),
-      transition(':leave', [
-        style({height: '*', opacity: 1, overflow: 'hidden'}),
-        animate('300ms ease-in-out', style({height: '0', opacity: 0}))
-      ])
-    ])
-  ]
+  animations: [collapseHeight]
 })
 export class LoadingComponent {
   readonly loading = input.required<boolean>();
