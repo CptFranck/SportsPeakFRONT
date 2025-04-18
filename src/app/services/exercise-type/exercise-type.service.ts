0a46a1668fcec7c73f2e40863039de784bc22bf7
CptFranck <francois.caillet71@gmail.com>
2025-04-18 14:36:34 +0200

    rework exercise type service

--- src/app/services/exercise-type/exercise-type.service.ts
+++ src/app/services/exercise-type/exercise-type.service.ts
@@ -1,5 +1,5 @@
 import {inject, Injectable} from '@angular/core';
-import {Apollo, MutationResult} from "apollo-angular";
+import {MutationResult} from "apollo-angular";
 import {FormGroup} from "@angular/forms";
 import {
   ADD_EXERCISE_TYPES,
@@ -9,194 +9,71 @@ import {
 } from "../../graphql/operations/exercise-type.operation";
 import {AlertService} from "../alert/alert.service";
 import {ExerciseType} from "../../interface/dto/exercise-type";
-import {ApolloCache, ApolloQueryResult} from "@apollo/client";
+import {ApolloQueryResult} from "@apollo/client";
 import {BehaviorSubject} from "rxjs";
+import {ApolloWrapperService} from "../apollo-wrapper/apollo-wrapper.service";
 
 @Injectable({
   providedIn: 'root'
 })
 export class ExerciseTypeService {
 
-  exerciseTypes: BehaviorSubject<ExerciseType[]> = new BehaviorSubject<ExerciseType[]>([]);
-  isLoading: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(true);
+  exerciseTypes = new BehaviorSubject<ExerciseType[]>([]);
+  isLoading = new BehaviorSubject<boolean>(true);
 
-  private readonly apollo: Apollo = inject(Apollo);
-  private readonly alertService: AlertService = inject(AlertService);
-
-  // private exerciseTypes: WritableSignal<ExerciseType[]> = signal<ExerciseType[]>([]);
-  // private isLoading: WritableSignal<boolean> = signal<boolean>(true);
+  private readonly alertService = inject(AlertService);
+  private readonly apolloWrapperService = inject(ApolloWrapperService);
 
   constructor() {
     this.getExerciseTypes();
   }
 
-  // get allExerciseTypes() {
-  //   return computed(() => this.exerciseTypes());
-  // }
-  //
-  // get loadingStatus() {
-  //   return computed(() => this.isLoading());
-  // }
-
-  // getExerciseTypes() {
-  //   this.isLoading.next(true);
-  //   this.apollo.watchQuery({
-  //     query: GET_EXERCISE_TYPES,
-  //   })
-  //     .valueChanges
-  //     .subscribe((result: ApolloQueryResult<any>): void => {
-  //       if (result.errors) {
-  //         this.alertService.graphQLErrorAlertHandler(result.errors);
-  //       } else {
-  //         this.exerciseTypes.next(result.data.getExerciseTypes);
-  //         this.isLoading.next(result.loading);
-  //       }
-  //     });
-  // }
-
   getExerciseTypes() {
     this.isLoading.next(true);
-    this.apollo.watchQuery({
+    this.apolloWrapperService.watchQuery({
       query: GET_EXERCISE_TYPES,
     }).valueChanges.subscribe(({data, errors, loading}: ApolloQueryResult<any>) => {
-      if (errors) {
+      if (errors)
         this.alertService.graphQLErrorAlertHandler(errors);
-      } else {
-        this.exerciseTypes.next(data.getExerciseTypes);
-        this.isLoading.next(loading);
-      }
+      this.exerciseTypes.next(data.getExerciseTypes);
+      this.isLoading.next(loading);
     });
   }
 
-  // addExerciseType(exerciseTypeForm: FormGroup) {
-  //   this.apollo.mutate({
-  //     mutation: ADD_EXERCISE_TYPES,
-  //     variables: {
-  //       inputNewExerciseType: exerciseTypeForm.value,
-  //     },
-  //     refetchQueries: [{
-  //       query: GET_EXERCISE_TYPES,
-  //     }]
-  //   }).subscribe((result: MutationResult) => {
-  //     if (result.errors) {
-  //       this.alertService.graphQLErrorAlertHandler(result.errors);
-  //     } else {
-  //       let message: string = "ExerciseType " + result.data.addExerciseType.name + " been successfully created.";
-  //       this.alertService.addSuccessAlert(message);
-  //     }
-  //   });
-  // }
-
   addExerciseType(exerciseTypeForm: FormGroup) {
-    this.apollo.mutate({
+    this.apolloWrapperService.mutate({
       mutation: ADD_EXERCISE_TYPES,
       variables: {
         inputNewExerciseType: exerciseTypeForm.value,
-      },
-      update: (cache: ApolloCache<{ getExerciseTypes: ExerciseType[] }>, {data}: MutationResult) => {
-        if (!data) return;
-        const newExerciseType: ExerciseType = data.addExerciseType;
-        const existingData: any = cache.readQuery({query: GET_EXERCISE_TYPES});
-        cache.writeQuery({
-          query: GET_EXERCISE_TYPES,
-          data: {
-            getExerciseTypes: [...existingData.getExerciseTypes, newExerciseType]
-          }
-        });
       }
     }).subscribe(({errors, data}: MutationResult) => {
       if (errors)
         this.alertService.graphQLErrorAlertHandler(errors);
-      else if (data)
-        this.alertService.addSuccessAlert(`ExerciseType ${data.addExerciseType.name} created successfully.`);
-      else
-        this.alertService.addErrorAlert("data is null or undefined");
+      this.alertService.addSuccessAlert(`ExerciseType ${data.addExerciseType.name} has been successfully created.`);
     });
   }
 
-  // modifyExerciseType(muscleForm: FormGroup) {
-  //   this.apollo.mutate({
-  //     mutation: MOD_EXERCISE_TYPES,
-  //     variables: {
-  //       inputExerciseType: muscleForm.value,
-  //     },
-  //     refetchQueries: [{
-  //       query: GET_EXERCISE_TYPES,
-  //     }]
-  //   }).subscribe((result: MutationResult) => {
-  //     if (result.errors) {
-  //       this.alertService.graphQLErrorAlertHandler(result.errors);
-  //     } else {
-  //       let message: string = "ExerciseType " + result.data.modifyExerciseType.name + " has been successfully updated.";
-  //       this.alertService.addSuccessAlert(message);
-  //     }
-  //   });
-  // }
-
   modifyExerciseType(exerciseTypeForm: FormGroup) {
-    this.apollo.mutate({
+    this.apolloWrapperService.mutate({
       mutation: MOD_EXERCISE_TYPES,
       variables: {
         inputExerciseType: exerciseTypeForm.value,
       },
-      update: (cache: ApolloCache<{ getExerciseTypes: ExerciseType[] }>, {data}: MutationResult) => {
-        if (!data) return;
-        const updatedExerciseType: ExerciseType = data.modifyExerciseType;
-        const existingData: any = cache.readQuery({query: GET_EXERCISE_TYPES});
-        const updatedList: ExerciseType[] = existingData.getExerciseTypes.map((et: ExerciseType) =>
-          et.id === updatedExerciseType.id ? updatedExerciseType : et
-        );
-        cache.writeQuery({
-          query: GET_EXERCISE_TYPES,
-          data: {getExerciseTypes: updatedList}
-        });
-      }
     }).subscribe(({errors, data}: MutationResult) => {
       if (errors)
         this.alertService.graphQLErrorAlertHandler(errors);
-      else if (data)
-        this.alertService.addSuccessAlert(`ExerciseType ${data.modifyExerciseType.name} updated successfully.`);
-      else
-        this.alertService.addErrorAlert("data is null or undefined");
+      this.alertService.addSuccessAlert(`ExerciseType ${data.modifyExerciseType.name} has been successfully updated.`);
     });
   }
 
-  // deleteExerciseType(exerciseType: ExerciseType) {
-  //   this.apollo.mutate({
-  //     mutation: DEL_EXERCISE_TYPES,
-  //     variables: {
-  //       exerciseTypeId: exerciseType.id,
-  //     },
-  //     refetchQueries: [{
-  //       query: GET_EXERCISE_TYPES,
-  //     }]
-  //   }).subscribe((result: MutationResult) => {
-  //     if (result.errors) {
-  //       this.alertService.graphQLErrorAlertHandler(result.errors);
-  //     } else {
-  //       let message: string = "ExerciseType " + exerciseType.name + " has been successfully deleted.";
-  //       this.alertService.addSuccessAlert(message);
-  //     }
-  //   });
-  // }
-
   deleteExerciseType(exerciseType: ExerciseType) {
-    this.apollo.mutate({
+    this.apolloWrapperService.mutate({
       mutation: DEL_EXERCISE_TYPES,
       variables: {exerciseTypeId: exerciseType.id},
-      update: (cache: ApolloCache<{ getExerciseTypes: ExerciseType[] }>) => {
-        const existingData: any = cache.readQuery({query: GET_EXERCISE_TYPES});
-        const filteredData = existingData.getExerciseTypes.filter((et: ExerciseType) => et.id !== exerciseType.id);
-        cache.writeQuery({
-          query: GET_EXERCISE_TYPES,
-          data: {getExerciseTypes: filteredData}
-        });
-      }
     }).subscribe(({errors}: MutationResult) => {
       if (errors)
         this.alertService.graphQLErrorAlertHandler(errors);
-      else
-        this.alertService.addSuccessAlert(`ExerciseType ${exerciseType.name} deleted successfully.`);
+      this.alertService.addSuccessAlert(`ExerciseType ${exerciseType.name} has been successfully deleted.`);
     });
   }
 }
