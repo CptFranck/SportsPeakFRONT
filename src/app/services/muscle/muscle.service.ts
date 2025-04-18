67d841072b6c4def34c4da7c704f74d9a426f61d
CptFranck <francois.caillet71@gmail.com>
2025-04-18 14:35:54 +0200

    rework muscle service

--- src/app/services/muscle/muscle.service.ts
+++ src/app/services/muscle/muscle.service.ts
@@ -1,22 +1,23 @@
 import {inject, Injectable} from '@angular/core';
-import {Apollo, MutationResult} from "apollo-angular";
+import {MutationResult} from "apollo-angular";
 import {ADD_MUSCLE, DEL_MUSCLE, GET_MUSCLES, MOD_MUSCLE} from "../../graphql/operations/muscle.operations";
 import {FormGroup} from "@angular/forms";
 import {BehaviorSubject} from "rxjs";
 import {Muscle} from "../../interface/dto/muscle";
 import {AlertService} from "../alert/alert.service";
 import {ApolloQueryResult} from "@apollo/client";
+import {ApolloWrapperService} from "../apollo-wrapper/apollo-wrapper.service";
 
 @Injectable({
   providedIn: 'root'
 })
 export class MuscleService {
 
-  muscles: BehaviorSubject<Muscle[]> = new BehaviorSubject<Muscle[]>([]);
-  isLoading: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(true);
+  muscles = new BehaviorSubject<Muscle[]>([]);
+  isLoading = new BehaviorSubject<boolean>(true);
 
-  private readonly apollo: Apollo = inject(Apollo);
-  private readonly alertService: AlertService = inject(AlertService);
+  private readonly apolloWrapperService = inject(ApolloWrapperService);
+  private readonly alertService = inject(AlertService);
 
   constructor() {
     this.getMuscles();
@@ -24,72 +25,54 @@ export class MuscleService {
 
   getMuscles() {
     this.isLoading.next(true);
-    return this.apollo.watchQuery({
+    this.apolloWrapperService.watchQuery({
       query: GET_MUSCLES,
-    }).valueChanges.subscribe((result: ApolloQueryResult<any>): void => {
-      if (result.errors) {
-        this.alertService.graphQLErrorAlertHandler(result.errors);
-      }
-      this.muscles.next(result.data.getMuscles);
-      this.isLoading.next(result.loading);
+    }).valueChanges.subscribe(({data, errors, loading}: ApolloQueryResult<any>) => {
+      if (errors)
+        this.alertService.graphQLErrorAlertHandler(errors);
+      this.muscles.next(data.getMuscles);
+      this.isLoading.next(loading);
     });
   }
 
   addMuscle(muscleForm: FormGroup) {
-    return this.apollo.mutate({
+    this.apolloWrapperService.mutate({
       mutation: ADD_MUSCLE,
       variables: {
         inputNewMuscle: muscleForm.value,
-      },
-      refetchQueries: [{
-        query: GET_MUSCLES,
-      }]
-    }).subscribe(
-      (result: MutationResult): void => {
-        if (result.errors) {
-          this.alertService.graphQLErrorAlertHandler(result.errors);
-        } else {
-          let message: string = "Muscle " + result.data.addMuscle.name + " been successfully created.";
-          this.alertService.addSuccessAlert(message);
-        }
-      });
+      }
+    }).subscribe(({errors, data}: MutationResult) => {
+      if (errors)
+        this.alertService.graphQLErrorAlertHandler(errors);
+      const message: string = "Muscle " + data.addMuscle.name + " been successfully created.";
+      this.alertService.addSuccessAlert(message);
+    });
   }
 
   modifyMuscle(muscleForm: FormGroup) {
-    return this.apollo.mutate({
+    this.apolloWrapperService.mutate({
       mutation: MOD_MUSCLE,
       variables: {
         inputMuscle: muscleForm.value,
-      },
-      refetchQueries: [{
-        query: GET_MUSCLES,
-      }]
-    }).subscribe((result: MutationResult): void => {
-      if (result.errors) {
-        this.alertService.graphQLErrorAlertHandler(result.errors);
-      } else {
-        let message: string = "Muscle " + result.data.modifyMuscle.name + " been successfully updated.";
-        this.alertService.addSuccessAlert(message);
       }
+    }).subscribe(({errors, data}: MutationResult) => {
+      if (errors)
+        this.alertService.graphQLErrorAlertHandler(errors);
+      const message: string = "Muscle " + data.modifyMuscle.name + " been successfully updated.";
+      this.alertService.addSuccessAlert(message);
     });
   }
 
   deleteMuscle(muscle: Muscle) {
-    return this.apollo.mutate({
+    this.apolloWrapperService.mutate({
       mutation: DEL_MUSCLE,
       variables: {
         muscleId: muscle.id,
-      },
-      refetchQueries: [{
-        query: GET_MUSCLES,
-      }]
-    }).subscribe((result: MutationResult): void => {
-      if (result.errors) {
-        this.alertService.graphQLErrorAlertHandler(result.errors);
-      } else {
-        let message: string = "Muscle " + muscle.name + " has been successfully deleted.";
-        this.alertService.addSuccessAlert(message);
       }
+    }).subscribe(({errors}: MutationResult) => {
+      if (errors)
+        this.alertService.graphQLErrorAlertHandler(errors);
+      this.alertService.addSuccessAlert(`Muscle ${muscle.name} has been successfully deleted.`);
     });
   }
 }
