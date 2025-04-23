420169544832e9a89efdbd8d9005af1a985d0f6c
CptFranck <francois.caillet71@gmail.com>
2025-04-23 16:10:46 +0200

    update muscle card

--- src/app/components/card/muscle-card/muscle-card.component.ts
+++ src/app/components/card/muscle-card/muscle-card.component.ts
@@ -1,17 +1,17 @@
 import {Component, input} from '@angular/core';
 import {Muscle} from "../../../interface/dto/muscle";
-import {RouterLink} from "@angular/router";
 import {NgOptimizedImage} from "@angular/common";
+import {RouterLink} from "@angular/router";
 
 @Component({
   selector: 'app-muscle-card',
   imports: [
-    RouterLink,
     NgOptimizedImage,
+    RouterLink,
   ],
   templateUrl: './muscle-card.component.html',
   styleUrl: 'muscle-card.component.css'
 })
 export class MuscleCardComponent {
-  muscle = input.required<Muscle>();
+  readonly muscle = input.required<Muscle>();
 }
