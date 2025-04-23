742b9f439c56e5d7549fbf1d2a0f8dc0adcbe5be
CptFranck <francois.caillet71@gmail.com>
2025-04-23 13:38:52 +0200

    add muscle details route

--- src/app/page/docs/docs.routes.ts
+++ src/app/page/docs/docs.routes.ts
@@ -2,12 +2,14 @@ import {Routes} from "@angular/router";
 import {MusclesComponent} from "./muscles/muscles/muscles.component";
 import {ExerciseTypesComponent} from "./exercise-types/exercise-types/exercise-types.component";
 import {ExercisesComponent} from "./exercises/exercises/exercises.component";
+import {MuscleDetailsComponent} from "./muscles/muscle-details/muscle-details.component";
 
 export const DOCS_ROUTES: Routes = [
   {
     path: '',
     children: [
       {path: 'muscles', component: MusclesComponent},
+      {path: 'muscle-details/:id', component: MuscleDetailsComponent},
       {path: 'exercises', component: ExercisesComponent},
       {path: 'exercise-types', component: ExerciseTypesComponent},
     ],
