e16f358e607ca36c01c34900cdb50539ae85b020
CptFranck <francois.caillet71@gmail.com>
2025-05-30 12:12:37 +0200

    update CollapseBlockComponent component

--- src/app/components/collapse-block/collapse-block.component.ts
+++ src/app/components/collapse-block/collapse-block.component.ts
@@ -1,4 +1,4 @@
-import {Component, computed, ElementRef, input, signal, TemplateRef} from '@angular/core';
+import {Component, computed, ElementRef, input, signal, TemplateRef, viewChild} from '@angular/core';
 import {NgIf, NgTemplateOutlet} from "@angular/common";
 import {Subject} from "rxjs";
 import {ActionType} from "../../interface/enum/action-type";
@@ -21,41 +21,38 @@ export class CollapseBlockComponent {
   readonly actionType = input.required<ActionType | undefined>();
 
   readonly submitButton = computed<boolean>(() => {
-    const action = this.actionType();
-    return action === ActionType.update || action === ActionType.delete;
+    return this.actionType() === ActionType.update || this.actionType() === ActionType.delete;
   });
   readonly validateButtonClass = computed<string>(() => {
-    const action = this.actionType();
-    if (action == ActionType.delete)
-      return "btn-danger"
-    return "btn-success"
+    if (this.actionType() == ActionType.delete) return "btn-danger"
+    else return "btn-success"
   });
   readonly closeButtonTitle = computed<string>(() => {
-    const action = this.actionType();
-    if (action)
-      return "Cancel"
-    return "Close"
+    if (this.actionType()) return "Cancel"
+    else return "Close"
   });
   readonly validationButtonTitle = computed<string>(() => {
-    const action = this.actionType();
-    if (action == ActionType.create)
-      return "Create"
-    if (action == ActionType.update)
-      return "Update"
-    if (action == ActionType.delete)
-      return "Delete"
-    return "Ok"
+    switch (this.actionType()) {
+      case ActionType.create:
+        return 'Create';
+      case ActionType.update:
+        return 'Update';
+      case ActionType.delete:
+        return 'Delete';
+      default:
+        return 'Submit';
+    }
   });
+
+  readonly elementRef = viewChild.required<ElementRef>('collapseBlock');
 
   private lastButton: ElementRef | undefined;
 
-  constructor(private readonly elementRef: ElementRef) {
-  }
-
   toggle(elementRef: ElementRef) {
-    this.elementRef.nativeElement.scrollIntoView();
     if (this.hidden() || !this.hidden() && elementRef === this.lastButton)
       this.hidden.update(value => !value);
+    else
+      this.elementRef().nativeElement.scrollIntoView();
     this.lastButton = elementRef;
   }
 
