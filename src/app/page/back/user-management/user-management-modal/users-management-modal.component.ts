67f97ad93fd1664c38f7e6c6bf344c8da583ceed
CptFranck <francois.caillet71@gmail.com>
2025-05-30 12:34:23 +0200

    update UsersManagementModalComponent with if block

--- src/app/page/back/user-management/user-management-modal/users-management-modal.component.ts
+++ src/app/page/back/user-management/user-management-modal/users-management-modal.component.ts
@@ -6,7 +6,6 @@ import {
   UserRolesFormComponent
 } from "../../../../components/form/user-management/user-entity-form/user-roles-form.component";
 import {ModalComponent} from "../../../../components/modal/modal/modal.component";
-import {NgIf} from "@angular/common";
 import {User} from "../../../../interface/dto/user";
 import {ActionType} from "../../../../interface/enum/action-type";
 
@@ -14,7 +13,6 @@ import {ActionType} from "../../../../interface/enum/action-type";
   selector: 'app-user-modal',
   imports: [
     ModalComponent,
-    NgIf,
     UserDetailsDisplayComponent,
     UserRolesFormComponent
   ],
