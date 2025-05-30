c50f2d0fcf5932236017de33ff9afd0b2c3ef0c4
CptFranck <francois.caillet71@gmail.com>
2025-05-30 12:31:04 +0200

    update LoadingComponent with if block

--- src/app/components/loading/loading.component.ts
+++ src/app/components/loading/loading.component.ts
@@ -1,12 +1,8 @@
 import {Component, input} from '@angular/core';
-import {NgIf} from "@angular/common";
 import {collapseHeight} from "../../animation/collapseHeigh";
 
 @Component({
   selector: 'app-loading',
-  imports: [
-    NgIf
-  ],
   templateUrl: './loading.component.html',
   animations: [collapseHeight]
 })
