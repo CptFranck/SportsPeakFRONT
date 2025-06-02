b3ee433cdd343dd29231631a569408502721d8bd
CptFranck <francois.caillet71@gmail.com>
2025-06-02 18:16:48 +0200

    update PrivilegeDetailDisplayComponent with if and for blocks

--- src/app/components/modal-component/privilege/privilege-detail-display/privilege-detail-display.component.ts
+++ src/app/components/modal-component/privilege/privilege-detail-display/privilege-detail-display.component.ts
@@ -1,13 +1,8 @@
 import {Component, input} from '@angular/core';
-import {NgForOf, NgIf} from "@angular/common";
 import {Privilege} from "../../../../interface/dto/privilege";
 
 @Component({
   selector: 'app-privilege-details-display',
-  imports: [
-    NgForOf,
-    NgIf
-  ],
   templateUrl: './privilege-detail-display.component.html'
 })
 export class PrivilegeDetailDisplayComponent {
