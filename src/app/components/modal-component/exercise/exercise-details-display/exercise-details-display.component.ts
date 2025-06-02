dfdbe3c50c6cf08b497814dae637a113d47fdadd
CptFranck <francois.caillet71@gmail.com>
2025-06-02 17:03:14 +0200

    update ExerciseDetailsDisplayComponent with if and for blocks

--- src/app/components/modal-component/exercise/exercise-details-display/exercise-details-display.component.ts
+++ src/app/components/modal-component/exercise/exercise-details-display/exercise-details-display.component.ts
@@ -1,13 +1,8 @@
 import {Component, input} from '@angular/core';
-import {NgForOf, NgIf} from "@angular/common";
 import {Exercise} from "../../../../interface/dto/exercise";
 
 @Component({
   selector: 'app-exercise-details-display',
-  imports: [
-    NgForOf,
-    NgIf
-  ],
   templateUrl: './exercise-details-display.component.html'
 })
 export class ExerciseDetailsDisplayComponent {
