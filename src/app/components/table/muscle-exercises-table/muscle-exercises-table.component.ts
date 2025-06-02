fbd5a3ec329429466f7b0c2203c01b843a8ca454
CptFranck <francois.caillet71@gmail.com>
2025-06-02 19:03:24 +0200

    update MuscleExercisesTableComponent with for blocks

--- src/app/components/table/muscle-exercises-table/muscle-exercises-table.component.ts
+++ src/app/components/table/muscle-exercises-table/muscle-exercises-table.component.ts
@@ -1,12 +1,10 @@
 import {Component, input} from '@angular/core';
-import {NgForOf} from "@angular/common";
 import {RouterLink} from "@angular/router";
 import {Muscle} from "../../../interface/dto/muscle";
 
 @Component({
   selector: 'app-muscle-exercises-table',
   imports: [
-    NgForOf,
     RouterLink
   ],
   templateUrl: './muscle-exercises-table.component.html',
