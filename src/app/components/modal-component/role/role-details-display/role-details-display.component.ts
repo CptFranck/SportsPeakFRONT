f5ac4452b3dcf4ea180eb6374a54f3919be37e22
CptFranck <francois.caillet71@gmail.com>
2025-06-02 18:38:59 +0200

    update RoleDetailsDisplayComponent with if and for blocks

--- src/app/components/modal-component/role/role-details-display/role-details-display.component.ts
+++ src/app/components/modal-component/role/role-details-display/role-details-display.component.ts
@@ -1,13 +1,8 @@
 import {Component, input} from '@angular/core';
-import {NgForOf, NgIf} from "@angular/common";
 import {Role} from "../../../../interface/dto/role";
 
 @Component({
   selector: 'app-role-details-display',
-  imports: [
-    NgForOf,
-    NgIf
-  ],
   templateUrl: './role-details-display.component.html'
 })
 export class RoleDetailsDisplayComponent {
