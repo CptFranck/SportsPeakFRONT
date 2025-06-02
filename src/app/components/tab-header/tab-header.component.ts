e3c607d2a9f5e49d9a2e0cfb5260d6512f05bae9
CptFranck <francois.caillet71@gmail.com>
2025-06-02 19:04:20 +0200

    update TabHeaderComponent with for blocks

--- src/app/components/tab-header/tab-header.component.ts
+++ src/app/components/tab-header/tab-header.component.ts
@@ -1,13 +1,9 @@
 import {Component, input} from '@angular/core';
-import {NgForOf} from "@angular/common";
 import {TabOption} from "../../interface/components/tab/tabOption";
 
 @Component({
-    selector: 'app-tab-header',
-    imports: [
-        NgForOf
-    ],
-    templateUrl: './tab-header.component.html'
+  selector: 'app-tab-header',
+  templateUrl: './tab-header.component.html'
 })
 export class TabHeaderComponent {
   readonly tabId = input.required<string>();
