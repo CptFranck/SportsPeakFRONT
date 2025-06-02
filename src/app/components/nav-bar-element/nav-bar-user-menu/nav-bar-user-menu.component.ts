4ec796e215d81143a53b5954b68333928f2a8606
CptFranck <francois.caillet71@gmail.com>
2025-06-02 18:35:51 +0200

    update NavBarUserMenuComponent with if and for blocks

--- src/app/components/nav-bar-element/nav-bar-user-menu/nav-bar-user-menu.component.ts
+++ src/app/components/nav-bar-element/nav-bar-user-menu/nav-bar-user-menu.component.ts
@@ -1,23 +1,20 @@
-import {Component, inject, OnDestroy, OnInit} from '@angular/core';
+import {Component, inject, OnDestroy, OnInit, signal} from '@angular/core';
 import {User} from "../../../interface/dto/user";
 import {UserLoggedService} from "../../../services/user-logged/user-logged.service";
-import {NgForOf, NgIf} from "@angular/common";
 import {RouterLink, RouterLinkActive} from "@angular/router";
 import {Subject, takeUntil} from "rxjs";
 
 @Component({
-    selector: 'app-nav-bar-user-menu',
-    imports: [
-        NgForOf,
-        NgIf,
-        RouterLink,
-        RouterLinkActive
-    ],
-    templateUrl: './nav-bar-user-menu.component.html'
+  selector: 'app-nav-bar-user-menu',
+  imports: [
+    RouterLink,
+    RouterLinkActive
+  ],
+  templateUrl: './nav-bar-user-menu.component.html'
 })
 export class NavBarUserMenuComponent implements OnInit, OnDestroy {
-  user: User | undefined = undefined;
-  isAdmin: boolean | undefined;
+  user = signal<User | undefined>(undefined);
+  isAdmin = signal<boolean | undefined>(undefined);
   navbarDropdownId: string = "NavBarUserMenu"
 
   private readonly unsubscribe$: Subject<void> = new Subject<void>();
@@ -27,8 +24,8 @@ export class NavBarUserMenuComponent implements OnInit, OnDestroy {
     this.userService.currentUser
       .pipe(takeUntil(this.unsubscribe$))
       .subscribe((user: User | undefined) => {
-        this.user = user;
-        this.isAdmin = this.userService.isAdmin();
+        this.user.set(user);
+        this.isAdmin.set(this.userService.isAdmin());
       })
   }
 
