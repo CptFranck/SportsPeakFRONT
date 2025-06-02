e31f0235d1ce00799857c37c3535e0f643fe5a3c
CptFranck <francois.caillet71@gmail.com>
2025-06-02 19:01:20 +0200

    update RolesArrayComponent with for blocks

--- src/app/page/back/role/roles-array/roles-array.component.ts
+++ src/app/page/back/role/roles-array/roles-array.component.ts
@@ -1,5 +1,4 @@
 import {Component, input, output} from '@angular/core';
-import {NgForOf} from "@angular/common";
 import {ModalButtonComponent} from "../../../../components/modal/modal-button/modal-button.component";
 import {Role} from "../../../../interface/dto/role";
 import {FormIndicator} from "../../../../interface/utils/form-indicator";
@@ -10,7 +9,6 @@ import {ActionType} from "../../../../interface/enum/action-type";
   selector: 'app-roles-array',
   imports: [
     ModalButtonComponent,
-    NgForOf
   ],
   templateUrl: './roles-array.component.html'
 })
