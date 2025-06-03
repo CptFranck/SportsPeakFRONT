b672da6caea0adaec9a83fd56d4e231a7172e224
CptFranck <francois.caillet71@gmail.com>
2025-06-03 17:21:27 +0200

    add fileWeightValidator to validators functions

--- src/app/validators/confirmValidator.ts
+++ src/app/validators/confirmValidator.ts
@@ -30,3 +30,18 @@ export function fileTypeValidator(type: string): ValidatorFn {
     return null;
   };
 }
+
+export function fileWeightValidator(maxSizeInByte: number, maxSizeValueAndUnit: string): ValidatorFn {
+  return (abstractControl: AbstractControl): ValidationErrors | null => {
+    const file: File | null = abstractControl.value;
+    if (file) {
+      const fileSizeInByte = file.size;
+      if (fileSizeInByte > maxSizeInByte) {
+        const error: ValidationErrors = {maxFileSizeSupported: `file size is larger than ${maxSizeValueAndUnit}`}
+        abstractControl.setErrors(error);
+        return error;
+      }
+    }
+    return null;
+  };
+}
