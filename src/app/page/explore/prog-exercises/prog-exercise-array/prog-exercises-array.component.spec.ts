b750d926cd927f9e70241be7dafc6bae952dc0af
CptFranck <francois.caillet71@gmail.com>
2025-06-04 19:24:42 +0200

    fix ProgExercisesArrayComponent test

--- src/app/page/explore/prog-exercises/prog-exercise-array/prog-exercises-array.component.spec.ts
+++ src/app/page/explore/prog-exercises/prog-exercise-array/prog-exercises-array.component.spec.ts
@@ -1,8 +1,11 @@
 import {ComponentFixture, TestBed} from '@angular/core/testing';
 
 import {ProgExercisesArrayComponent} from './prog-exercises-array.component';
+import {User} from "../../../../shared/model/dto/user";
+import {generateTestExercise, generateTestUser} from "../../../../utils/testFunctions";
+import {Exercise} from "../../../../shared/model/dto/exercise";
 
-describe('ProgExerciseArrayComponent', () => {
+describe('ProgExercisesArrayComponent', () => {
   let component: ProgExercisesArrayComponent;
   let fixture: ComponentFixture<ProgExercisesArrayComponent>;
 
@@ -14,6 +17,12 @@ describe('ProgExerciseArrayComponent', () => {
 
     fixture = TestBed.createComponent(ProgExercisesArrayComponent);
     component = fixture.componentInstance;
+
+    const userMock: User = generateTestUser();
+    const exerciseMock: Exercise = generateTestExercise();
+    fixture.componentRef.setInput('modalId', "Id");
+    fixture.componentRef.setInput('progExercises', [])
+
     fixture.detectChanges();
   });
 
