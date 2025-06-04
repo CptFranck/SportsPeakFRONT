b6cc2fca4da8571523e7188a74a82e97bcd330fd
CptFranck <francois.caillet71@gmail.com>
2025-06-04 14:44:18 +0200

    update environment variable

--- src/app/environment/environment.ts
+++ src/app/environment/environment.ts
@@ -1,5 +1,7 @@
 export const environment = {
   production: false,
-  apiBaseUrl: 'https://localhost:8443',
-  graphqlEndPoint: 'https://localhost:8443/service/api/graphql',
+  apiHttpBaseUrl: 'https://localhost:8443',
+  graphqlEndPoint: '/service/api/graphql',
+  restEndPoint: '/service/api/rest',
+  illustrationPath: '/illustrations',
 };
