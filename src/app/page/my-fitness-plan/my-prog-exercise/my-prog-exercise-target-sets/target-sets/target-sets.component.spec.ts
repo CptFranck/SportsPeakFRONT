0112305ea022482ba2cd0dbe93d4568084d464d9
CptFranck <francois.caillet71@gmail.com>
2025-06-04 19:15:39 +0200

    fix TargetSetsComponent test

--- src/app/page/my-fitness-plan/my-prog-exercise/my-prog-exercise-target-sets/target-sets/target-sets.component.spec.ts
+++ src/app/page/my-fitness-plan/my-prog-exercise/my-prog-exercise-target-sets/target-sets/target-sets.component.spec.ts
@@ -1,8 +1,11 @@
 import {ComponentFixture, TestBed} from '@angular/core/testing';
 
 import {TargetSetsComponent} from './target-sets.component';
+import {User} from "../../../../../shared/model/dto/user";
+import {generateTestExercise, generateTestProgExercise, generateTestUser} from "../../../../../utils/testFunctions";
+import {Exercise} from "../../../../../shared/model/dto/exercise";
 
-describe('TargetSetComponent', () => {
+describe('TargetSetsComponent', () => {
   let component: TargetSetsComponent;
   let fixture: ComponentFixture<TargetSetsComponent>;
 
@@ -14,6 +17,13 @@ describe('TargetSetComponent', () => {
 
     fixture = TestBed.createComponent(TargetSetsComponent);
     component = fixture.componentInstance;
+
+    let mockUser: User = generateTestUser();
+    let mockExercise: Exercise = generateTestExercise();
+    fixture.componentRef.setInput('progExercise', generateTestProgExercise(mockUser, mockExercise))
+    fixture.componentRef.setInput('targetSetModalId', "Title");
+    fixture.componentRef.setInput('performanceLogModalId', "Id");
+
     fixture.detectChanges();
   });
 
