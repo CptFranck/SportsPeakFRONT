243e1fdbbcdcc00e84bd7a0d4504e8e3bb0c952e
CptFranck <francois.caillet71@gmail.com>
2025-06-04 17:32:09 +0200

    fix AlertDisplayComponent test

--- src/app/components/alert-display/alert-display.component.spec.ts
+++ src/app/components/alert-display/alert-display.component.spec.ts
@@ -1,13 +1,15 @@
 import {ComponentFixture, TestBed} from '@angular/core/testing';
 
 import {AlertDisplayComponent} from './alert-display.component';
+import {provideAnimations} from "@angular/platform-browser/animations";
 
-describe('CrudAlertComponent', () => {
+describe('AlertDisplayComponent', () => {
   let component: AlertDisplayComponent;
   let fixture: ComponentFixture<AlertDisplayComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      providers: [provideAnimations(),],
       imports: [AlertDisplayComponent]
     })
       .compileComponents();
