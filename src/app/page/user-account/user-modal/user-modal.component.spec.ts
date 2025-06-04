839473229aadd4fde0bf36e371735cf3242e5dc2
CptFranck <francois.caillet71@gmail.com>
2025-06-04 19:22:50 +0200

    fix UserModalComponent test

--- src/app/page/user-account/user-modal/user-modal.component.spec.ts
+++ src/app/page/user-account/user-modal/user-modal.component.spec.ts
@@ -1,6 +1,8 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { UserModalComponent } from './user-modal.component';
+import {UserModalComponent} from './user-modal.component';
+import {ActionEnum} from "../../../shared/model/enum/action.enum";
+import {ModificationFieldEnum} from "../../../shared/model/enum/user-modification-field.enum";
 
 describe('UserModalComponent', () => {
   let component: UserModalComponent;
@@ -10,10 +12,17 @@ describe('UserModalComponent', () => {
     await TestBed.configureTestingModule({
       imports: [UserModalComponent]
     })
-    .compileComponents();
-    
+      .compileComponents();
+
     fixture = TestBed.createComponent(UserModalComponent);
     component = fixture.componentInstance;
+
+    fixture.componentRef.setInput('modalTitle', "title");
+    fixture.componentRef.setInput('userModalId', "Id");
+    fixture.componentRef.setInput('user', undefined);
+    fixture.componentRef.setInput('action', ActionEnum.read);
+    fixture.componentRef.setInput('modification', ModificationFieldEnum.email);
+
     fixture.detectChanges();
   });
 
