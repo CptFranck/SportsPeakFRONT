c334372bae1d21ba2048ebcaddc64944c4316d42
CptFranck <francois.caillet71@gmail.com>
2025-06-04 19:35:56 +0200

    fix TargetSetModalComponent test

--- src/app/page/my-fitness-plan/my-prog-exercise/my-prog-exercise-target-sets/target-set-modal/target-set-modal.component.spec.ts
+++ src/app/page/my-fitness-plan/my-prog-exercise/my-prog-exercise-target-sets/target-set-modal/target-set-modal.component.spec.ts
@@ -1,6 +1,7 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { TargetSetModalComponent } from './target-set-modal.component';
+import {TargetSetModalComponent} from './target-set-modal.component';
+import {ActionEnum} from "../../../../../shared/model/enum/action.enum";
 
 describe('TargetSetModalComponent', () => {
   let component: TargetSetModalComponent;
@@ -10,10 +11,18 @@ describe('TargetSetModalComponent', () => {
     await TestBed.configureTestingModule({
       imports: [TargetSetModalComponent]
     })
-    .compileComponents();
-    
+      .compileComponents();
+
     fixture = TestBed.createComponent(TargetSetModalComponent);
     component = fixture.componentInstance;
+
+    fixture.componentRef.setInput('targetSetModalId', 'Id');
+    fixture.componentRef.setInput('targetSetModalTitle', 'title');
+    fixture.componentRef.setInput('targetSet', undefined);
+    fixture.componentRef.setInput('progExercise', undefined);
+    fixture.componentRef.setInput('performanceLog', undefined);
+    fixture.componentRef.setInput('action', ActionEnum.read);
+
     fixture.detectChanges();
   });
 
