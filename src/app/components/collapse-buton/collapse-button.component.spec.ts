9e4518e1e2ea8ab83b59fcfd4f378d1b03aa3bd4
CptFranck <francois.caillet71@gmail.com>
2025-06-04 19:16:19 +0200

    fix CollapseButtonComponent test

--- src/app/components/collapse-buton/collapse-button.component.spec.ts
+++ src/app/components/collapse-buton/collapse-button.component.spec.ts
@@ -2,7 +2,7 @@ import {ComponentFixture, TestBed} from '@angular/core/testing';
 
 import {CollapseButtonComponent} from './collapse-button.component';
 
-describe('CollapseButonComponent', () => {
+describe('CollapseButtonComponent', () => {
   let component: CollapseButtonComponent;
   let fixture: ComponentFixture<CollapseButtonComponent>;
 
@@ -14,6 +14,11 @@ describe('CollapseButonComponent', () => {
 
     fixture = TestBed.createComponent(CollapseButtonComponent);
     component = fixture.componentInstance;
+
+    fixture.componentRef.setInput('value', true)
+    fixture.componentRef.setInput('btnClass', "btnClass");
+    fixture.componentRef.setInput('collapseId', "Id");
+
     fixture.detectChanges();
   });
 
