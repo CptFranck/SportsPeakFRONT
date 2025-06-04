5a48baa2c67c66627919c30ae08bec36ae198a6d
CptFranck <francois.caillet71@gmail.com>
2025-06-04 18:13:17 +0200

    fix MultiSelectComponent test

--- src/app/components/multi-select/multi-select.component.spec.ts
+++ src/app/components/multi-select/multi-select.component.spec.ts
@@ -1,6 +1,7 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { MultiSelectComponent } from './multi-select.component';
+import {MultiSelectComponent} from './multi-select.component';
+import {provideAnimations} from "@angular/platform-browser/animations";
 
 describe('MultiSelectComponent', () => {
   let component: MultiSelectComponent;
@@ -8,12 +9,18 @@ describe('MultiSelectComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      providers: [provideAnimations()],
       imports: [MultiSelectComponent]
     })
-    .compileComponents();
-    
+      .compileComponents();
+
     fixture = TestBed.createComponent(MultiSelectComponent);
     component = fixture.componentInstance;
+
+    fixture.componentRef.setInput('selectedOptions', [])
+    fixture.componentRef.setInput('optionList', [])
+    fixture.componentRef.setInput('isLoading', false);
+
     fixture.detectChanges();
   });
 
