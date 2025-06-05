aca70b018f6e0142583982604ae7e5ba90e4225c
CptFranck <francois.caillet71@gmail.com>
2025-06-05 17:33:19 +0200

    fix MuscleCardComponent test

--- src/app/components/card/muscle-card/muscle-card.component.spec.ts
+++ src/app/components/card/muscle-card/muscle-card.component.spec.ts
@@ -1,19 +1,38 @@
 import {ComponentFixture, TestBed} from '@angular/core/testing';
 
 import {MuscleCardComponent} from './muscle-card.component';
+import {MuscleService} from "../../../core/services/muscle/muscle.service";
+import {generateTestMuscle} from "../../../utils/testFunctions";
+import {IllustrationService} from "../../../core/services/illustration/illustration.service";
+import {provideRouter} from "@angular/router";
 
-describe('MuscleComponent', () => {
+describe('MuscleCardComponent', () => {
   let component: MuscleCardComponent;
   let fixture: ComponentFixture<MuscleCardComponent>;
 
+  let mockMuscleService: jasmine.SpyObj<MuscleService> =
+    jasmine.createSpyObj('MuscleService', ['allMuscle', 'loading']);
+
+  let mockIllustrationService: jasmine.SpyObj<IllustrationService> =
+    jasmine.createSpyObj('IllustrationService', ['getImageUrl']);
+  mockIllustrationService.getImageUrl.and.returnValues("");
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [MuscleCardComponent]
+      imports: [MuscleCardComponent],
+      providers: [
+        provideRouter([]),
+        {provide: MuscleService, useValue: mockMuscleService},
+        {provide: IllustrationService, useValue: mockIllustrationService},
+      ]
     })
       .compileComponents();
 
     fixture = TestBed.createComponent(MuscleCardComponent);
     component = fixture.componentInstance;
+
+    fixture.componentRef.setInput('muscle', generateTestMuscle());
+
     fixture.detectChanges();
   });
 
