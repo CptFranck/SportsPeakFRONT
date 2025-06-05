85a1ede9764a3b97d043b51ecb2bc6e0dbd3663a
CptFranck <francois.caillet71@gmail.com>
2025-06-05 11:59:46 +0200

    fix PrivilegeModalComponent test

--- src/app/page/back/privilege/privilege-modal/privilege-modal.component.spec.ts
+++ src/app/page/back/privilege/privilege-modal/privilege-modal.component.spec.ts
@@ -1,6 +1,7 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { PrivilegeModalComponent } from './privilege-modal.component';
+import {PrivilegeModalComponent} from './privilege-modal.component';
+import {ActionEnum} from "../../../../shared/model/enum/action.enum";
 
 describe('PrivilegeModalComponent', () => {
   let component: PrivilegeModalComponent;
@@ -10,10 +11,16 @@ describe('PrivilegeModalComponent', () => {
     await TestBed.configureTestingModule({
       imports: [PrivilegeModalComponent]
     })
-    .compileComponents();
-    
+      .compileComponents();
+
     fixture = TestBed.createComponent(PrivilegeModalComponent);
     component = fixture.componentInstance;
+
+    fixture.componentRef.setInput('modalTitle', "title");
+    fixture.componentRef.setInput('privilegeModalId', "Id");
+    fixture.componentRef.setInput('privilege', undefined);
+    fixture.componentRef.setInput('action', ActionEnum.read);
+
     fixture.detectChanges();
   });
 
