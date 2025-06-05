12e7091b06a4814a89be8e450a1469d9fd5b696c
CptFranck <francois.caillet71@gmail.com>
2025-06-05 17:30:40 +0200

    fix UserUsernameFormComponent test

--- src/app/components/form/user/user-username-form/user-username-form.component.spec.ts
+++ src/app/components/form/user/user-username-form/user-username-form.component.spec.ts
@@ -2,12 +2,14 @@ import {ComponentFixture, TestBed} from '@angular/core/testing';
 
 import {UserUsernameFormComponent} from './user-username-form.component';
 import {UserService} from "../../../../core/services/user/user.service";
+import {ModificationFieldEnum} from "../../../../shared/model/enum/user-modification-field.enum";
 
 describe('UserUsernameFormComponent', () => {
   let component: UserUsernameFormComponent;
   let fixture: ComponentFixture<UserUsernameFormComponent>;
 
-  let mockUserService: jasmine.SpyObj<UserService>;
+  let mockUserService: jasmine.SpyObj<UserService> =
+    jasmine.createSpyObj('UserService', ['modifyUserUsername']);
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -20,6 +22,11 @@ describe('UserUsernameFormComponent', () => {
 
     fixture = TestBed.createComponent(UserUsernameFormComponent);
     component = fixture.componentInstance;
+
+    fixture.componentRef.setInput('user', undefined);
+    fixture.componentRef.setInput('submitEventActionType$', undefined);
+    fixture.componentRef.setInput('modification', ModificationFieldEnum.username);
+
     fixture.detectChanges();
   });
 
