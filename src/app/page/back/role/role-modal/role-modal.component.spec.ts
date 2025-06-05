80f451872c77acb05aca07b66f095781c7c76023
CptFranck <francois.caillet71@gmail.com>
2025-06-05 12:21:34 +0200

    fix RoleModalComponent test

--- src/app/page/back/role/role-modal/role-modal.component.spec.ts
+++ src/app/page/back/role/role-modal/role-modal.component.spec.ts
@@ -1,19 +1,43 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { RoleModalComponent } from './role-modal.component';
+import {RoleModalComponent} from './role-modal.component';
+import {ActionEnum} from "../../../../shared/model/enum/action.enum";
+import {RoleService} from "../../../../core/services/role/role.service";
+import {PrivilegeService} from "../../../../core/services/privilege/privilege.service";
+import {BehaviorSubject} from "rxjs";
+import {Privilege} from "../../../../shared/model/dto/privilege";
+import {provideAnimations} from "@angular/platform-browser/animations";
 
 describe('RoleModalComponent', () => {
   let component: RoleModalComponent;
   let fixture: ComponentFixture<RoleModalComponent>;
 
+  let mockRoleService: jasmine.SpyObj<RoleService> =
+    jasmine.createSpyObj('RoleService', ['addRole', 'modifyRole']);
+  let mockPrivilegeService: jasmine.SpyObj<PrivilegeService> =
+    jasmine.createSpyObj('UserLoggedService', ['currentUser']);
+  mockPrivilegeService.privileges = new BehaviorSubject<Privilege[]>([]);
+  mockPrivilegeService.isLoading = new BehaviorSubject<boolean>(true);
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      providers: [
+        provideAnimations(),
+        {provide: RoleService, useValue: mockRoleService},
+        {provide: PrivilegeService, useValue: mockPrivilegeService},
+      ],
       imports: [RoleModalComponent]
     })
-    .compileComponents();
-    
+      .compileComponents();
+
     fixture = TestBed.createComponent(RoleModalComponent);
     component = fixture.componentInstance;
+
+    fixture.componentRef.setInput('modalTitle', "Title");
+    fixture.componentRef.setInput('roleModalId', "Id");
+    fixture.componentRef.setInput('action', ActionEnum.create);
+    fixture.componentRef.setInput('role', undefined)
+
     fixture.detectChanges();
   });
 
