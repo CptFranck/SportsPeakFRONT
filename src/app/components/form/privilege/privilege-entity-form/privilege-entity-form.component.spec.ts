6763813cf87ad6a92e6d47bdc2b6d996cde51cdd
CptFranck <francois.caillet71@gmail.com>
2025-06-05 16:55:57 +0200

    fix PrivilegeEntityFormComponent test

--- src/app/components/form/privilege/privilege-entity-form/privilege-entity-form.component.spec.ts
+++ src/app/components/form/privilege/privilege-entity-form/privilege-entity-form.component.spec.ts
@@ -7,25 +7,28 @@ import {UserLoggedService} from "../../../../core/services/user-logged/user-logg
 import {User} from "../../../../shared/model/dto/user";
 import {RoleService} from "../../../../core/services/role/role.service";
 import {Role} from "../../../../shared/model/dto/role";
+import {provideAnimations} from "@angular/platform-browser/animations";
 
 describe('PrivilegeEntityFormComponent', () => {
   let component: PrivilegeEntityFormComponent;
   let fixture: ComponentFixture<PrivilegeEntityFormComponent>;
 
-  let mockPrivilegeService: jasmine.SpyObj<PrivilegeService>;
+  let mockPrivilegeService: jasmine.SpyObj<PrivilegeService> =
+    jasmine.createSpyObj('PrivilegeService', ['addPrivilege', 'modifyPrivilege']);
 
   let mockRoleService: jasmine.SpyObj<RoleService> =
-    jasmine.createSpyObj('RoleService', ['roles', 'isLoading']);
+    jasmine.createSpyObj('RoleService', ['']);
   mockRoleService.roles = new BehaviorSubject<Role[]>([]);
   mockRoleService.isLoading = new BehaviorSubject<boolean>(true);
 
   let mockUserLoggedService: jasmine.SpyObj<UserLoggedService> =
-    jasmine.createSpyObj('PrivilegeService', ['currentUser']);
+    jasmine.createSpyObj('PrivilegeService', ['']);
   mockUserLoggedService.currentUser = new BehaviorSubject<User | undefined>(undefined);
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       providers: [
+        provideAnimations(),
         {provide: RoleService, useValue: mockRoleService},
         {provide: PrivilegeService, useValue: mockPrivilegeService},
         {provide: UserLoggedService, useValue: mockUserLoggedService},
@@ -36,6 +39,10 @@ describe('PrivilegeEntityFormComponent', () => {
 
     fixture = TestBed.createComponent(PrivilegeEntityFormComponent);
     component = fixture.componentInstance;
+
+    fixture.componentRef.setInput('privilege', undefined);
+    fixture.componentRef.setInput('submitEventActionType$', undefined);
+
     fixture.detectChanges();
   });
 
