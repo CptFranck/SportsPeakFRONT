341bb06d1bda900b43804a826f1cff1b92581923
CptFranck <francois.caillet71@gmail.com>
2025-06-05 11:56:26 +0200

    fix ModalComponent test

--- src/app/components/modal/modal/modal.component.spec.ts
+++ src/app/components/modal/modal/modal.component.spec.ts
@@ -1,5 +1,6 @@
 import {ComponentFixture, TestBed} from '@angular/core/testing';
 import {ModalComponent} from './modal.component';
+import {ActionEnum} from "../../../shared/model/enum/action.enum";
 
 describe('ModalComponent', () => {
   let component: ModalComponent;
@@ -12,6 +13,11 @@ describe('ModalComponent', () => {
 
     fixture = TestBed.createComponent(ModalComponent);
     component = fixture.componentInstance;
+
+    fixture.componentRef.setInput('title', "title");
+    fixture.componentRef.setInput('modalId', "Id");
+    fixture.componentRef.setInput('actionType', ActionEnum.read);
+
     fixture.detectChanges();
   });
 
