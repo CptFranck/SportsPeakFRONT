82d0caefb8513a8c65e6122f72af7135f1b14df2
CptFranck <francois.caillet71@gmail.com>
2025-06-05 17:50:28 +0200

    fix UserRolesFormComponent test

--- src/app/components/form/user-management/user-entity-form/user-roles-form.component.spec.ts
+++ src/app/components/form/user-management/user-entity-form/user-roles-form.component.spec.ts
@@ -1,16 +1,19 @@
 import {ComponentFixture, TestBed} from '@angular/core/testing';
 
 import {UserRolesFormComponent} from './user-roles-form.component';
-import {UserService} from "../../../../core/services/user/user.service";
 import {BehaviorSubject} from "rxjs";
 import {RoleService} from "../../../../core/services/role/role.service";
 import {Role} from "../../../../shared/model/dto/role";
+import {UserService} from "../../../../core/services/user/user.service";
+import {provideAnimations} from "@angular/platform-browser/animations";
 
 describe('UserRolesFormComponent', () => {
   let component: UserRolesFormComponent;
   let fixture: ComponentFixture<UserRolesFormComponent>;
 
-  let mockUserService: jasmine.SpyObj<UserService>;
+  let mockUserService: jasmine.SpyObj<UserService> =
+    jasmine.createSpyObj('UserService', ['modifyUserRoles']);
+
 
   let mockRoleService: jasmine.SpyObj<RoleService> =
     jasmine.createSpyObj('RoleService', ['roles', 'isLoading']);
@@ -20,6 +23,7 @@ describe('UserRolesFormComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       providers: [
+        provideAnimations(),
         {provide: UserService, useValue: mockUserService},
         {provide: RoleService, useValue: mockRoleService},
       ],
@@ -29,6 +33,10 @@ describe('UserRolesFormComponent', () => {
 
     fixture = TestBed.createComponent(UserRolesFormComponent);
     component = fixture.componentInstance;
+
+    fixture.componentRef.setInput('user', undefined);
+    fixture.componentRef.setInput('submitEvents', undefined);
+
     fixture.detectChanges();
   });
 
