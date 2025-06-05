4e1b490f9e83c32b6375b6f9e2e5431fb9523055
CptFranck <francois.caillet71@gmail.com>
2025-06-05 17:36:29 +0200

    fix ExerciseEntityFormComponent test

--- src/app/components/form/exercise/exercise-entity-form/exercise-entity-form.component.spec.ts
+++ src/app/components/form/exercise/exercise-entity-form/exercise-entity-form.component.spec.ts
@@ -8,10 +8,17 @@ import {Muscle} from "../../../../shared/model/dto/muscle";
 import {ExerciseTypeService} from "../../../../core/services/exercise-type/exercise-type.service";
 import {ExerciseType} from "../../../../shared/model/dto/exercise-type";
 import {provideAnimations} from "@angular/platform-browser/animations";
+import {ProgExerciseService} from "../../../../core/services/prog-exercise/prog-exercise.service";
+import {ProgExercise} from "../../../../shared/model/dto/prog-exercise";
 
 describe('ExerciseEntityFormComponent', () => {
   let component: ExerciseEntityFormComponent;
   let fixture: ComponentFixture<ExerciseEntityFormComponent>;
+
+  let mockProgExerciseService: jasmine.SpyObj<ProgExerciseService> =
+    jasmine.createSpyObj('ProgExerciseService', ['']);
+  mockProgExerciseService.progExercise = new BehaviorSubject<ProgExercise | undefined>(undefined);
+  mockProgExerciseService.isLoading = new BehaviorSubject<boolean>(true);
 
   let mockMuscleService: jasmine.SpyObj<MuscleService> =
     jasmine.createSpyObj('MuscleService', ['allMuscle', 'loading']);
@@ -22,7 +29,7 @@ describe('ExerciseEntityFormComponent', () => {
     jasmine.createSpyObj('ExerciseService', ['addExercise', 'modifyExercise']);
 
   let mockExerciseTypeService: jasmine.SpyObj<ExerciseTypeService> =
-    jasmine.createSpyObj('ExerciseTypeService', ['exerciseTypes', 'isLoading']);
+    jasmine.createSpyObj('ExerciseTypeService', ['']);
   mockExerciseTypeService.exerciseTypes = new BehaviorSubject<ExerciseType[]>([]);
   mockExerciseTypeService.isLoading = new BehaviorSubject<boolean>(true);
 
@@ -33,6 +40,7 @@ describe('ExerciseEntityFormComponent', () => {
         {provide: MuscleService, useValue: mockMuscleService},
         {provide: ExerciseService, useValue: mockExerciseService},
         {provide: ExerciseTypeService, useValue: mockExerciseTypeService},
+        {provide: ProgExerciseService, useValue: mockProgExerciseService},
       ],
       imports: [ExerciseEntityFormComponent]
     })
