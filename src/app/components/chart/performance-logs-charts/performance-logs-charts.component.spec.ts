ac104f49dc94a933e94f4f090ae37f5ee06d1bb6
CptFranck <francois.caillet71@gmail.com>
2025-06-05 18:14:17 +0200

    fix PerformanceLogsChartsComponent test

--- src/app/components/chart/performance-logs-charts/performance-logs-charts.component.spec.ts
+++ src/app/components/chart/performance-logs-charts/performance-logs-charts.component.spec.ts
@@ -1,6 +1,7 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { PerformanceLogsChartsComponent } from './performance-logs-charts.component';
+import {PerformanceLogsChartsComponent} from './performance-logs-charts.component';
+import {CollapseBlockComponent} from "../../collapse-block/collapse-block.component";
 
 describe('PerformanceLogsChartsComponent', () => {
   let component: PerformanceLogsChartsComponent;
@@ -10,11 +11,17 @@ describe('PerformanceLogsChartsComponent', () => {
     await TestBed.configureTestingModule({
       imports: [PerformanceLogsChartsComponent]
     })
-    .compileComponents();
-    
+      .compileComponents();
+
     fixture = TestBed.createComponent(PerformanceLogsChartsComponent);
     component = fixture.componentInstance;
+
+    fixture.componentRef.setInput('collapseBlock', CollapseBlockComponent);
+    fixture.componentRef.setInput('accordionParentIdGraph', "Id");
+    fixture.componentRef.setInput('performanceLogsInputSortedBySet', []);
+
     fixture.detectChanges();
+
   });
 
   it('should create', () => {
