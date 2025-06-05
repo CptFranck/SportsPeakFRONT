bf684dc5d89374d086a7cec88627ba7d07d506b2
CptFranck <francois.caillet71@gmail.com>
2025-06-05 12:17:28 +0200

    fix RoleEntityFormComponent test

--- src/app/components/form/role/role-entity-form/role-entity-form.component.spec.ts
+++ src/app/components/form/role/role-entity-form/role-entity-form.component.spec.ts
@@ -4,15 +4,17 @@ import {RoleEntityFormComponent} from './role-entity-form.component';
 import {BehaviorSubject} from "rxjs";
 import {UserLoggedService} from "../../../../core/services/user-logged/user-logged.service";
 import {User} from "../../../../shared/model/dto/user";
-import {RoleService} from "../../../../core/services/role/role.service";
 import {PrivilegeService} from "../../../../core/services/privilege/privilege.service";
 import {Privilege} from "../../../../shared/model/dto/privilege";
+import {RoleService} from "../../../../core/services/role/role.service";
+import {provideAnimations} from "@angular/platform-browser/animations";
 
 describe('RoleEntityFormComponent', () => {
   let component: RoleEntityFormComponent;
   let fixture: ComponentFixture<RoleEntityFormComponent>;
 
-  let mockRoleService: jasmine.SpyObj<RoleService>;
+  let mockRoleService: jasmine.SpyObj<RoleService> =
+    jasmine.createSpyObj('RoleService', ['addRole', 'modifyRole']);
 
   let mockUserLoggedService: jasmine.SpyObj<UserLoggedService> =
     jasmine.createSpyObj('UserLoggedService', ['currentUser']);
@@ -26,6 +28,7 @@ describe('RoleEntityFormComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       providers: [
+        provideAnimations(),
         {provide: RoleService, useValue: mockRoleService},
         {provide: PrivilegeService, useValue: mockPrivilegeService},
         {provide: UserLoggedService, useValue: mockUserLoggedService},
@@ -36,6 +39,11 @@ describe('RoleEntityFormComponent', () => {
 
     fixture = TestBed.createComponent(RoleEntityFormComponent);
     component = fixture.componentInstance;
+
+    fixture.componentRef.setInput('role', undefined);
+    fixture.componentRef.setInput('submitEventActionType$', undefined);
+
+
     fixture.detectChanges();
   });
 
