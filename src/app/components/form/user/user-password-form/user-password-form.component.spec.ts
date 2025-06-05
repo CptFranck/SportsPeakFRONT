323d7593dced9dffeb2b2691a4d6a1ab2ddec600
CptFranck <francois.caillet71@gmail.com>
2025-06-05 17:17:43 +0200

    fix UserPasswordFormComponent test

--- src/app/components/form/user/user-password-form/user-password-form.component.spec.ts
+++ src/app/components/form/user/user-password-form/user-password-form.component.spec.ts
@@ -2,12 +2,14 @@ import {ComponentFixture, TestBed} from '@angular/core/testing';
 
 import {UserPasswordFormComponent} from './user-password-form.component';
 import {UserService} from "../../../../core/services/user/user.service";
+import {ModificationFieldEnum} from "../../../../shared/model/enum/user-modification-field.enum";
 
 describe('UserPasswordFormComponent', () => {
   let component: UserPasswordFormComponent;
   let fixture: ComponentFixture<UserPasswordFormComponent>;
 
-  let mockUserService: jasmine.SpyObj<UserService>;
+  let mockUserService: jasmine.SpyObj<UserService> =
+    jasmine.createSpyObj('UserService', ['modifyUserPassword']);
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -20,6 +22,11 @@ describe('UserPasswordFormComponent', () => {
 
     fixture = TestBed.createComponent(UserPasswordFormComponent);
     component = fixture.componentInstance;
+
+    fixture.componentRef.setInput('user', undefined);
+    fixture.componentRef.setInput('modification', ModificationFieldEnum.password);
+    fixture.componentRef.setInput('submitEventActionType$', undefined);
+
     fixture.detectChanges();
   });
 
