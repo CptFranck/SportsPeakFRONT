a11aaf55eeaafbaf8ebb34ffd77e6fd343680a43
CptFranck <francois.caillet71@gmail.com>
2025-06-05 15:24:42 +0200

    fix ExerciseTypeCardComponent test

--- src/app/components/card/exercise-type-card/exercise-type-card.component.spec.ts
+++ src/app/components/card/exercise-type-card/exercise-type-card.component.spec.ts
@@ -1,6 +1,8 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { ExerciseTypeCardComponent } from './exercise-type-card.component';
+import {ExerciseTypeCardComponent} from './exercise-type-card.component';
+import {provideRouter} from "@angular/router";
+import {generateTestExerciseType} from "../../../utils/testFunctions";
 
 describe('ExerciseTypeCardComponent', () => {
   let component: ExerciseTypeCardComponent;
@@ -8,12 +10,19 @@ describe('ExerciseTypeCardComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      providers: [
+        provideRouter([]),
+      ],
       imports: [ExerciseTypeCardComponent]
     })
-    .compileComponents();
+      .compileComponents();
 
     fixture = TestBed.createComponent(ExerciseTypeCardComponent);
     component = fixture.componentInstance;
+
+    const exerciseType = generateTestExerciseType();
+    fixture.componentRef.setInput('exerciseType', exerciseType);
+
     fixture.detectChanges();
   });
 
