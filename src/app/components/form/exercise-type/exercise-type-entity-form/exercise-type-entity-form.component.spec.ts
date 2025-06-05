8a4c971c1afbb992a7a3e50d12fc8fa7b3e02924
CptFranck <francois.caillet71@gmail.com>
2025-06-05 13:21:13 +0200

    fix ExerciseTypeEntityFormComponent test

--- src/app/components/form/exercise-type/exercise-type-entity-form/exercise-type-entity-form.component.spec.ts
+++ src/app/components/form/exercise-type/exercise-type-entity-form/exercise-type-entity-form.component.spec.ts
@@ -1,16 +1,18 @@
 import {ComponentFixture, TestBed} from '@angular/core/testing';
 
 import {ExerciseTypeEntityFormComponent} from './exercise-type-entity-form.component';
-import {ExerciseTypeService} from "../../../../core/services/exercise-type/exercise-type.service";
 import {BehaviorSubject} from "rxjs";
 import {ExerciseService} from "../../../../core/services/exercise/exercise.service";
 import {Exercise} from "../../../../shared/model/dto/exercise";
+import {ExerciseTypeService} from "../../../../core/services/exercise-type/exercise-type.service";
+import {provideAnimations} from "@angular/platform-browser/animations";
 
 describe('ExerciseTypeEntityFormComponent', () => {
   let component: ExerciseTypeEntityFormComponent;
   let fixture: ComponentFixture<ExerciseTypeEntityFormComponent>;
 
-  let mockExerciseTypeService: jasmine.SpyObj<ExerciseTypeService>;
+  let mockExerciseTypeService: jasmine.SpyObj<ExerciseTypeService> =
+    jasmine.createSpyObj('ExerciseTypeService', ['addExerciseType', 'modifyExerciseType']);
 
   let mockExerciseService: jasmine.SpyObj<ExerciseService> =
     jasmine.createSpyObj('ExerciseService', ['exercises', 'isLoading']);
@@ -20,6 +22,7 @@ describe('ExerciseTypeEntityFormComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       providers: [
+        provideAnimations(),
         {provide: ExerciseService, useValue: mockExerciseService},
         {provide: ExerciseTypeService, useValue: mockExerciseTypeService},
       ],
@@ -29,6 +32,10 @@ describe('ExerciseTypeEntityFormComponent', () => {
 
     fixture = TestBed.createComponent(ExerciseTypeEntityFormComponent);
     component = fixture.componentInstance;
+
+    fixture.componentRef.setInput('exerciseType', undefined);
+    fixture.componentRef.setInput('submitEventActionType$', undefined);
+
     fixture.detectChanges();
   });
 
