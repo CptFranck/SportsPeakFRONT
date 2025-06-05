66e06d772ded237662502b06d1ab8879c9b3a29c
CptFranck <francois.caillet71@gmail.com>
2025-06-05 11:53:24 +0200

    fix DurationInputComponent test

--- src/app/components/input/duration-inputs/duration-input.component.spec.ts
+++ src/app/components/input/duration-inputs/duration-input.component.spec.ts
@@ -1,8 +1,11 @@
 import {ComponentFixture, TestBed} from '@angular/core/testing';
 
 import {DurationInputComponent} from './duration-input.component';
+import {createDurationForm} from "../../../utils/duration-functions";
+import {Duration} from "../../../shared/model/dto/duration";
+import {FormGroup} from "@angular/forms";
 
-describe('DurationInputsComponent', () => {
+describe('DurationInputComponent', () => {
   let component: DurationInputComponent;
   let fixture: ComponentFixture<DurationInputComponent>;
 
@@ -14,6 +17,12 @@ describe('DurationInputsComponent', () => {
 
     fixture = TestBed.createComponent(DurationInputComponent);
     component = fixture.componentInstance;
+
+    const defaultDuration: Duration = {seconds: 0, minutes: 0, hours: 0};
+    const durationFormGroup = new FormGroup({defaultDuration: createDurationForm(defaultDuration)})
+    fixture.componentRef.setInput('fieldName', "defaultDuration");
+    fixture.componentRef.setInput('formGroup', durationFormGroup);
+
     fixture.detectChanges();
   });
 
