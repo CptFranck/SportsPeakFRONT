bcfbe51696e033f440b3c4d1b4510354235ad8dd
CptFranck <francois.caillet71@gmail.com>
2025-06-05 12:34:35 +0200

    fix services test

--- src/app/core/services/exercise/exercise.service.spec.ts
+++ src/app/core/services/exercise/exercise.service.spec.ts
@@ -2,17 +2,14 @@ import {TestBed} from '@angular/core/testing';
 
 import {ExerciseService} from './exercise.service';
 import {ApolloTestingModule} from "apollo-angular/testing";
-import { provideHttpClientTesting } from "@angular/common/http/testing";
-import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 
 describe('ExerciseService', () => {
   let service: ExerciseService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-    imports: [ApolloTestingModule],
-    providers: [provideHttpClient(withInterceptorsFromDi()), provideHttpClientTesting()]
-});
+      imports: [ApolloTestingModule],
+    });
     service = TestBed.inject(ExerciseService);
   });
 
