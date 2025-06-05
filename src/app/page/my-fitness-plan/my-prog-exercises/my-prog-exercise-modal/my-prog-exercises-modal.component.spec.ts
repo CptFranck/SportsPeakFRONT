da03bc55d8fd766a393aff69958daf926a46f0b9
CptFranck <francois.caillet71@gmail.com>
2025-06-05 16:14:18 +0200

    fix MyProgExercisesModalComponent test

--- src/app/page/my-fitness-plan/my-prog-exercises/my-prog-exercise-modal/my-prog-exercises-modal.component.spec.ts
+++ src/app/page/my-fitness-plan/my-prog-exercises/my-prog-exercise-modal/my-prog-exercises-modal.component.spec.ts
@@ -1,19 +1,42 @@
 import {ComponentFixture, TestBed} from '@angular/core/testing';
 
 import {MyProgExercisesModalComponent} from './my-prog-exercises-modal.component';
+import {ActionEnum} from "../../../../shared/model/enum/action.enum";
+import {ProgExerciseService} from "../../../../core/services/prog-exercise/prog-exercise.service";
+import {ExerciseService} from "../../../../core/services/exercise/exercise.service";
+import {BehaviorSubject} from "rxjs";
+import {Exercise} from "../../../../shared/model/dto/exercise";
 
-describe('MyProgExerciseModalComponent', () => {
+describe('MyProgExercisesModalComponent', () => {
   let component: MyProgExercisesModalComponent;
   let fixture: ComponentFixture<MyProgExercisesModalComponent>;
 
+  let mockProgExerciseService: jasmine.SpyObj<ProgExerciseService> =
+    jasmine.createSpyObj('ProgExerciseService', ['progExercise', 'isLoading']);
+
+  let mockExerciseService: jasmine.SpyObj<ExerciseService> =
+    jasmine.createSpyObj('ExerciseService', ['exercises', 'isLoading']);
+  mockExerciseService.exercises = new BehaviorSubject<Exercise[]>([]);
+  mockExerciseService.isLoading = new BehaviorSubject<boolean>(true);
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [MyProgExercisesModalComponent]
+      imports: [MyProgExercisesModalComponent],
+      providers: [
+        {provide: ExerciseService, useValue: mockExerciseService},
+        {provide: ProgExerciseService, useValue: mockProgExerciseService},
+      ],
     })
       .compileComponents();
 
     fixture = TestBed.createComponent(MyProgExercisesModalComponent);
     component = fixture.componentInstance;
+
+    fixture.componentRef.setInput('modalTitle', "Title");
+    fixture.componentRef.setInput('progExerciseModalId', "Id");
+    fixture.componentRef.setInput('progExercise', undefined);
+    fixture.componentRef.setInput('action', ActionEnum.create);
+
     fixture.detectChanges();
   });
 
