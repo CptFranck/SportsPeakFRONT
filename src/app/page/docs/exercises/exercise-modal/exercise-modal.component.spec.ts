d0f9ccf36b205f31e0c4335ff8c8375620e99682
CptFranck <francois.caillet71@gmail.com>
2025-06-05 15:18:41 +0200

    fix ExerciseModalComponent test

--- src/app/page/docs/exercises/exercise-modal/exercise-modal.component.spec.ts
+++ src/app/page/docs/exercises/exercise-modal/exercise-modal.component.spec.ts
@@ -1,19 +1,53 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { ExerciseModalComponent } from './exercise-modal.component';
+import {ExerciseModalComponent} from './exercise-modal.component';
+import {ActionEnum} from "../../../../shared/model/enum/action.enum";
+import {generateTestExercise} from "../../../../utils/testFunctions";
+import {ExerciseService} from "../../../../core/services/exercise/exercise.service";
+import {provideAnimations} from "@angular/platform-browser/animations";
+import {MuscleService} from "../../../../core/services/muscle/muscle.service";
+import {BehaviorSubject} from "rxjs";
+import {Muscle} from "../../../../shared/model/dto/muscle";
+import {ExerciseTypeService} from "../../../../core/services/exercise-type/exercise-type.service";
+import {ExerciseType} from "../../../../shared/model/dto/exercise-type";
 
 describe('ExerciseModalComponent', () => {
   let component: ExerciseModalComponent;
   let fixture: ComponentFixture<ExerciseModalComponent>;
 
+  let mockMuscleService: jasmine.SpyObj<MuscleService> =
+    jasmine.createSpyObj('MuscleService', ['allMuscle', 'loading']);
+  mockMuscleService.allMuscle.and.returnValues(new BehaviorSubject<Muscle[]>([]));
+  mockMuscleService.loading.and.returnValues(new BehaviorSubject<boolean>(true));
+
+  let mockExerciseService: jasmine.SpyObj<ExerciseService> =
+    jasmine.createSpyObj('ExerciseService', ['addExercise', 'modifyExercise']);
+
+  let mockExerciseTypeService: jasmine.SpyObj<ExerciseTypeService> =
+    jasmine.createSpyObj('ExerciseTypeService', ['exerciseTypes', 'isLoading']);
+  mockExerciseTypeService.exerciseTypes = new BehaviorSubject<ExerciseType[]>([]);
+  mockExerciseTypeService.isLoading = new BehaviorSubject<boolean>(true);
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      providers: [
+        provideAnimations(),
+        {provide: MuscleService, useValue: mockMuscleService},
+        {provide: ExerciseService, useValue: mockExerciseService},
+        {provide: ExerciseTypeService, useValue: mockExerciseTypeService},
+      ],
       imports: [ExerciseModalComponent]
     })
-    .compileComponents();
-    
+      .compileComponents();
+
     fixture = TestBed.createComponent(ExerciseModalComponent);
     component = fixture.componentInstance;
+
+    const exercise = generateTestExercise();
+    fixture.componentRef.setInput('action', ActionEnum.create)
+    fixture.componentRef.setInput('exercise', exercise)
+    fixture.componentRef.setInput('modalTitle', "Title")
+    fixture.componentRef.setInput('exerciseModalId', "Id")
+
     fixture.detectChanges();
   });
 
