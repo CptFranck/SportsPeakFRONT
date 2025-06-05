aefde11ef47a7e0589a0efc1e50f2f96bfd79982
CptFranck <francois.caillet71@gmail.com>
2025-06-05 17:15:22 +0200

    fix MuscleModalComponent test

--- src/app/components/modal-component/muscle-modal/muscle-modal.component.spec.ts
+++ src/app/components/modal-component/muscle-modal/muscle-modal.component.spec.ts
@@ -1,19 +1,43 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { MuscleModalComponent } from './muscle-modal.component';
+import {MuscleModalComponent} from './muscle-modal.component';
+import {ActionEnum} from "../../../shared/model/enum/action.enum";
+import {MuscleService} from "../../../core/services/muscle/muscle.service";
+import {ExerciseService} from "../../../core/services/exercise/exercise.service";
+import {BehaviorSubject} from "rxjs";
+import {Exercise} from "../../../shared/model/dto/exercise";
+import {provideAnimations} from "@angular/platform-browser/animations";
 
 describe('MuscleModalComponent', () => {
   let component: MuscleModalComponent;
   let fixture: ComponentFixture<MuscleModalComponent>;
 
+  let mockMuscleService: jasmine.SpyObj<MuscleService> =
+    jasmine.createSpyObj('MuscleService', ['addMuscle', 'modifyMuscle', 'deleteMuscle']);
+
+  let mockExerciseService: jasmine.SpyObj<ExerciseService> =
+    jasmine.createSpyObj('ExerciseService', ['']);
+  mockExerciseService.exercises = new BehaviorSubject<Exercise[]>([]);
+  mockExerciseService.isLoading = new BehaviorSubject<boolean>(true);
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [MuscleModalComponent]
+      imports: [MuscleModalComponent],
+      providers: [
+        provideAnimations(),
+        {provide: MuscleService, useValue: mockMuscleService},
+        {provide: ExerciseService, useValue: mockExerciseService},
+      ]
     })
-    .compileComponents();
-    
+      .compileComponents();
+
     fixture = TestBed.createComponent(MuscleModalComponent);
     component = fixture.componentInstance;
+
+    fixture.componentRef.setInput('modalTitle', "Title")
+    fixture.componentRef.setInput('muscleModalId', "Id")
+    fixture.componentRef.setInput('action', ActionEnum.create)
+
     fixture.detectChanges();
   });
 
