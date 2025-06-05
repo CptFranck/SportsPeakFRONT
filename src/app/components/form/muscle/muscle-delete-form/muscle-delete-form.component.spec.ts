fa2076bc78f95fa74a2d80e526de27f78efa19a5
CptFranck <francois.caillet71@gmail.com>
2025-06-05 15:55:49 +0200

    fix MuscleDeleteFormComponent test

--- src/app/components/form/muscle/muscle-delete-form/muscle-delete-form.component.spec.ts
+++ src/app/components/form/muscle/muscle-delete-form/muscle-delete-form.component.spec.ts
@@ -3,11 +3,13 @@ import {ComponentFixture, TestBed} from '@angular/core/testing';
 import {MuscleDeleteFormComponent} from './muscle-delete-form.component';
 import {MuscleService} from "../../../../core/services/muscle/muscle.service";
 
-describe('DeleteMuscleComponent', () => {
+describe('MuscleDeleteFormComponent', () => {
   let component: MuscleDeleteFormComponent;
   let fixture: ComponentFixture<MuscleDeleteFormComponent>;
 
-  let mockMuscleService: jasmine.SpyObj<MuscleService>;
+  let mockMuscleService: jasmine.SpyObj<MuscleService> =
+    jasmine.createSpyObj('MuscleService', ['deleteMuscle']);
+
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -20,6 +22,10 @@ describe('DeleteMuscleComponent', () => {
 
     fixture = TestBed.createComponent(MuscleDeleteFormComponent);
     component = fixture.componentInstance;
+
+    fixture.componentRef.setInput('muscle', undefined);
+    fixture.componentRef.setInput('submitEventActionType$', undefined);
+
     fixture.detectChanges();
   });
 
