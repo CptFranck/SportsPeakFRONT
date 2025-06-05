953b9d0d493c9de73b2c08d3ed2b3ef61f3ac5c9
CptFranck <francois.caillet71@gmail.com>
2025-06-05 18:18:25 +0200

    fix ImageFormComponent test

--- src/app/components/form/image-form/image-form.component.spec.ts
+++ src/app/components/form/image-form/image-form.component.spec.ts
@@ -1,16 +1,26 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { ImageFormComponent } from './image-form.component';
+import {ImageFormComponent} from './image-form.component';
+import {IllustrationService} from "../../../core/services/illustration/illustration.service";
+import {BehaviorSubject} from "rxjs";
 
 describe('ImageFormComponent', () => {
   let component: ImageFormComponent;
   let fixture: ComponentFixture<ImageFormComponent>;
 
+  let mockIllustrationService: jasmine.SpyObj<IllustrationService> =
+    jasmine.createSpyObj('IllustrationService', ['getImageUrl', 'refreshData']);
+  mockIllustrationService.getImageUrl.and.returnValues("");
+  mockIllustrationService.refreshData.and.returnValues(new BehaviorSubject<string>(""));
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [ImageFormComponent]
+      imports: [ImageFormComponent],
+      providers: [
+        {provide: IllustrationService, useValue: mockIllustrationService},
+      ]
     })
-    .compileComponents();
+      .compileComponents();
 
     fixture = TestBed.createComponent(ImageFormComponent);
     component = fixture.componentInstance;
