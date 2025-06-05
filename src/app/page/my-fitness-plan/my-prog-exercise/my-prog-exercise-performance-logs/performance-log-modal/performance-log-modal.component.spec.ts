28f4608cc59371704d6c03e595c618f29d5d9b92
CptFranck <francois.caillet71@gmail.com>
2025-06-05 16:12:18 +0200

    fix PerformanceLogModalComponent test

--- src/app/page/my-fitness-plan/my-prog-exercise/my-prog-exercise-performance-logs/performance-log-modal/performance-log-modal.component.spec.ts
+++ src/app/page/my-fitness-plan/my-prog-exercise/my-prog-exercise-performance-logs/performance-log-modal/performance-log-modal.component.spec.ts
@@ -1,19 +1,40 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { PerformanceLogModalComponent } from './performance-log-modal.component';
+import {PerformanceLogModalComponent} from './performance-log-modal.component';
+import {ActionEnum} from "../../../../../shared/model/enum/action.enum";
+import {generateTestExercise, generateTestProgExercise, generateTestUser} from "../../../../../utils/testFunctions";
+import {User} from "../../../../../shared/model/dto/user";
+import {Exercise} from "../../../../../shared/model/dto/exercise";
+import {PerformanceLogService} from "../../../../../core/services/performance-log/performance-log.service";
 
 describe('PerformanceLogModalComponent', () => {
   let component: PerformanceLogModalComponent;
   let fixture: ComponentFixture<PerformanceLogModalComponent>;
 
+  let mockPerformanceLogService: jasmine.SpyObj<PerformanceLogService> =
+    jasmine.createSpyObj('PerformanceLogService', ['deletePerformanceLog']);
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [PerformanceLogModalComponent]
+      imports: [PerformanceLogModalComponent],
+      providers: [
+        {provide: PerformanceLogService, useValue: mockPerformanceLogService},
+      ]
     })
-    .compileComponents();
-    
+      .compileComponents();
+
     fixture = TestBed.createComponent(PerformanceLogModalComponent);
     component = fixture.componentInstance;
+
+    let mockUser: User = generateTestUser();
+    let mockExercise: Exercise = generateTestExercise();
+    fixture.componentRef.setInput('performanceLogModalTitle', "Title");
+    fixture.componentRef.setInput('performanceLogModalId', "Id");
+    fixture.componentRef.setInput('progExercise', generateTestProgExercise(mockUser, mockExercise));
+    fixture.componentRef.setInput('targetSet', undefined);
+    fixture.componentRef.setInput('performanceLog', undefined);
+    fixture.componentRef.setInput('action', ActionEnum.create);
+
     fixture.detectChanges();
   });
 
