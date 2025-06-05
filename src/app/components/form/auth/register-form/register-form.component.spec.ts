3111043ec2f6dc426346663741c7ef074b705825
CptFranck <francois.caillet71@gmail.com>
2025-06-05 15:28:42 +0200

    fix RegisterFormComponent test

--- src/app/components/form/auth/register-form/register-form.component.spec.ts
+++ src/app/components/form/auth/register-form/register-form.component.spec.ts
@@ -6,7 +6,8 @@ import {AuthService} from "../../../../core/services/auth/auth.service";
 describe('RegisterFormComponent', () => {
   let component: RegisterFormComponent;
   let fixture: ComponentFixture<RegisterFormComponent>;
-  let mockAuthService: jasmine.SpyObj<AuthService>
+  let mockAuthService: jasmine.SpyObj<AuthService> =
+    jasmine.createSpyObj('AuthService', ['register']);
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
