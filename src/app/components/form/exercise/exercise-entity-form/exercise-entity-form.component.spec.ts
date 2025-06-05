0dbfaecc47908663fab021790ec540737324fa17
CptFranck <francois.caillet71@gmail.com>
2025-06-05 13:10:40 +0200

    fix ExerciseEntityFormComponent test

--- src/app/components/form/exercise/exercise-entity-form/exercise-entity-form.component.spec.ts
+++ src/app/components/form/exercise/exercise-entity-form/exercise-entity-form.component.spec.ts
@@ -2,22 +2,24 @@ import {ComponentFixture, TestBed} from '@angular/core/testing';
 
 import {ExerciseEntityFormComponent} from './exercise-entity-form.component';
 import {ExerciseService} from "../../../../core/services/exercise/exercise.service";
-import {BehaviorSubject} from "rxjs";
 import {MuscleService} from "../../../../core/services/muscle/muscle.service";
+import {BehaviorSubject} from "rxjs";
 import {Muscle} from "../../../../shared/model/dto/muscle";
 import {ExerciseTypeService} from "../../../../core/services/exercise-type/exercise-type.service";
 import {ExerciseType} from "../../../../shared/model/dto/exercise-type";
+import {provideAnimations} from "@angular/platform-browser/animations";
 
 describe('ExerciseEntityFormComponent', () => {
   let component: ExerciseEntityFormComponent;
   let fixture: ComponentFixture<ExerciseEntityFormComponent>;
 
   let mockMuscleService: jasmine.SpyObj<MuscleService> =
-    jasmine.createSpyObj('MuscleService', ['muscles', 'isLoading']);
-  mockMuscleService.muscles = new BehaviorSubject<Muscle[]>([]);
-  mockMuscleService.isLoading = new BehaviorSubject<boolean>(true);
+    jasmine.createSpyObj('MuscleService', ['allMuscle', 'loading']);
+  mockMuscleService.allMuscle.and.returnValues(new BehaviorSubject<Muscle[]>([]));
+  mockMuscleService.loading.and.returnValues(new BehaviorSubject<boolean>(true));
 
-  let mockExerciseService: jasmine.SpyObj<ExerciseService>;
+  let mockExerciseService: jasmine.SpyObj<ExerciseService> =
+    jasmine.createSpyObj('ExerciseService', ['addExercise', 'modifyExercise']);
 
   let mockExerciseTypeService: jasmine.SpyObj<ExerciseTypeService> =
     jasmine.createSpyObj('ExerciseTypeService', ['exerciseTypes', 'isLoading']);
@@ -27,6 +29,7 @@ describe('ExerciseEntityFormComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       providers: [
+        provideAnimations(),
         {provide: MuscleService, useValue: mockMuscleService},
         {provide: ExerciseService, useValue: mockExerciseService},
         {provide: ExerciseTypeService, useValue: mockExerciseTypeService},
@@ -37,6 +40,10 @@ describe('ExerciseEntityFormComponent', () => {
 
     fixture = TestBed.createComponent(ExerciseEntityFormComponent);
     component = fixture.componentInstance;
+
+    fixture.componentRef.setInput('exercise', undefined);
+    fixture.componentRef.setInput('submitEventActionType$', undefined);
+
     fixture.detectChanges();
   });
 
