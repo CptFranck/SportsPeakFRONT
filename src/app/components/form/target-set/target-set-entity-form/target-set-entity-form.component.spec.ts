f29aaa312eb0760552e058e686fc4ac926fd06d7
CptFranck <francois.caillet71@gmail.com>
2025-06-05 17:39:37 +0200

    fix TargetSetEntityFormComponent test

--- src/app/components/form/target-set/target-set-entity-form/target-set-entity-form.component.spec.ts
+++ src/app/components/form/target-set/target-set-entity-form/target-set-entity-form.component.spec.ts
@@ -2,12 +2,16 @@ import {ComponentFixture, TestBed} from '@angular/core/testing';
 
 import {TargetSetEntityFormComponent} from './target-set-entity-form.component';
 import {TargetSetService} from "../../../../core/services/target-set/target-set.service";
+import {ActionEnum} from "../../../../shared/model/enum/action.enum";
 
 describe('TargetSetEntityFormComponent', () => {
   let component: TargetSetEntityFormComponent;
   let fixture: ComponentFixture<TargetSetEntityFormComponent>;
 
-  let mockTargetSetService: jasmine.SpyObj<TargetSetService>;
+  let mockTargetSetService: jasmine.SpyObj<TargetSetService> =
+    jasmine.createSpyObj('TargetSetService',
+      ['addTargetSet', 'modifyTargetSet', 'modifyTargetSetState', 'deleteTargetSet']);
+  ;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -20,6 +24,12 @@ describe('TargetSetEntityFormComponent', () => {
 
     fixture = TestBed.createComponent(TargetSetEntityFormComponent);
     component = fixture.componentInstance;
+
+    fixture.componentRef.setInput('targetSet', undefined);
+    fixture.componentRef.setInput('progExercise', undefined);
+    fixture.componentRef.setInput('actionType', ActionEnum.read);
+    fixture.componentRef.setInput('submitEventActionType$', undefined);
+
     fixture.detectChanges();
   });
 
