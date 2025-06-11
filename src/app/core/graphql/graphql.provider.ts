ffe0938ce6a14b5083539491bbea564e88043214
CptFranck <francois.caillet71@gmail.com>
2025-06-11 11:15:28 +0200

    refacto graphql provider class

--- src/app/core/graphql/graphql.provider.ts
+++ src/app/core/graphql/graphql.provider.ts
@@ -1,52 +1,14 @@
 import {Apollo, APOLLO_OPTIONS} from 'apollo-angular';
-import {HttpLink, HttpLinkHandler} from 'apollo-angular/http';
+import {HttpLink} from 'apollo-angular/http';
 import {ApplicationConfig, inject} from '@angular/core';
 import {ApolloClientOptions, ApolloLink, InMemoryCache} from '@apollo/client/core';
 import {AlertService} from "../services/alert/alert.service";
 import {ErrorResponse, onError} from "@apollo/client/link/error";
 import {setContext} from "@apollo/client/link/context";
 import {TokenService} from "../services/token/token.service";
-import {AuthToken} from "../../shared/model/dto/token";
 import {environment} from "../environment";
 
-// URL of the GraphQL server here
-const uri: string = environment.apiHttpBaseUrl + environment.graphqlEndPoint;
-
-function apolloOptionsFactory(): ApolloClientOptions<any> {
-  const httpLink: HttpLink = inject(HttpLink);
-  const alertService: AlertService = inject(AlertService)
-  const tokenService: TokenService = inject(TokenService)
-
-  const auth: ApolloLink = setContext(() => {
-    const authToken: AuthToken | null = tokenService.getCurrentToken();
-    if (!authToken) {
-      return {};
-    }
-    return {
-      headers: {
-        Authorization: `${authToken.tokenType} ${authToken.accessToken}`,
-      },
-    };
-  });
-
-  const errorLinkHandler: ApolloLink = onError((errorResponse: ErrorResponse) => {
-    if (errorResponse.graphQLErrors)
-      alertService.graphQLErrorAlertHandler(errorResponse.graphQLErrors);
-    if (errorResponse.networkError)
-      alertService.createNetWorkErrorAlert(errorResponse.networkError)
-  });
-
-  const httpLinkHandler: HttpLinkHandler = httpLink.create({uri})
-
-  return {
-    link: ApolloLink.from([
-      auth,
-      errorLinkHandler,
-      httpLinkHandler,
-    ]),
-    cache: new InMemoryCache(),
-  };
-}
+const GRAPHQL_API_URI = environment.apiHttpBaseUrl + environment.graphqlEndPoint;
 
 export const graphqlProvider: ApplicationConfig['providers'] = [
   Apollo,
@@ -55,3 +17,48 @@ export const graphqlProvider: ApplicationConfig['providers'] = [
     useFactory: apolloOptionsFactory,
   },
 ];
+
+function apolloOptionsFactory(): ApolloClientOptions<any> {
+  const httpLink: HttpLink = inject(HttpLink);
+  const alertService: AlertService = inject(AlertService)
+  const tokenService: TokenService = inject(TokenService)
+
+  return {
+    link: createApolloLink(tokenService, alertService, httpLink),
+    cache: new InMemoryCache(),
+  };
+}
+
+function createApolloLink(tokenService: TokenService, alertService: AlertService, httpLink: HttpLink): ApolloLink {
+  return ApolloLink.from([
+    setAuthHeaders(tokenService),
+    handleGraphQLErrors(alertService),
+    httpLink.create({
+      uri: GRAPHQL_API_URI,
+      withCredentials: true,
+    }),
+  ]);
+}
+
+function setAuthHeaders(tokenService: TokenService): ApolloLink {
+  return setContext((operation, context) => {
+    console.log("auth", context)
+    const authToken = tokenService.getCurrentToken();
+    if (!authToken) return {...context};
+    return {
+      headers: {
+        ...context["headers"],
+        Authorization: `${authToken.tokenType} ${authToken.accessToken}`,
+      },
+    };
+  });
+}
+
+function handleGraphQLErrors(alertService: AlertService): ApolloLink {
+  return onError((errorResponse: ErrorResponse) => {
+    if (errorResponse.graphQLErrors)
+      alertService.graphQLErrorAlertHandler(errorResponse.graphQLErrors);
+    if (errorResponse.networkError)
+      alertService.createNetWorkErrorAlert(errorResponse.networkError);
+  });
+}
