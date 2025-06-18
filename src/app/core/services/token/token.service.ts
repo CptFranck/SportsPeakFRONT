e78e329a58304a0b621ffb1b03ce6501f8f92eca
CptFranck <francois.caillet71@gmail.com>
2025-06-18 17:45:26 +0200

    update TokenService

--- src/app/core/services/token/token.service.ts
+++ src/app/core/services/token/token.service.ts
@@ -2,6 +2,7 @@ import {Injectable} from '@angular/core';
 import {AuthToken} from "../../../shared/model/dto/authToken";
 import {Auth} from "../../../shared/model/dto/auth";
 import {jwtDecode, JwtPayload} from "jwt-decode";
+import {BehaviorSubject} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
@@ -9,24 +10,20 @@ import {jwtDecode, JwtPayload} from "jwt-decode";
 export class TokenService {
 
   private authToken: AuthToken | null = null;
+  private expirationTimeout: ReturnType<typeof setTimeout> | undefined;
+  private readonly authTokenExpiredSubject = new BehaviorSubject<void>(undefined);
 
-  setAuthToken(data: Auth) {
-    this.authToken = this.createAuthToken(data);
+  get authTokenExpired$() {
+    return this.authTokenExpiredSubject.asObservable();
   }
 
   getAuthToken() {
     return this.authToken;
   }
 
-  removeAuthToken() {
-    this.authToken = null;
-  }
-
-  isTokenValid(authToken: AuthToken | null): boolean {
-    const currentTime = Date.now() / 1000;
-    if (authToken !== null && authToken.expiration)
-      return authToken.expiration > currentTime;
-    return false;
+  setAuthToken(data: Auth) {
+    this.authToken = this.createAuthToken(data);
+    this.setTokenExpirationTimer();
   }
 
   private createAuthToken(data: Auth): AuthToken {
@@ -36,5 +33,22 @@ export class TokenService {
       accessToken: data.accessToken,
       expiration: decodedToken.exp
     };
+  }
+
+  private setTokenExpirationTimer(): void {
+    if (!this.authToken || !this.authToken.expiration) return;
+
+    const currentTime = Date.now() / 1000;
+    const expiresInSec = this.authToken.expiration - currentTime;
+
+    if (expiresInSec <= 0) {
+      this.authTokenExpiredSubject.next();
+      return;
+    }
+    if (this.expirationTimeout) clearTimeout(this.expirationTimeout);
+    this.expirationTimeout = setTimeout(() => {
+      this.authToken = null;
+      this.authTokenExpiredSubject.next();
+    }, expiresInSec * 1000);
   }
 }
