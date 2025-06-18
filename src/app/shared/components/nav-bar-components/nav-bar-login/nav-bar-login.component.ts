848eaa992b2a59635953278fbe426c72bcc9f04a
CptFranck <francois.caillet71@gmail.com>
2025-06-18 20:46:39 +0200

    update NavBarLoginComponent

--- src/app/shared/components/nav-bar-components/nav-bar-login/nav-bar-login.component.ts
+++ src/app/shared/components/nav-bar-components/nav-bar-login/nav-bar-login.component.ts
@@ -2,6 +2,7 @@ import {Component, inject, OnDestroy, OnInit} from '@angular/core';
 import {Router, RouterLink} from "@angular/router";
 import {Subject, takeUntil} from "rxjs";
 import {AuthService} from "../../../../core/services/auth/auth.service";
+import {AuthState} from "../../../model/enum/authState";
 
 @Component({
   selector: 'app-nav-bar-login',
@@ -21,9 +22,7 @@ export class NavBarLoginComponent implements OnInit, OnDestroy {
   ngOnInit() {
     this.authService.isAuthenticated$
       .pipe(takeUntil(this.unsubscribe$))
-      .subscribe((isLogged: boolean) => {
-        this.isLogged = isLogged;
-      });
+      .subscribe((isLogged: AuthState) => this.isLogged = isLogged === AuthState.authenticated);
   }
 
   ngOnDestroy() {
