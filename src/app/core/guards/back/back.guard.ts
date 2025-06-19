ea5da07f8e7eebade1bcf68395f0c3bad4190297
CptFranck <francois.caillet71@gmail.com>
2025-06-19 12:46:48 +0200

    update BackGuard

--- src/app/core/guards/back/back.guard.ts
+++ src/app/core/guards/back/back.guard.ts
@@ -1,21 +1,15 @@
 import {CanActivateFn, Router} from '@angular/router';
 import {inject} from "@angular/core";
 import {CurrentUserService} from "../../services/current-user/current-user.service";
-import {AuthService} from "../../services/auth/auth.service";
-import {filter, from, map, of, switchMap, take} from "rxjs";
-import {AuthState} from "../../../shared/model/enum/authState";
+import {from, map, of, switchMap, take} from "rxjs";
 
 export const BackGuard: CanActivateFn = () => {
   const router: Router = inject(Router);
-  const authService = inject(AuthService);
   const currentUserService: CurrentUserService = inject(CurrentUserService)
 
-  return authService.isAuthenticated$.pipe(
-    filter(state => state !== AuthState.unknown),
+  return currentUserService.currentUser$.pipe(
     take(1),
-    switchMap(state => {
-      if (state !== AuthState.authenticated)
-        return from(router.navigate(['auth'])).pipe(map(() => false));
+    switchMap(() => {
       if (!currentUserService.isStaff())
         return from(router.navigate(['/error/access-denied'])).pipe(map(() => false));
       return of(true);
